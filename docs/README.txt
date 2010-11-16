ea0ed0ce344da6452b58c20a36d7e79e377a0e49
Francisco de la Peña <frandelapena@gmail.com>
2010-11-16 10:18:48 +0100

    Removed the rst2pdf dependency to build the docs to enable at least the html compilation on windows

--- docs/README.txt
+++ docs/README.txt
@@ -1,9 +1,1 @@
-To build the documentation you need to install python-sphinx and rst2pdf
-
-Once you have installed the dependencies simply call:
-
-# For html
-make html
-
-# For pdf
-make latexpdf
+To build the documentation you need to install python-sphinx
--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ from  src import Release
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'rst2pdf.pdfbuilder']
+extensions = ['sphinx.ext.autodoc'] #, 'rst2pdf.pdfbuilder']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
