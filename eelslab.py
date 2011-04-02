be6467f0b6e5faed007b09e3cf6df18cdc8ef215
Francisco de la Peña <frandelapena@gmail.com>
2011-04-02 18:39:11 +0200

    BUG-FIX: an error was raised when the backend was not WX

--- eelslab.py
+++ eelslab.py
@@ -18,6 +18,17 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  
 # USA
 
+# The following code was commented because it does not work with
+# ETS 3.6.0 , to investigate...
+from enthought.etsconfig.api import ETSConfig
+import matplotlib
+if matplotlib.get_backend() == 'Qt4Agg':
+    ETSConfig.toolkit ='qt4'
+elif matplotlib.get_backend() == 'WXAgg':
+    ETSConfig.toolkit ='wx'
+else:
+    ETSConfig.toolkit ='null'
+
 import silib.Release
 import silib.components as components
 
