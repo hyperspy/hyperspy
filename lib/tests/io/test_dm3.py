92fa3a864ea8d10ba64e924d7d8e035c042224e3
Francisco de la Peña <frandelapena@gmail.com>
2011-04-07 17:24:06 +0200

    [BUG-FIX] data_cube renamed to data to make it work with the Signal class

--- lib/tests/io/test_dm3.py
+++ lib/tests/io/test_dm3.py
@@ -10,14 +10,14 @@ old = False
 def test_dtypes():
     for key in data_types.iterkeys():
         s = load("dm3_1D_data/test-%s.dm3" % key, old = old)
-        yield check_dtype, s.data_cube.dtype, np.dtype(data_types[key]), key
+        yield check_dtype, s.data.dtype, np.dtype(data_types[key]), key
 
 # TODO: the RGB data generated is not correct        
 def test_1D_content():
     for key in data_types.iterkeys():
         dat = np.arange(1,3, dtype = data_types[key])
         s = load("dm3_1D_data/test-%s.dm3" % key, old = old)
-        yield check_content, s.data_cube, dat, key
+        yield check_content, s.data, dat, key
 
 def check_dtype(d1, d2, i):
     assert_true(d1 == d2, msg = 'test_dtype-%i' % i)
