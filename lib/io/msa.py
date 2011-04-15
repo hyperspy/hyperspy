2f2752332446206b5453309f6164326bb3459a8b
Francisco de la Peña <frandelapena@gmail.com>
2011-04-15 14:44:09 +0200

    The msa format has been totally rewritten and improved for the signal class. Fixes #6
    ENHANCE: the reader stores all the keywords with the right type
    ENHANCE: when writing, it writes all the keywords from the original file, so no information is lost
    ENHANCE: it supports all the data formats defined in the standard. Unlike the original code from the default branch, it reads all the information from the test files in http://www.amc.anl.gov/ANLSoftwareLibrary/02-MMSLib/XEDS/EMMFF/EMMFF.IBM/Emmff.Total
    ENHANCE: it reads the units from the keywords and store then in a separate key if the parameter is mapped

--- lib/io/msa.py
+++ lib/io/msa.py
@@ -29,6 +29,7 @@ from ..utils import generate_axis
 from ..microscope import microscope
 from .. import messages
 from ..utils_varia import overwrite
+from .. import Release
 
 # Plugin characteristics
 # ----------------------
@@ -48,26 +49,126 @@ writes_spectrum = True
 writes_spectrum_image = False
 # ----------------------
 
-
-def file_reader(filename, **kwds):
-    calibration_dict = {}
-    acquisition_dict = {}
-    spectrum_file = open(filename)
-    parameters = {}
-    y = []
-    for line in spectrum_file.readlines():
-        if line[0] == "#":
-            parameters[line[1:line.find(' ')]] = \
-                line[line.find(' : ')+3:].strip()
-        else :
-            if parameters['DATATYPE'] == 'XY' :
-                pair = line.strip().split(', ')
-                if len(pair) != 2:
-                    pair = line.strip().split('\t')
-                y.append(float(pair[1]))
-            elif parameters['DATATYPE'] == 'Y' :
-                y.append(float(line.replace(',','')))
+# For a description of the EMSA/MSA format, incluiding the meaning of the 
+# following keywords: 
+# http://www.amc.anl.gov/ANLSoftwareLibrary/02-MMSLib/XEDS/EMMFF/EMMFF.IBM/Emmff.Total
+keywords = {    
+                # Required parameters
+                'FORMAT' : {'dtype' : str, 'mapped_to': None},
+                'VERSION' : {'dtype' : str, 'mapped_to': None},
+                'TITLE' : {'dtype' : str, 'mapped_to': None},
+                'DATE' : {'dtype' : str, 'mapped_to': None},     
+                'TIME' : {'dtype' : str, 'mapped_to': None},
+                'OWNER' : {'dtype' : str, 'mapped_to': None},
+                'NPOINTS' : {'dtype' : float, 'mapped_to': None},
+                'NCOLUMNS' : {'dtype' : float, 'mapped_to': None},
+                'DATATYPE' : {'dtype' : str, 'mapped_to': None},
+                'XPERCHAN' : {'dtype' : float, 'mapped_to': None},
+                'OFFSET' : {'dtype' : float, 'mapped_to': None},
+                # Optional parameters
+                ## Spectrum characteristics
+                'SIGNALTYPE' : {'dtype' : str, 'mapped_to': None},
+                'XLABEL' : {'dtype' : str, 'mapped_to': None},
+                'YLABEL' : {'dtype' : str, 'mapped_to': None},
+                'XUNITS' : {'dtype' : str, 'mapped_to': None},
+                'YUNITS' : {'dtype' : str, 'mapped_to': None},
+                'CHOFFSET' : {'dtype' : float, 'mapped_to': None},
+                'COMMENT' : {'dtype' : str, 'mapped_to': None},
+                ## Microscope
+                'BEAMKV' : {'dtype' : float, 'mapped_to': 'beam_energy'},
+                'EMISSION' : {'dtype' : float, 'mapped_to': None},
+                'PROBECUR' : {'dtype' : float, 'mapped_to': None},
+                'BEAMDIAM' : {'dtype' : float, 'mapped_to': None},
+                'MAGCAM' : {'dtype' : float, 'mapped_to': None},
+                'OPERMODE' : {'dtype' : str, 'mapped_to': None},
+                'CONVANGLE' : {'dtype' : float, 'mapped_to': None},
                 
+                ## Specimen
+                'THICKNESS' : {'dtype' : float, 'mapped_to': None},
+                'XTILTSTGE' : {'dtype' : float, 'mapped_to': None},
+                'YTILTSTGE' : {'dtype' : float, 'mapped_to': None},
+                'XPOSITION' : {'dtype' : float, 'mapped_to': None},
+                'YPOSITION' : {'dtype' : float, 'mapped_to': None},
+                'ZPOSITION' : {'dtype' : float, 'mapped_to': None},
+                
+                ## EELS
+                'INTEGTIME' : {'dtype' : float, 'mapped_to': None}, # in ms
+                'DWELLTIME' : {'dtype' : float, 'mapped_to': None}, # in ms
+                'COLLANGLE' : {'dtype' : float, 'mapped_to': None},
+                'ELSDET' :  {'dtype' : str, 'mapped_to': None},
+
+                ## EDS
+                'ELEVANGLE' : {'dtype' : float, 'mapped_to': None},
+                'AZIMANGLE' : {'dtype' : float, 'mapped_to': None},
+                'SOLIDANGLE' : {'dtype' : float, 'mapped_to': None},
+                'LIVETIME' : {'dtype' : float, 'mapped_to': None},
+                'REALTIME' : {'dtype' : float, 'mapped_to': None},
+                'TBEWIND' : {'dtype' : float, 'mapped_to': None},
+                'TAUWIND' : {'dtype' : float, 'mapped_to': None},
+                'TDEADLYR' : {'dtype' : float, 'mapped_to': None},
+                'TACTLYR' : {'dtype' : float, 'mapped_to': None},
+                'TALWIND' : {'dtype' : float, 'mapped_to': None},
+                'TPYWIND' : {'dtype' : float, 'mapped_to': None},
+                'TBNWIND' : {'dtype' : float, 'mapped_to': None},
+                'TDIWIND' : {'dtype' : float, 'mapped_to': None},
+                'THCWIND' : {'dtype' : float, 'mapped_to': None},
+                'EDSDET'  : {'dtype' : str, 'mapped_to': None},	
+            }
+def file_reader(filename, **kwds):
+    parameters = {}
+    mapped = {}
+    spectrum_file = open(filename)
+
+
+    y = []
+    # Read the keywords
+    data_section = False
+    for line in spectrum_file.readlines():
+        if data_section is False:
+            if line[0] == "#":
+                key,value = line.split(': ')
+                key = key.strip('#').strip()
+                value = value.strip()
+                
+                if key != 'SPECTRUM':
+                    parameters[key] = value
+                else:
+                    data_section = True
+        else:
+            # Read the data
+            if line[0] != "#" and line.strip(): 
+                if parameters['DATATYPE'] == 'XY':
+                    xy = line.replace(',', ' ').strip().split()
+                    y.append(float(xy[1]))
+                elif parameters['DATATYPE'] == 'Y':
+                    data = [
+                    float(i) for i in line.replace(',', ' ').strip().split()]
+                    y.extend(data)
+    # We rewrite the format value to be sure that it complies with the 
+    # standard, because it will be used by the writer routine
+    parameters['FORMAT'] = "EMSA/MAS Spectral Data File"
+    
+    # Convert the parameters to the right type and map some
+    # TODO: the msa format seems to support specifying the units of some 
+    # parametes. We should add this feature here
+    for parameter, value in parameters.iteritems():
+        # Some parameters names can contain the units information
+        # e.g. #AZIMANGLE-dg: 90.
+        if '-' in parameter:
+            clean_par, units = parameter.split('-')
+            clean_par, units = clean_par.strip(), units.strip()
+        else:
+            clean_par, units = parameter, None
+        if clean_par in keywords:
+            parameters[parameter] = keywords[clean_par]['dtype'](value)
+            if keywords[clean_par]['mapped_to'] is not None:
+                mapped[keywords[clean_par]['mapped_to']] = parameters[parameter]
+                if units is not None:
+                    mapped[keywords[clean_par]['mapped_to']+'_units'] = units
+                
+    # The data parameter needs some extra care
+    # It is necessary to change the locale to US english to read the date
+    # keyword            
     loc = locale.getlocale(locale.LC_TIME)
     
     if os_name == 'posix':
@@ -76,176 +177,136 @@ def file_reader(filename, **kwds):
         locale.setlocale(locale.LC_TIME, 'english')
     try:
         H, M = time.strptime(parameters['TIME'], "%H:%M")[3:5]
+        mapped['time'] = datetime.time(H, M)
     except:
-        H, M = time.strptime('00:00', "%H:%M")[3:5]
-    calibration_dict['time'] = datetime.time(H, M)
-    try:
+        print('The time information could not be retrieved')
+    try:    
         Y, M, D = time.strptime(parameters['DATE'], "%d-%b-%Y")[0:3]
+        mapped['date'] = datetime.date(Y, M, D)
     except:
-        messages.warning('The date information could not be properly read\n'
-                         "Ernst Ruska's birthday is used instead")
-        # Default to Ernst Ruska birth day
-        Y, M, D = time.strptime('25-Dec-1906', "%d-%b-%Y")[0:3]
-    calibration_dict['date'] = datetime.date(Y, M, D)
+        print('The date information could not be retrieved')
+
     locale.setlocale(locale.LC_TIME, loc) # restore saved locale
 
-    if 'TITLE' in parameters:
-        calibration_dict['title'] = parameters['TITLE']
-    else:
-        calibration_dict['title'] = 'Untitled'
-    if 'OWNER' in parameters:
-        calibration_dict['owner'] = parameters['OWNER']
-    else:
-        calibration_dict['owner'] = 'Unknown'
-    calibration_dict['xorigin'] = 0
-    calibration_dict['xscale'] = 1
-    calibration_dict['xdimension'] = 1
-    calibration_dict['xunits'] = ""
-    
-    calibration_dict['yorigin'] = 0
-    calibration_dict['yscale'] = 1
-    calibration_dict['ydimension'] = 1
-    calibration_dict['yunits'] = ""
-    print_microscope = False
-    if parameters.has_key('INTEGTIME'):
-        acquisition_dict['exposure'] = float(parameters['INTEGTIME'])
-        
-    if parameters.has_key('CONVANGLE'):
-       microscope.alpha = float(parameters['CONVANGLE'])
-       print_microscope = True
-    if parameters.has_key('COLLANGLE'):
-       microscope.beta = float(parameters['COLLANGLE'])
-       print_microscope = True
-    if parameters.has_key('BEAMKV'):
-       microscope.E0 = float(parameters['BEAMKV'])
-       print_microscope = True
-    if parameters.has_key('PPPC'):
-       microscope.pppc = float(parameters['PPPC'])
-       print_microscope = True
-    if parameters.has_key('CORRFAC'):
-       microscope.correlation_factor = float(parameters['CORRFAC'])
-       print_microscope = True
-    if print_microscope:
-        print "\nWarning: Reading the microscope parameters from the file"
-        print microscope
-    calibration_dict['energyorigin'] = float(parameters['OFFSET'])
-    calibration_dict['energyscale'] = float(parameters['XPERCHAN'])
-    calibration_dict['energydimension'] = int(parameters['NPOINTS'])
-    calibration_dict['energyunits'] =  parameters['XUNITS']
-    calibration_dict['energy_axis'] = generate_axis(calibration_dict['energyorigin'],calibration_dict['energyscale'], 
-    calibration_dict['energydimension'])
-    calibration_dict['data_cube'] = np.zeros((calibration_dict['energydimension'],1,1))
-    calibration_dict['data_cube'][:,0,0] = np.array(y)
-    spectrum_file.close()
-    dictionary = {'data_type' : 'SI', 'calibration' : calibration_dict, 
-    'acquisition' : acquisition_dict}
+    axes = []
+
+    axes.append({
+            'size' : len(y), 
+            'index_in_array' : 0,
+            'name' : parameters['XLABEL'] if 'XLABEL' in parameters else '', 
+            'scale': parameters['XPERCHAN'] if 'XPERCHAN' in parameters else 1,
+            'offset' : parameters['OFFSET'] if 'OFFSET' in parameters else 0,
+            'units' : parameters['XUNITS'] if 'XUNITS' in parameters else '',
+                })
+
+
+
+    dictionary = {
+                    'data_type' : 'SI', 
+                    'data' : np.array(y),
+                    'axes' : axes,
+                    'mapped_parameters': mapped,
+                    'original_parameters' : parameters
+                }
     return [dictionary,]
 
-def file_writer(filename, spectrum, write_microscope_parameters = True, 
-                format = 'Y', separator = ', '):
+def file_writer(filename, signal, format = None, separator = ', '):
     if not overwrite(filename): # we do not want to blindly overwrite, do we?
         return 0
-    FORMAT = "EMSA/MAS Spectral Data File"
-    VERSION = '1.0'
-    STARTDATA = 'SPECTRUM'
-    ENDDATA = 'ENDOFDATA'
     keywords = {}
-    if hasattr(spectrum, "title"):
-        if len(spectrum.title) > 64 :
-            print "The maximum lenght of the title is 64 char"
-            print "The current title:\"", spectrum.title, "\" is too long."
-            print "Truncating to the first 64 characters."
-            keywords['TITLE'] = spectrum.title[:64] # max 64 char
-        else :
-            keywords['TITLE'] = spectrum.title # max 64 char
-    else :
-        keywords['TITLE'] = 'Untitled'
-    if hasattr(spectrum, "date"):
-        loc = locale.getlocale(locale.LC_TIME)
-        if os_name == 'posix':
-            locale.setlocale(locale.LC_TIME, ('en_US', 'UTF8'))
-        elif os_name == 'windows':
-            locale.setlocale(locale.LC_TIME, 'english')
-        keywords['DATE'] = spectrum.date.strftime("%d-%b-%Y")
-        locale.setlocale(locale.LC_TIME, loc) # restore saved locale
-    else:
-        loc = locale.getlocale(locale.LC_TIME)
-        if os_name == 'posix':
-            locale.setlocale(locale.LC_TIME, ('en_US', 'UTF8'))
-        elif os_name == 'windows':
-            locale.setlocale(locale.LC_TIME, 'english')
-        keywords['DATE'] = time.strftime("%d-%b-%Y")
-        locale.setlocale(locale.LC_TIME, loc) # restore saved locale
-    if hasattr(spectrum, "time"):
-        keywords['TIME'] = spectrum.time.strftime("%H:%M")
-    else:
-        keywords['TIME'] = time.strftime("%H:%M")
-    if hasattr(spectrum, "owner"):
-        keywords['OWNER'] = spectrum.owner
-    else:
-        keywords['OWNER'] = 'Undefined'
-    if spectrum.energydimension > 4096 :
-        print "The MSA format does not support spectrum with more \
-         than 4096 channels."
-        print "The file was not saved."
-        return 0                # exception?
-    else :
-       keywords['NPOINTS'] = spectrum.energydimension
-    keywords['NCOLUMNS'] = 1
-
-    xunits = 'eV'
-    if hasattr(spectrum, "energyunits"): # we assume a simple spectrum
-        if spectrum.energyunits:     # must not be empty
-            xunits = spectrum.energyunits
+    FORMAT = "EMSA/MAS Spectral Data File"
+    if 'FORMAT' in signal.original_parameters and \
+    signal.original_parameters['FORMAT'] == FORMAT:
+        keywords = signal.original_parameters
+        if format is not None:
+            keywords['DATATYPE'] = format
         else:
-            print "The units 'x units' are not set. Assuming 'eV'."
-    keywords['XUNITS'] = xunits
+            if 'DATATYPE' in keywords:
+                format = keywords['DATATYPE']
+    else:
+        if format is None:
+            format = 'X'
+        if hasattr(signal.mapped_parameters, "date"):
+            loc = locale.getlocale(locale.LC_TIME)
+            if os_name == 'posix':
+                locale.setlocale(locale.LC_TIME, ('en_US', 'UTF8'))
+            elif os_name == 'windows':
+                locale.setlocale(locale.LC_TIME, 'english')
+            keywords['DATE'] = signal.mapped_parameters.data.strftime("%d-%b-%Y")
+            locale.setlocale(locale.LC_TIME, loc) # restore saved locale
+            
+    keys_from_signal = {
+        # Required parameters
+        'FORMAT' : FORMAT,
+        'VERSION' : '1.0',
+        'TITLE' : signal.title[:64] if hasattr(signal, "title") else '',
+        'DATA' : '',
+        'TIME' : '',
+        'OWNER' : '',
+        'NPOINTS' : signal.axes_manager.axes[0].size,
+        'NCOLUMNS' : 1,
+        'DATATYPE' : format,
+        'XPERCHAN' : signal.axes_manager.axes[0].scale,
+        'OFFSET' : signal.axes_manager.axes[0].offset,
+        ## Spectrum characteristics
+
+        'XLABEL' : signal.axes_manager.axes[0].name,
+#        'YLABEL' : '',
+        'XUNITS' : signal.axes_manager.axes[0].units,
+#        'YUNITS' : '',
+        'COMMENT' : 'File created by EELSLab version %s' % Release.version,
+#        ## Microscope
+#        'BEAMKV' : ,
+#        'EMISSION' : ,
+#        'PROBECUR' : ,
+#        'BEAMDIAM' : ,
+#        'MAGCAM' : ,
+#        'OPERMODE' : ,
+#        'CONVANGLE' : ,
+#        ## Specimen
+#        'THICKNESS' : ,
+#        'XTILTSTGE' : ,
+#        'YTILTSTGE' : ,
+#        'XPOSITION' : ,
+#        'YPOSITION' : ,
+#        'ZPOSITION' : ,
+#        
+#        ## EELS
+#        'INTEGTIME' : , # in ms
+#        'DWELLTIME' : , # in ms
+#        'COLLANGLE' : ,
+#        'ELSDET' :  ,                     
+    }
     
-    yunits = 'counts'
-    if hasattr(spectrum, "yunits"):
-        if spectrum.yunits:     # must not be empty
-            yunits = spectrum.yunits
-        else:
-            print "The units 'y units' are not set. Assuming 'counts'."
-    keywords['YUNITS'] = yunits
+    # Update the keywords with the information retrieved from the signal class
+    for key, value in keys_from_signal.iteritems():
+        if key not in keywords or value != '':
+            keywords[key] = value
 
-    if spectrum.acquisition_parameters.exposure is not None:
-        keywords['INTEGTIME'] = spectrum.acquisition_parameters.exposure
-    if format == 'XY':
-        keywords['DATATYPE'] = 'XY'
+    f = open(filename, 'w')   
+    # Remove the following keys from keywords if they are in 
+    # (although they shouldn't)
+    for key in ['SPECTRUM', 'ENDOFDATA']:
+        if key in keywords: del(keywords[key])
+    
+    f.write(u'#%-12s: %s\u000D\u000A' % ('FORMAT', keywords.pop('FORMAT')))
+    f.write(u'#%-12s: %s\u000D\u000A' % ('VERSION', keywords.pop('VERSION')))
+    for keyword, value in keywords.items():
+        f.write(u'#%-12s: %s\u000D\u000A' % (keyword, value))
+    
+    f.write(u'#%-12s: Spectral Data Starts Here\u000D\u000A' % 'SPECTRUM')
+
+    if format == 'XY':        
+        for x,y in zip(signal.axes_manager.axes[0].axis, signal.data):
+            f.write("%g%s%g" % (x, separator, y))
+            f.write(u'\u000D\u000A')
     elif format == 'Y':
-        keywords['DATATYPE'] = 'Y'
-    keywords['XPERCHAN'] = spectrum.energyscale
-    keywords['OFFSET'] = spectrum.energyorigin
-    if write_microscope_parameters is True:
-        print "\nWarning: the microscope attributes will be written to the file"
-        print microscope
-        print
-        keywords['CONVANGLE'] = microscope.alpha
-        keywords['COLLANGLE'] = microscope.beta
-        keywords['BEAMKV'] = microscope.E0
-        keywords['PPPC'] = microscope.pppc
-        keywords['CORRFAC'] = microscope.correlation_factor
-    with open(filename, 'w') as spectrum_file:
-        spectrum_file.write(u'#%-12s: %s\u000D\u000A' % ('FORMAT', FORMAT))
-        spectrum_file.write(u'#%-12s: %s\u000D\u000A' % ('VERSION', VERSION))
-        for keyword, value in keywords.items():
-            spectrum_file.write(u'#%-12s: %s\u000D\u000A' % (keyword, value))
-        spectrum_file.write(u'#%-12s: Spectral Data Starts Here\u000D\u000A'
-                            % STARTDATA)
-        i = 0
-        ix = spectrum.coordinates.ix
-        iy = spectrum.coordinates.iy
-        if format == 'XY':        
-            for row in spectrum.data_cube[:,ix, iy]:
-                spectrum_file.write("%g%s%g" % (spectrum.energy_axis[i], 
-                                                separator, row))
-                spectrum_file.write(u'\u000D\u000A')
-                i += 1
-        elif format == 'Y':
-            for row in spectrum.data_cube[:, ix, iy]:
-                spectrum_file.write('%f%s' % (row, separator))
-                spectrum_file.write(u'\u000D\u000A')
-        
-        spectrum_file.write(u'#%-12s: End Of Data and File' % ENDDATA)
-        print "File '%s' saved." % filename
+        for y in signal.data:
+            f.write('%f%s' % (y, separator))
+            f.write(u'\u000D\u000A')
+    else:
+        raise ValueError('format must be one of: None, \'X\' or \'Y\'')
+
+    f.write(u'#%-12s: End Of Data and File' % 'ENDOFDATA')
+    f.close()   
+
