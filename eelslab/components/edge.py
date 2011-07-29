ec9cd64bbad8aa1401d5d965b80fef18eef184b5
Francisco de la Peña <frandelapena@gmail.com>
2011-07-29 17:44:44 +0100

    MOVE: the content of EffectiveAngle has been moved to components/edge.py

--- eelslab/components/edge.py
+++ eelslab/components/edge.py
@@ -30,7 +30,6 @@ from scipy.signal import cspline1d_eval
 from eelslab.defaults_parser import defaults
 from eelslab.component import Component
 from eelslab.microscope import microscope
-from eelslab.effectiveangle import EffectiveAngle
 from eelslab.edges_db import edges_dict
 from eelslab import messages
 
@@ -47,6 +46,49 @@ c = 2997.92458e8 #speed of light in m/s
 gamma = 1.0 + (e * microscope.E0) / (m0 * pow(c,2.0)) #dimensionless
 T = microscope.E0 * (1.0 + gamma) / (2.0 * pow(gamma, 2.0)) #in eV
 
+def EffectiveAngle(E0,E,alpha,beta):
+    """Calculates the effective collection angle
+    
+    Parameters
+    ----------
+    E0 : float
+        incident energy in eV
+    E : float
+        energy loss in eV
+    alpha : float
+        convergence angle in mrad
+    beta : float
+        collection angle in mrad
+        
+    Returns
+    -------
+    float : effective collection angle
+    
+    Notes
+    -----
+    Code from Egerton (SE) page 420
+    """	   
+    if alpha == 0:
+        return beta * 10**-3
+    E0=10.**-3*E0 # In KeV
+    E=float(E)
+    alpha=float(alpha)
+    beta=float(beta)
+    TGT=E0*(1. + E0/1022.)/(1.+E0/511.)
+    thetaE=E/TGT
+    A2=alpha*alpha*1e-6
+    B2=beta*beta*1e-6
+    T2=thetaE*thetaE*1e-6
+    eta1=math.sqrt((A2+B2+T2)**2-4.*A2*B2)-A2-B2-T2
+    eta2=2.*B2*math.log(0.5/T2*(math.sqrt((A2+T2-B2)**2+4.*B2*T2)+A2+T2-B2))
+    eta3=2.*A2*math.log(0.5/T2*(math.sqrt((B2+T2-A2)**2+4.*A2*T2)+B2+T2-A2))
+#    ETA=(eta1+eta2+eta3)/A2/math.log(4./T2)
+    F1=(eta1+eta2+eta3)/2/A2/math.log(1.+B2/T2)
+    F2=F1
+    if (alpha/beta)> 1 :
+        F2=F1*A2/B2
+    BSTAR=thetaE*math.sqrt(math.exp(F2*math.log(1.+B2/T2))-1.)
+    return BSTAR*10**-3 # In rad
 class Edge(Component):
     """ This class builds a single cross section edge.
     Currently it only supports cross sections from Gatan Digital Micrograph(c)
