3f22f5fa0e99610dae8083ac7d014eb7ad795230
Francisco de la Peña <frandelapena@gmail.com>
2011-08-26 16:40:43 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -34,7 +34,9 @@ import hyperspy.Release as Release
 if os.path.exists('build'):
     distutils.dir_util.remove_tree('build')
 
-install_req = ['scipy', 'ipython', 'matplotlib', 'numpy', 'mdp', 'netcdf','nose']
+install_req = ['scipy', 'ipython', 'matplotlib', 'numpy', 'mdp', 'netcdf', 
+'nose', 'traits', 'traitsui', 'h5py', 'nose',]
+# Add also open-cv and chaco?
 
 def are_we_building4windows():
     for arg in sys.argv:
