302f64d1101e18becb4c5e4327595009386c0e02
Francisco de la Peña <frandelapena@gmail.com>
2011-09-22 17:11:29 +0100

    BUG-FIX: the two_gaussians example was not working since we renamed origin to centre

--- examples/simple_simulations/two_gaussians.py
+++ examples/simple_simulations/two_gaussians.py
@@ -1,6 +1,7 @@
 """Creates a 2D hyperspectrum consisting of two gaussians and plots it.
 
-This example can serve as starting point to test other functionalities on the simulated hyperspectrum.
+This example can serve as starting point to test other functionalities on the 
+simulated hyperspectrum.
 
 """
 
@@ -28,8 +29,8 @@ m.append(gs1)
 # Set the sparameters
 gs1.sigma.value = 10
 # Make the center vary in the -5,5 range around 128
-gs1.origin.map['values'][:] = 256 + (np.random.random((32,32)) - 0.5) * 10
-gs1.origin.map['is_set'][:] = True
+gs1.centre.map['values'][:] = 256 + (np.random.random((32,32)) - 0.5) * 10
+gs1.centre.map['is_set'][:] = True
 
 # Make the area vary between 0 and 10000
 gs1.A.map['values'][:] = 10000 * np.random.random((32,32))
@@ -44,8 +45,8 @@ m.append(gs2)
 gs2.sigma.value = 20
 
 # Make the center vary in the -10,10 range around 768
-gs2.origin.map['values'][:] = 768 + (np.random.random((32,32)) - 0.5) * 20
-gs2.origin.map['is_set'][:] = True
+gs2.centre.map['values'][:] = 768 + (np.random.random((32,32)) - 0.5) * 20
+gs2.centre.map['is_set'][:] = True
 
 # Make the area vary between 0 and 20000
 gs2.A.map['values'][:] = 20000 * np.random.random((32,32))
