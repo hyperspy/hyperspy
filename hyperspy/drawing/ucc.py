BUG-FIX: peak locations and characteristics were being converted to integers.  Was ruining subpixel precision and characteristic plotting.

hyperspy/drawing/ucc.py
@@ -500,7 +500,7 @@ class TemplatePicker(HasTraits):
             parent=self.sig
         else:
             parent=self.sig.mapped_parameters.original_files[self.titles[idx]]
-        positions=np.zeros((peaks.shape[0],1),dtype=[('filename','a256'),('id','i4'),('position','i4',(1,2))])
+        positions=np.zeros((peaks.shape[0],1),dtype=[('filename','a256'),('id','i4'),('position','f4',(1,2))])
         for i in xrange(peaks.shape[0]):
             # crop the cells from the given locations
             data[i,:,:]=self.sig.data[idx,peaks[i,1]:peaks[i,1]+tmp_sz,peaks[i,0]:peaks[i,0]+tmp_sz]

hyperspy/peak_char.py
@@ -471,11 +471,7 @@ def peak_attribs_stack(stack, peak_width, subpixel=True, target_locations=None,
                for j in xrange(target_locations.shape[0])])
 
     # pre-allocate result array.  7 rows for each peak, 1 column for each image
-    if imcoords:
-        # an extra 2 rows for keeping track of image coordinates
-        rlt=np.zeros((7*peak_locations.shape[0]+2,stack.shape[2]))
-    else:
-        rlt=np.zeros((7*peak_locations.shape[0],stack.shape[2]))
+    rlt=np.zeros((7*peak_locations.shape[0],stack.shape[2]))
     rlt_tmp=np.zeros((peak_locations.shape[0],5))
     for i in xrange(stack.shape[2]):
         rlt_tmp=peak_attribs_image(stack[i,:,:], 

hyperspy/signals/image.py
@@ -127,12 +127,12 @@ class Image(Signal):
         locations=self.mapped_parameters.target_locations
 
         if plot_shifts:
-            shifts=np.zeros((locations.shape[0]),dtype=[('location','i4',(1,2)),
-                                                        ('shift','i4',(1,2))])
+            shifts=np.zeros((locations.shape[0]),dtype=[('location','f4',(1,2)),
+                                                        ('shift','f4',(1,2))])
         else: shifts=None
         if plot_char:
-            char=np.zeros(locations.shape[0],dtype=[('location','i4',(1,2)),
-                                                          ('char','i4')])
+            char=np.zeros(locations.shape[0],dtype=[('location','f4',(1,2)),
+                                                          ('char','f4')])
         else: char=None
 
         for pos in xrange(locations.shape[0]):