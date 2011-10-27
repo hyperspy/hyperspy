f0ffb1ccef6541c75a80de80bdc6ab8d4fb0cfac
unknown <Francisco@ogre.msm.cam.ac.uk>
2011-10-27 10:06:11 +0100

    0.3.preview-1 release
    Added windows cross-compiling to the release_windows script
    Changed Release to 0.3.preview-1

--- hyperspy/Release.py
+++ hyperspy/Release.py
@@ -19,7 +19,7 @@
 
 name = 'hyperspy'
 
-version = '0.3.dev'
+version = '0.3.preview-1'
 
 description = "Hyperspectral data analysis toolbox"
 
--- release_windows.bat
+++ release_windows.bat
@@ -1,1 +1,2 @@
-python setup.py bdist_wininst --user-access-control auto --install-script win_post_installation.py
+python setup.py bdist_wininst  --plat-name=win32 --user-access-control auto --install-script win_post_installation.py
+python setup.py bdist_wininst  --plat-name=win-amd64 --user-access-control auto --install-script win_post_installation.py
