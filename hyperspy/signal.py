ad8a845aa46cbbd5288730a2af35fa7e489cd116
Francisco de la Peña <frandelapena@gmail.com>
2012-06-07 09:47:54 +0100

    BUG-FIX: Signal.split_in was transfering the axes information to the splitting signals

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -454,11 +454,14 @@ reconstruction created using either get_decomposition_model or get_bss_model met
             cut_node = range(0, rounded+step, step)
         else:
             cut_node = np.array([0] + steps).cumsum()
+        axes_dict = self.axes_manager._get_axes_dicts()
         for i in xrange(len(cut_node)-1):
+            axes_dict[axis]['offset'] = \
+                self.axes_manager.axes[axis].index2value(cut_node[i])
             data = self.data[
             (slice(None), ) * axis + (slice(cut_node[i], cut_node[i + 1]),
             Ellipsis)]
-            s = Signal({'data': data})
+            s = Signal({'data': data, 'axes' : axes_dict})
             # TODO: When copying plotting does not work
 #            s.axes = copy.deepcopy(self.axes_manager)
             s.get_dimensions_from_data()
