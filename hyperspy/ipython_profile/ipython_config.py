e8f3d113f7ffc7c9db2b78d641a76eeb3e775a52
Francisco de la Peña <frandelapena@gmail.com>
2012-07-13 09:54:44 +0100

    ipython_config.py contained duplicated lines

--- hyperspy/ipython_profile/ipython_config.py
+++ hyperspy/ipython_profile/ipython_config.py
@@ -2,13 +2,11 @@ from hyperspy import Release
 
 # Configuration file for ipython.
 c = get_config()
-
-
-c.TerminalIPythonApp.exec_lines = [
-    'from hyperspy.hspy import *']
-
 c.TerminalIPythonApp.ignore_old_config = True
-c.InteractiveShellApp.exec_lines = ['from hyperspy.hspy import *',
-                                    'print(Release.info)']
 c.TerminalInteractiveShell.banner2 = Release.info
+c.TerminalIPythonApp.exec_lines = ['from hyperspy.hspy import *']
+
+
+
+
 
