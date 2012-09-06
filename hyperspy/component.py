BUG-FIX: Fitting was failing after changing the fine_structure_width if the standard deviation was defined after a previous fit but is not defined in the next one

hyperspy/component.py
@@ -175,10 +175,17 @@ class Parameter(object):
             ('values','float', self._number_of_elements), 
             ('std', 'float', self._number_of_elements), 
             ('is_set', 'bool', 1)])
-        if self.map is None  or self.map.shape != shape or \
-        self.map.dtype != dtype_:
+        if (self.map is None  or self.map.shape != shape or 
+                    self.map.dtype != dtype_):
+            print("Updating")
             self.map = np.zeros(shape, dtype_)       
             self.map['std'][:] = np.nan
+            # TODO: in the future this class should have access to 
+            # axes manager and should be able to charge its own
+            # values. Until then, the next line is necessary to avoid
+            # erros when self.std is defined and the shape is different
+            # from the newly defined arrays
+            self.std = None
             
     def as_signal(self, field = 'values'):
         """Get a parameter map as a signal object.

hyperspy/components/eels_cl_edge.py
@@ -124,8 +124,6 @@ class EELSCLEdge(Component):
     def _set_fine_structure_width(self,arg):
         self.__fine_structure_width = arg
         self.setfine_structure_coeff()
-        # Force replot
-        self.intensity.value = self.intensity.value
     fine_structure_width = property(_get_fine_structure_width,_set_fine_structure_width)
     
     
@@ -173,13 +171,17 @@ class EELSCLEdge(Component):
     def setfine_structure_coeff(self):
         if self.energy_scale is None:
             return
-        self.fine_structure_coeff._number_of_elements = \
-        int(round(self.knots_factor * self.fine_structure_width / self.energy_scale)) + 4        
-        self.fine_structure_coeff.bmin, self.fine_structure_coeff.bmax = None, None
-        self.fine_structure_coeff.value = np.zeros(self.fine_structure_coeff._number_of_elements).tolist()
+        self.fine_structure_coeff._number_of_elements = int(
+            round(self.knots_factor * self.fine_structure_width / 
+                self.energy_scale)) + 4        
+        self.fine_structure_coeff.bmin = None
+        self.fine_structure_coeff.bmax = None
+        self.fine_structure_coeff.value = np.zeros(
+            self.fine_structure_coeff._number_of_elements).tolist()
         self.calculate_knots()
         if self.fine_structure_coeff.map is not None:
-            self.fine_structure_coeff.create_array(self.fine_structure_coeff.map.shape)
+            self.fine_structure_coeff.create_array(
+                self.fine_structure_coeff.map.shape)
             
     def set_microscope_parameters(self, E0, alpha, beta, energy_scale):
         """