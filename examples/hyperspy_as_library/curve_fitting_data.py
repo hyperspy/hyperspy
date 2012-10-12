e84882b6ae8718356be077f4ef7a73b7a7d2d64c
magnunor <magnus.nord@ntnu.no>
2012-10-12 14:21:30 -0500

    Edit curve_fitting_data.py and add example spectrums for it.

--- examples/hyperspy_as_library/curve_fitting_data.py
+++ examples/hyperspy_as_library/curve_fitting_data.py
@@ -1,17 +1,20 @@
-""" Loads hyperspy as a regular python library, loads spectrums from files, does curve fitting, and exports the results as hdf5 files"""
+""" Loads hyperspy as a regular python library, loads spectrums from files, does curve fitting, and plotting the model and original spectrum to a png file"""
 
 import hyperspy.hspy as hspy
-import numpy as np
 import matplotlib.pyplot as plt
 
-coreLossSpectrumFileName = "test.dm3"
+coreLossSpectrumFileName = "coreloss_spectrum.msa"
+lowlossSpectrumFileName = "lowloss_spectrum.msa"
 
 s = hspy.load(coreLossSpectrumFileName).to_EELS()
-s.add_elements(("Ti","O","Mn"))
-s.set_microscope_parameters(beam_energy=200, convergence_angle=20.0, collection_angle=10.0)
+s.add_elements(("Mn","O"))
+s.set_microscope_parameters(beam_energy=300, convergence_angle=24.6, collection_angle=13.6)
 
-m = hspy.create_model(s)
+ll = hspy.load(lowlossSpectrumFileName).to_EELS()
+
+m = hspy.create_model(s, ll=ll)
 m.enable_fine_structure()
 m.multifit(kind="smart")
+m.plot()
 
-m.export_results(format="hdf5")
+plt.savefig("model_original_spectrum_plot.png")
