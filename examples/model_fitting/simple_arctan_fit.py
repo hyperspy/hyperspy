d5bee58b89a65e12b2a1e3f01c7b9a0eb308f4ae
magnunor <magnus.nord@ntnu.no>
2012-10-26 11:26:53 -0500

    Modify variable names in simple_arctan_fit.py

--- examples/model_fitting/simple_arctan_fit.py
+++ examples/model_fitting/simple_arctan_fit.py
@@ -4,26 +4,26 @@ import numpy as np
 
 #Generate the data and make the spectrum
 x_range = np.array(range(-500,500))
-arctanSpectrum = signals.SpectrumSimulation({"data":np.arctan(x_range)})
-arctanSpectrum.axes_manager.axes[0].offset = -500
-arctanSpectrum.axes_manager.axes[0].units = ""
-arctanSpectrum.axes_manager.axes[0].name = "x"
-arctanSpectrum.mapped_parameters.title = "Simple arctan fit"
+s = signals.SpectrumSimulation({"data":np.arctan(x_range)})
+s.axes_manager.axes[0].offset = -500
+s.axes_manager.axes[0].units = ""
+s.axes_manager.axes[0].name = "x"
+s.mapped_parameters.title = "Simple arctan fit"
 			
-arctanSpectrum.add_gaussian_noise(0.1)
+s.add_gaussian_noise(0.1)
 
 #Make the arctan component for use in the model
 arctanComponent = components.Arctan()
 
 #Create the model and add the arctan component
-arctanModel = create_model(arctanSpectrum)
-arctanModel.append(arctanComponent)
+m = create_model(s)
+m.append(arctanComponent)
 
 #Fit the arctan component to the spectrum
-arctanModel.fit()
+m.fit()
 
 # Print the result of the fit
-arctanModel.print_current_values()
+m.print_current_values()
 
 #Plot the spectrum and the model fitting
-arctanModel.plot()
+m.plot()
