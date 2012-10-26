7c4e259ecd3a0b33f91c10897f1721a78500b135
Francisco de la Peña <fjd29@cam.ac.uk>
2012-10-26 07:32:19 +0100

    Added x-axis properties and gaussian noise and fixed a small typo

--- examples/model_fitting/simple_arctan_fit.py
+++ examples/model_fitting/simple_arctan_fit.py
@@ -4,7 +4,13 @@ import numpy as np
 
 #Generate the data and make the spectrum
 x_range = np.array(range(-500,500))
-arctanSpectrum = signals.Spectrum({"data":np.arctan(x_range})
+arctanSpectrum = signals.SpectrumSimulation({"data":np.arctan(x_range)})
+arctanSpectrum.axes_manager.axes[0].offset = -500
+arctanSpectrum.axes_manager.axes[0].units = ""
+arctanSpectrum.axes_manager.axes[0].name = "x"
+arctanSpectrum.mapped_parameters.title = "Simple arctan fit"
+			
+arctanSpectrum.add_gaussian_noise(0.1)
 
 #Make the arctan component for use in the model
 arctanComponent = components.Arctan()
@@ -15,6 +21,9 @@ arctanModel.append(arctanComponent)
 
 #Fit the arctan component to the spectrum
 arctanModel.fit()
+
+# Print the result of the fit
+arctanModel.print_current_values()
 
 #Plot the spectrum and the model fitting
 arctanModel.plot()
