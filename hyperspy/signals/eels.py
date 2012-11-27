BUG-FIX: Add padding to fourier-log and fourier-ratio deconvolution to fix the wrap-around problem and increase the efficiency of the fft calculation by changing the size of the spectra to a multiple of 2. Also replace the ffts by real ffts.

hyperspy/signals/eels.py
@@ -315,8 +315,7 @@ class EELSSpectrum(Spectrum):
         Parameters
         ----------
         zlp : EELSSpectrum
-            The corresponding zero-loss peak. Note that 
-            it must have exactly the same shape as the current spectrum.
+            The corresponding zero-loss peak.
         add_zlp : bool
             If True, adds the ZLP to the deconvolved spectrum
         
@@ -332,15 +331,35 @@ class EELSSpectrum(Spectrum):
         """
         s = self.deepcopy()
         s.hanning_taper()
+        zlp_size = zlp.axes_manager.signal_axes[0].size 
+        self_size = self.axes_manager.signal_axes[0].size
+        # Conservative new size to solve the wrap-around problem 
+        size = zlp_size + self_size -1
+        # Increase to the closest multiple of two to enhance the FFT 
+        # performance
+        size = int(2 ** np.ceil(np.log2(size)))
         axis = self.axes_manager.signal_axes[0]
-        z = np.fft.fft(zlp.data, axis=axis.index_in_array)
-        j = np.fft.fft(s.data, axis=axis.index_in_array)
-        j1 = z*np.log(j/z)
-        s.data = np.fft.ifft(j1, axis=axis.index_in_array).real
+        z = np.fft.rfft(zlp.data, n=size, axis=axis.index_in_array)
+        j = np.fft.rfft(s.data, n=size, axis=axis.index_in_array)
+        j1 = z * np.log(j / z)
+        sdata = np.fft.irfft(j1, axis=axis.index_in_array).real
+        def get_cropping_slice(axis, index):
+            cslice = [slice,] * len(self.axes_manager.axes)
+            cslice[axis] = slice(None, index)
+            return tuple(cslice)
+        s.data = sdata[s.axes_manager._get_data_slice(
+            [(axis.index_in_array, slice(None,self_size)),])]
         if add_zlp is True:
-            s.data += zlp.data
+            if self_size >= zlp_size:
+                s.data[s.axes_manager._get_data_slice(
+                    [(axis.index_in_array, slice(None,zlp_size)),])
+                    ] += zlp.data
+            else:
+                s.data += zlp.data[s.axes_manager._get_data_slice(
+                    [(axis.index_in_array, slice(None,self_size)),])]
+                    
         s.mapped_parameters.title = (s.mapped_parameters.title + 
-            ' after Fourier-log deconvolution')
+                                     ' after Fourier-log deconvolution')
         if s.tmp_parameters.has_item('filename'):
                 s.tmp_parameters.filename = (
                     self.tmp_parameters.filename +
@@ -348,7 +367,9 @@ class EELSSpectrum(Spectrum):
         return s
 
     def fourier_ratio_deconvolution(self, ll, fwhm=None,
-                                    threshold=None):
+                                    threshold=None,
+                                    extrapolate_lowloss=True,
+                                    extrapolate_coreloss=True):
         """Performs Fourier-ratio deconvolution.
         
         The core-loss should have the background removed. To reduce
@@ -358,8 +379,7 @@ class EELSSpectrum(Spectrum):
         Parameters
         ----------
         ll: EELSSpectrum
-            The corresponding low-loss (ll) EELSSpectrum. Note that 
-            it must have exactly the same shape as the current spectrum
+            The corresponding low-loss (ll) EELSSpectrum.
             
         fwhm : float or None
             Full-width half-maximum of the Gaussian function by which 
@@ -371,6 +391,8 @@ class EELSSpectrum(Spectrum):
             Truncation energy to estimate the intensity of the 
             elastic scattering. If None the threshold is taken as the
              first minimum after the ZLP centre.
+        extrapolate_lowloss, extrapolate_coreloss : bool
+            If True the signals are extrapolated using a power law,
             
         Notes
         -----        
@@ -378,14 +400,32 @@ class EELSSpectrum(Spectrum):
         Spectroscopy in the Electron Microscope. Springer-Verlag, 2011.
         
         """
+        orig_cl_size = self.axes_manager.signal_axes[0].size
+        if extrapolate_coreloss is True:
+            cl = self.power_law_extrapolation(
+                window_size=20,
+                extrapolation_size=100)
+        else:
+            cl = self.deepcopy()
+            
+        if extrapolate_lowloss is True:
+            ll = ll.power_law_extrapolation(
+                window_size=100,
+                extrapolation_size=100)
+        else:
+            ll = ll.deepcopy()
+        
+        ll.hanning_taper()
+        cl.hanning_taper()
 
-        ll = ll.power_law_extrapolation(
-            window_size=100,
-            extrapolation_size=1024)
-        cl = self.power_law_extrapolation(
-            window_size=20,
-            extrapolation_size=1024)
-
+        ll_size = ll.axes_manager.signal_axes[0].size 
+        cl_size = self.axes_manager.signal_axes[0].size
+        # Conservative new size to solve the wrap-around problem 
+        size = ll_size + cl_size -1
+        # Increase to the closest multiple of two to enhance the FFT 
+        # performance
+        size = int(2 ** np.ceil(np.log2(size)))
+        
         axis = ll.axes_manager.signal_axes[0]
         if fwhm is None:
             fwhm = ll.estimate_FWHM()['FWHM']
@@ -404,26 +444,26 @@ class EELSSpectrum(Spectrum):
         g.sigma.value = fwhm / 2.3548
         g.A.value = 1
         g.centre.value = 0
-        zl = g.function(axis.axis)
-        ll.hanning_taper()
-        cl.hanning_taper()
-        z = np.fft.fft(zl)
-        jk = np.fft.fft(cl.data, axis=axis.index_in_array)
-        jl = np.fft.fft(ll.data, axis=axis.index_in_array)
+        zl = g.function(
+                np.linspace(axis.offset,
+                            axis.offset + axis.scale * (size - 1),
+                            size))
+        z = np.fft.rfft(zl)
+        jk = np.fft.rfft(cl.data, n=size,axis=axis.index_in_array)
+        jl = np.fft.rfft(ll.data, n=size, axis=axis.index_in_array)
         zshape = [1,] * len(cl.data.shape)
-        zshape[axis.index_in_array] = axis.size
-        s = cl.deepcopy()
-        s.data = np.fft.ifft(z.reshape(zshape) * jk / jl,
-                             axis=axis.index_in_array).real
-        s.data *= I0
-        s.crop_in_pixels(-1,None,self.axes_manager.signal_axes[0].size)
-        s.mapped_parameters.title = (self.mapped_parameters.title + 
+        zshape[axis.index_in_array] = jk.shape[axis.index_in_array]
+        cl.data = np.fft.irfft(z.reshape(zshape) * jk / jl,
+                             axis=axis.index_in_array)
+        cl.data *= I0
+        cl.crop_in_pixels(-1,None,orig_cl_size)
+        cl.mapped_parameters.title = (self.mapped_parameters.title + 
             ' after Fourier-ratio deconvolution')
-        if s.tmp_parameters.has_item('filename'):
-                s.tmp_parameters.filename = (
+        if cl.tmp_parameters.has_item('filename'):
+                cl.tmp_parameters.filename = (
                     self.tmp_parameters.filename +
                     'after_fourier_ratio_deconvolution')
-        return s
+        return cl
             
     def richardson_lucy_deconvolution(self,  psf, iterations=15, 
                                       mask=None):