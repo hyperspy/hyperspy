2f3eef614f8a4f5c2522ae702a65cb49e49feb7b
magnunor <magnus.nord@ntnu.no>
2013-01-29 14:07:28 +0100

    Add estimate_parameters to the voigt component
    Basically copied the estimate_parameters from the gaussian
    component

--- hyperspy/components/voigt.py
+++ hyperspy/components/voigt.py
@@ -21,6 +21,8 @@ import math
 
 from hyperspy.component import Component
 
+sqrt2pi = math.sqrt(2*math.pi)
+
 def voigt(x, FWHM=1, gamma=1, center=0, scale=1): 
     """ 
     Voigt peak. 
@@ -136,3 +138,87 @@ class Voigt(Component):
         else:
             return f
 
+    def estimate_parameters(self, signal, E1, E2, only_current = False):
+        """Estimate the voigt function by calculating the momenta the gaussian.
+
+        Parameters
+        ----------
+        signal : Signal instance
+        x1 : float
+            Defines the left limit of the spectral range to use for the 
+            estimation.
+        x2 : float
+            Defines the right limit of the spectral range to use for the 
+            estimation.
+            
+        only_current : bool
+            If False estimates the parameters for the full dataset.
+            
+        Returns
+        -------
+        bool
+        
+        Notes
+        -----
+        Adapted from http://www.scipy.org/Cookbook/FittingData
+        
+        Example
+        -------
+        
+        import numpy as np
+        from hyperspy.hspy import *
+        from hyperspy.signals.spectrum import Spectrum  
+        g = components.Gaussian()
+        x = np.arange(-10,10, 0.01)
+        data = np.zeros((32,32,2000))
+        data[:] = g.function(x).reshape((1,1,2000))
+        s = Spectrum({'data' : data})
+        s.axes_manager.axes[-1].offset = -10
+        s.axes_manager.axes[-1].scale = 0.01
+        g.estimate_parameters(s, -10,10, False)
+            
+        """
+        axis = signal.axes_manager.signal_axes[0]
+        
+        energy2index = axis.value2index
+        i1 = energy2index(E1)
+        i2 = energy2index(E2)
+        X = axis.axis[i1:i2]
+        if only_current is True:
+            data = signal()[i1:i2]
+            X_shape = (len(X),)
+            i = 0
+            center_shape = (1,)
+        else:
+            # TODO: write the rest of the code to estimate the parameters of 
+            # the full dataset
+            i = axis.index_in_array 
+            data_gi = [slice(None),] * len(signal.data.shape)
+            data_gi[axis.index_in_array] = slice(i1,i2)
+            data = signal.data[data_gi]
+            X_shape = [1,] * len(signal.data.shape)
+            X_shape[axis.index_in_array] = data.shape[i]
+            center_shape = list(data.shape)
+            center_shape[i] = 1
+        
+        center = np.sum(X.reshape(X_shape) * data, i
+            ) / np.sum(data, i)
+        
+        sigma = np.sqrt(np.abs(np.sum((X.reshape(X_shape) - center.reshape(
+        center_shape)) ** 2 * data, i) / np.sum(data, i)))
+        height = data.max(i)
+        if only_current is True:
+            self.centre.value = center
+            self.FWHM.value = sigma*2.3548200450309493
+            self.area.value = height * sigma * sqrt2pi
+            return True
+        else:
+            if self.area.map is None:
+                self.create_arrays(signal.axes_manager.navigation_shape)
+            self.area.map['values'][:] = height * sigma * sqrt2pi
+            self.area.map['is_set'][:] = True
+            self.FWHM.map['values'][:] = sigma*2.3548200450309493
+            self.FWHM.map['is_set'][:] = True
+            self.centre.map['values'][:] = center
+            self.centre.map['is_set'][:] = True
+            return True
