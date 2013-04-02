eb36038609b143fd69bc067473178b71c9fdbec9
magnunor <magnus.nord@ntnu.no>
2013-04-02 14:37:14 +0200

    Improvements to fit_component docstring

--- hyperspy/model.py
+++ hyperspy/model.py
@@ -1195,8 +1195,8 @@ class Model(list):
     def disable_adjust_position(self, components=None, fix_them=True):
         """Disables the interactive adjust position feature
         
-        See also:
-        ---------
+        See also
+        --------
         enable_adjust_position
         
         """
@@ -1212,31 +1212,29 @@ class Model(list):
             estimate_parameters=True, fit_independent=False, **kwargs):
         """Fit just the given component in the given signal range.
 
-
         This method is useful to obtain starting parameters for the 
         components. Any keyword arguments are passed to the fit method.
 
-
         Parameters
         ----------
-        component: component instance
+        component : component instance
             The component must be in the model, otherwise an exception 
             is raised.
-        signal_range: {'interactive', (left_value, right_value), None}
+        signal_range : {'interactive', (left_value, right_value), None}
             If 'interactive' the signal range is selected using the span
              selector on the spectrum plot. The signal range can also 
              be manually specified by passing a tuple of floats. If None
               the current signal range is used.
-        estimate_parameters: bool, default True
+        estimate_parameters : bool, default True
             If True will check if the component has an 
             estimate_parameters function, and use it to estimate the
             parameters in the component.
-        fit_independent: bool, default False
+        fit_independent : bool, default False
             Fits component independently of the other components in the
             model.
 
-        Example
-        -------
+        Examples
+        --------
         Signal range set interactivly
         >>> g1 = components.Gaussian()
         >>> m.append(g1)
