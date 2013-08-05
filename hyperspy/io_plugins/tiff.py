963d167615dd60db81069fbcc74f95036eb461ef
Francisco de la Peña <frandelapena@gmail.com>
2013-08-05 18:08:21 +0100

    Add reading and writing RGB tiff files and improve. In addition the axes names and tags of tiff files are now read where possible

--- hyperspy/io_plugins/tiff.py
+++ hyperspy/io_plugins/tiff.py
@@ -16,12 +16,14 @@
 # You should have received a copy of the GNU General Public License
 # along with  Hyperspy.  If not, see <http://www.gnu.org/licenses/>.
 
+import warnings
 
 import numpy as np
-import warnings
+import traits.api as t
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
-    from hyperspy.misc.io.tifffile import imsave, imread
+    from hyperspy.misc.io.tifffile import imsave, TiffFile 
+from hyperspy.misc import rgb_tools
 
 # Plugin characteristics
 # ----------------------
@@ -36,17 +38,42 @@ default_extension = 0 # tif
 writes = [(2,0), (2,1)]
 # ----------------------
 
+axes_label_codes = {
+        'X' : "width",
+        'Y' : "height",
+        'S' : "sample",
+        'P' : "plane",
+        'I' : "image series",         
+        'Z' : "depth",
+        'C' : "color|em-wavelength|channel",
+        'E' : "ex-wavelength|lambda",   
+        'T' : "time",
+        'R' : "region|tile",
+        'A' : "angle",
+        'F' : "phase",
+        'H' : "lifetime",
+        'L' : "exposure",
+        'V' : "event",
+        'Q' : t.Undefined,
+        '_' : t.Undefined}
 
-def file_writer(filename, signal, _rescale = True,  **kwds):
+def file_writer(filename, signal, **kwds):
     '''Writes data to tif using Christoph Gohlke's tifffile library
         
         Parameters
         ----------
         filename: str
         signal: a Signal instance
+
     '''
-    
-    imsave(filename, signal.data.squeeze(), **kwds)
+    data = signal.data
+    if signal.is_rgbx is True:
+        data = rgb_tools.rgbx2regular_array(data)
+    if description not in kwds:
+        if signal.mapped_parameters.title:
+            kwds['description'] = signal.mapped_parameters.title
+
+    imsave(filename, data, software="hyperspy", **kwds)
     
 def file_reader(filename, output_level=0, record_by='image',**kwds):
     '''Read data from tif files using Christoph Gohlke's tifffile
@@ -62,11 +89,30 @@ def file_reader(filename, output_level=0, record_by='image',**kwds):
         image.
     
     '''
-    dc = imread(filename, **kwds)
-    dt = 'image'    
-    return [{'data':dc, 
-             'mapped_parameters': { 'original_filename' : filename,
-                                    'record_by': dt,
-                                    'signal_type' : "",}
-             }]
+    with TiffFile(filename, **kwds) as tiff:
+        dc = tiff.asarray()
+        axes = tiff.series[0]['axes']
+        if tiff.is_rgb:
+            dc = rgb_tools.regular_array2rgbx(dc)
+            axes = axes[:-1]
+        op = {}
+        names =  [axes_label_codes[axis] for axis in axes]
+        axes=[{'size' : size,                                                                
+               'name' : unicode(name),                                          
+               #'scale': scales[i],                                                  
+               #'offset' : origins[i],                                               
+               #'units' : unicode(units[i]),
+               }
+              for size, name in zip(dc.shape, names)]
+        op = {}
+        for key, tag in tiff[0].tags.iteritems():
+            op[key] = tag.value
+        mp =  { 'original_filename' : filename,
+                'record_by': "image",
+                'signal_type' : "",}
+        return [{'data' : dc, 
+                 'axes' : axes,
+                 'original_parameters' : op,
+                 'mapped_parameters' : mp,
+                 }]
 
