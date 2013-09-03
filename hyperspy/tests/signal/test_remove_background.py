9870f385224469e742fed61924904d926247aa5e
Francisco de la Peña <frandelapena@gmail.com>
2013-09-03 16:46:31 +0100

    Enable float/index axis indexing and None (to mean beginning or end of axis) in estimate_parameters. Also some small corrections to the test

--- hyperspy/tests/signal/test_remove_background.py
+++ hyperspy/tests/signal/test_remove_background.py
@@ -1,34 +1,22 @@
 import numpy as np
-import copy
 from nose.tools import (
-    assert_true,
-    assert_equal,
-    assert_not_equal,
-    raises)
+    assert_true,)
 
-from hyperspy.signal import Signal
 from hyperspy import signals
 from hyperspy import components
 
-class Test2D:
+class TestRemoveBackground1D:
     def setUp(self):
-        self.xaxis = np.arange(0,10000,0.01) 
-
-        self.gaussian = components.Gaussian()
-        self.gaussian.A.value = 10000
-        self.gaussian.centre.value = 5000
-        self.gaussian.sigma.value = 1000
+        gaussian = components.Gaussian()
+        gaussian.A.value = 10
+        gaussian.centre.value = 10
+        gaussian.sigma.value = 1
+        self.signal = signals.Signal(
+                                    gaussian.function(np.arange(0,20,0.01)))
+        self.signal.axes_manager[0].scale = 0.01
 
     def test_background_remove_gaussian(self):
-        gaussian = copy.deepcopy(self.gaussian)
-
-        signal = signals.Signal(gaussian.function(self.xaxis))
-        s1 = signal.remove_background(
-                signal_range=(200000,800000), 
+        s1 = self.signal.remove_background(
+                signal_range=(None, None), 
                 background_type='Gaussian')
-        self.s1 = s1
-        assert_true(
-                np.allclose(
-                    s1.data,
-                    np.zeros(len(s1.data)),
-                    atol=0.00001*self.gaussian.A.value))
+        assert_true(np.allclose(s1.data, np.zeros(len(s1.data))))
