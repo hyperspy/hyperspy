244bb1956c695c497c2c9f936b0cb236838790f4
Francisco de la Peña <frandelapena@gmail.com>
2013-09-04 12:31:56 +0100

    Small corrections to the test

--- hyperspy/tests/signal/test_integrate_in_range.py
+++ hyperspy/tests/signal/test_integrate_in_range.py
@@ -9,14 +9,11 @@ class Test1D():
         gaussian = Gaussian() 
         gaussian.A.value = 20
         gaussian.sigma.value = 10
-        gaussian.centre.value = 100
-        self.signal = Signal(gaussian.function(np.arange(0,1000,0.01)))
+        gaussian.centre.value = 50
+        self.signal = Signal(gaussian.function(np.arange(0,100,0.01)))
+        self.signal.axes_manager[0].scale = 0.01
     
     def test_integrate_in_range(self):
-        integrated_signal = self.signal.integrate_in_range(signal_range=(0,100000))
-        assert_true(
-                np.allclose(
-                    integrated_signal.data,
-                    2000,
-                    rtol=0.0000001))
+        integrated_signal = self.signal.integrate_in_range(signal_range=(None,None))
+        assert_true(np.allclose(integrated_signal.data, 20,))
                     
