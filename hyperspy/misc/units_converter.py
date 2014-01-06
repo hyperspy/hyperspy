92b05ef41ea42a4d61a34ac6b3445a7847d45293
Francisco de la Peña <frandelapena@gmail.com>
2014-01-06 12:24:14 +0000

    Some small style and typo corrections

--- hyperspy/misc/units_converter.py
+++ hyperspy/misc/units_converter.py
@@ -1,49 +1,56 @@
 from hyperspy.misc.eds.elements import elements as elements_db
 
-def weigth_to_atomic(elements,compo_wt):
-    """Convert weigth percent in atomic percent
-    
+
+def weight_to_atomic(elements, compo_wt):
+    """Convert weight percent (wt%) to atomic percent (at.%).
+
     Parameters
     ----------
     elements: list of str
         A list of element abbreviations, e.g. ['Al','Zn']
-        
+
     compo_wt: list of float
         The weight fractions (composition) of the sample.
-        
+
     Returns
-    -------    
-    The atomic fractions (composition)
+    -------
+    compo_at : list
+        Composition in atomic percent.
+
     """
+
     tot = 0
     for i, element in enumerate(elements):
-        tot = tot + compo_wt[i]/elements_db[element]['A']
+        tot = tot + compo_wt[i] / elements_db[element]['A']
     compo_at = []
     for i, element in enumerate(elements):
-        compo_at.append(compo_wt[i]/elements_db[element]['A']/tot)
-        
+        compo_at.append(compo_wt[i] / elements_db[element]['A'] / tot)
+
     return compo_at
-    
-def atomic_to_weight(elements,compo_at):
-    """Convert atomic percent in weigth percent
-    
+
+
+def atomic_to_weight(elements, compo_at):
+    """Convert atomic percent to weight percent.
+
     Parameters
     ----------
     elements: list of str
         A list of element abbreviations, e.g. ['Al','Zn']
-        
+
     compo_at: list of float
         The atomic fractions (composition) of the sample.
-        
+
     Returns
-    -------    
-    The weight fractions (composition)
+    -------
+    compo_wt : composition in weight percent.
+
     """
+
     tot = 0
     for i, element in enumerate(elements):
-        tot = tot + compo_at[i]*elements_db[element]['A']
+        tot = tot + compo_at[i] * elements_db[element]['A']
     compo_wt = []
     for i, element in enumerate(elements):
-        compo_wt.append(compo_at[i]*elements_db[element]['A']/tot)
-        
+        compo_wt.append(compo_at[i] * elements_db[element]['A'] / tot)
+
     return compo_wt
--- hyperspy/utils/eds/__init__.py
+++ hyperspy/utils/eds/__init__.py
@@ -23,149 +23,145 @@ from hyperspy.misc.eds import utils as utils_eds
 from hyperspy.misc.eds.elements import elements as elements_db
 import hyperspy.misc.units_converter as units_converter
 
-def xray_range(Xray_line,beam_energy,rho='auto'):
-    '''Return the Anderson-Hasler X-ray range. 
-    
+
+def xray_range(xray_line, beam_energy, rho='auto'):
+    '''Return the Anderson-Hasler X-ray range.
+
     Return the maximum range of X-ray generation in a pure bulk material.
-    
+
     Parameters
     ----------
-    Xray_line: str
+    xray_line: str
         The X-ray line, e.g. 'Al_Ka'
-        
     beam_energy: float
         The energy of the beam in kV.
-        
-    rho: float | 'auto'
-        The density of the material in g/cm3. If 'auto', the density 
+    rho: {float, 'auto'}
+        The density of the material in g/cm3. If 'auto', the density
         of the pure element is used.
-    
+
     Returns
     -------
     X-ray range in micrometer.
-    
+
     Notes
     -----
     From Anderson, C.A. and M.F. Hasler (1966). In proceedings of the
     4th international conference on X-ray optics and microanalysis.
-    
+
     See also the textbook of Goldstein et al., Plenum publisher,
     third edition p 286
-    
+
     '''
-    element, line = utils_eds._get_element_and_line(Xray_line)
+
+    element, line = utils_eds._get_element_and_line(xray_line)
     if rho == 'auto':
         rho = elements_db[element]['density']
     Xray_energy = elements_db[element]['Xray_energy'][line]
-    
-    return 0.064/rho*(np.power(beam_energy,1.68)-
-        np.power(Xray_energy,1.68))
-        
-def electron_range(element,beam_energy,rho='auto',tilt=0):
-    '''Return the Kanaya-Okayama electron range 
-    
+
+    return 0.064 / rho * (np.power(beam_energy, 1.68) -
+                          np.power(Xray_energy, 1.68))
+
+
+def electron_range(element, beam_energy, rho='auto', tilt=0):
+    '''Return the Kanaya-Okayama electron range.
+
     Return the maximum electron range in a pure bulk material.
-    
+
     Parameters
     ----------
     element: str
-        The abbreviation of the element, e.g. 'Al'
-        
+        The element symbol, e.g. 'Al'.
     beam_energy: float
-        The energy of the beam in kV.
-        
-    rho: float | 'auto'
-        The density of the material in g/cm3. If 'auto', the density of 
+        The energy of the beam in keV.
+    rho: {float, 'auto'}
+        The density of the material in g/cm3. If 'auto', the density of
         the pure element is used.
-        
-    tilt: float (degree)
-        the tilt of the sample.
-        
+    tilt: float.
+        The tilt of the sample in degrees.
+
     Returns
     -------
-    Electron range in micrometer.
-    
+    Electron range in micrometers.
+
     Notes
     -----
     From Kanaya, K. and S. Okayama (1972). J. Phys. D. Appl. Phys. 5, p43
-    
+
     See also the textbook of Goldstein et al., Plenum publisher,
-    third edition p 72
+    third edition p 72.
+
     '''
 
     if rho == 'auto':
         rho = elements_db[element]['density']
     Z = elements_db[element]['Z']
     A = elements_db[element]['A']
-    
-    return (0.0276*A/np.power(Z,0.89)/rho*
-        np.power(beam_energy,1.67)*math.cos(math.radians(tilt)))
+
+    return (0.0276 * A / np.power(Z, 0.89) / rho *
+            np.power(beam_energy, 1.67) * math.cos(math.radians(tilt)))
 
 
-def density_from_composition(elements,compositions,compo_unit='at'):
-    """Return the density from the sample composition
-    
+def density_from_composition(elements, compositions, compo_unit='at'):
+    """Return the density from the sample composition.
+
     Parameters
     ----------
     elements: list of str
         A list of element abbreviations, e.g. ['Al','Zn']
-        
-    Compositions: list of float
+    compositions: list of float
         The atomic composition of the sample e.g. [0.2,0.8]. The composition
         is normalized.
-        
+
     Returns
     -------
-    
-    The density in g/cm3
-    
+    density: The density in g/cm3.
+
     """
-    
-    if compo_unit=='at':
-        weights = units_converter.atomic_to_weight(elements,compositions)
-    elif compo_unit=='wt':
-        weights = np.array(compositions)/float(sum(compositions))
+
+    if compo_unit == 'at':
+        weights = units_converter.atomic_to_weight(elements, compositions)
+    elif compo_unit == 'wt':
+        weights = np.array(compositions) / float(sum(compositions))
     density = 0
     for i, element in enumerate(elements):
-        density = density + elements_db[element]['density']*weights[i]
+        density = density + elements_db[element]['density'] * weights[i]
 
     return density
 
+
 def take_off_angle(tilt_stage,
-    azimuth_angle,
-    elevation_angle):
+                   azimuth_angle,
+                   elevation_angle):
     """Calculate the take-off-angle (TOA).
-    
-    TOA is the angle with which the X-rays leave the surface towards 
-    the detector. 
+
+    TOA is the angle with which the X-rays leave the surface towards
+    the detector.
 
     Parameters
     ----------
-    tilt_stage: float (Degree)
-        The tilt of the stage. The sample is facing the detector when
-        positively tilted. 
+    tilt_stage: float
+        The tilt of the stage in degrees. The sample is facing the detector when
+        positively tilted.
+    azimuth_angle: float
+        The azimuth of the detector in degrees. 0 is perpendicular to the tilt
+        axis.
+    elevation_angle: float
+        The elevation of the detector in degrees.
 
-    azimuth_angle: float (Degree)
-        The azimuth of the detector. 0 is perpendicular to the tilt 
-        axis. 
-
-    elevation_angle: float (Degree)
-        The elevation of the detector.
-                
     Returns
     -------
-    take_off_angle: float (Degree)
-    
+    take_off_angle: float.
+        In degrees.
+
     Notes
     -----
     Defined by M. Schaffer et al., Ultramicroscopy 107(8), pp 587-597 (2007)
-    
-    """        
 
-        
-    a = math.radians(90+tilt_stage)
+    """
+
+    a = math.radians(90 + tilt_stage)
     b = math.radians(azimuth_angle)
     c = math.radians(elevation_angle)
-    
-    return math.degrees( np.arcsin (-math.cos(a)*math.cos(b)*math.cos(c) \
-    + math.sin(a)*math.sin(c)))
+
+    return math.degrees(np.arcsin(-math.cos(a) * math.cos(b) * math.cos(c)
+                                  + math.sin(a) * math.sin(c)))
