a7be475dbe7c40b46ed84a83c37865d5a5a1fd7c
Francisco de la Peña <frandelapena@gmail.com>
2014-01-07 14:22:34 +0000

    Split test_xrange in test_xrange and test_electron_range, and avoid using density_of_mixture

--- hyperspy/tests/signal/test_eds_sem.py
+++ hyperspy/tests/signal/test_eds_sem.py
@@ -26,9 +26,10 @@ from hyperspy import utils
 
 
 class Test_mapped_parameters:
+
     def setUp(self):
         # Create an empty spectrum
-        s = EDSSEMSpectrum(np.ones((4,2,1024)))
+        s = EDSSEMSpectrum(np.ones((4, 2, 1024)))
         s.axes_manager.signal_axes[0].scale = 1e-3
         s.axes_manager.signal_axes[0].units = "keV"
         s.axes_manager.signal_axes[0].name = "Energy"
@@ -42,24 +43,24 @@ class Test_mapped_parameters:
     def test_sum_live_time(self):
         s = self.signal
         sSum = s.sum(0)
-        assert_equal(sSum.mapped_parameters.SEM.EDS.live_time, 3.1*2)
+        assert_equal(sSum.mapped_parameters.SEM.EDS.live_time, 3.1 * 2)
 
     def test_rebin_live_time(self):
         s = self.signal
         dim = s.axes_manager.shape
-        s = s.rebin([dim[0]/2,dim[1]/2,dim[2]])
-        assert_equal(s.mapped_parameters.SEM.EDS.live_time, 3.1*2*2)
+        s = s.rebin([dim[0] / 2, dim[1] / 2, dim[2]])
+        assert_equal(s.mapped_parameters.SEM.EDS.live_time, 3.1 * 2 * 2)
 
     def test_add_elements(self):
         s = self.signal
-        s.add_elements(['Al','Ni'])
-        assert_equal(s.mapped_parameters.Sample.elements, ['Al','Ni'])
-        s.add_elements(['Al','Ni'])
-        assert_equal(s.mapped_parameters.Sample.elements, ['Al','Ni'])
-        s.add_elements(["Fe",])
-        assert_equal(s.mapped_parameters.Sample.elements, ['Al',"Fe", 'Ni'])
-        s.set_elements(['Al','Ni'])
-        assert_equal(s.mapped_parameters.Sample.elements, ['Al','Ni'])
+        s.add_elements(['Al', 'Ni'])
+        assert_equal(s.mapped_parameters.Sample.elements, ['Al', 'Ni'])
+        s.add_elements(['Al', 'Ni'])
+        assert_equal(s.mapped_parameters.Sample.elements, ['Al', 'Ni'])
+        s.add_elements(["Fe", ])
+        assert_equal(s.mapped_parameters.Sample.elements, ['Al', "Fe", 'Ni'])
+        s.set_elements(['Al', 'Ni'])
+        assert_equal(s.mapped_parameters.Sample.elements, ['Al', 'Ni'])
 
     def test_add_lines(self):
         s = self.signal
@@ -69,13 +70,13 @@ class Test_mapped_parameters:
         assert_equal(s.mapped_parameters.Sample.Xray_lines, ["Fe_Ln"])
         s.add_lines(("Fe_Ln",))
         assert_equal(s.mapped_parameters.Sample.Xray_lines, ["Fe_Ln"])
-        s.add_elements(["Ti",])
+        s.add_elements(["Ti", ])
         s.add_lines(())
         assert_equal(s.mapped_parameters.Sample.Xray_lines, ['Fe_Ln', 'Ti_La'])
         s.set_lines((), only_one=False, only_lines=False)
         assert_equal(s.mapped_parameters.Sample.Xray_lines,
                      ['Fe_La', 'Fe_Lb3', 'Fe_Ll', 'Fe_Ln', 'Ti_La',
-                     'Ti_Lb3', 'Ti_Ll', 'Ti_Ln'])
+                      'Ti_Lb3', 'Ti_Ll', 'Ti_Ln'])
         s.mapped_parameters.SEM.beam_energy = 0.4
         s.set_lines((), only_one=False, only_lines=False)
         assert_equal(s.mapped_parameters.Sample.Xray_lines, ['Ti_Ll'])
@@ -92,10 +93,10 @@ class Test_mapped_parameters:
         s = self.signal
         mp = s.mapped_parameters
         assert_equal(mp.SEM.EDS.energy_resolution_MnKa,
-            preferences.EDS.eds_mn_ka)
+                     preferences.EDS.eds_mn_ka)
 
     def test_SEM_to_TEM(self):
-        s = self.signal[0,0]
+        s = self.signal[0, 0]
         signal_type = 'EDS_TEM'
         mp = s.mapped_parameters
         mp.SEM.EDS.energy_resolution_MnKa = 125.3
@@ -106,7 +107,7 @@ class Test_mapped_parameters:
         results.append(signal_type)
         resultsTEM = [mpTEM.TEM.EDS.energy_resolution_MnKa]
         resultsTEM.append(mpTEM.signal_type)
-        assert_equal(results,resultsTEM )
+        assert_equal(results, resultsTEM)
 
     def test_get_calibration_from(self):
         s = self.signal
@@ -116,17 +117,18 @@ class Test_mapped_parameters:
         energy_axis.offset = -0.10
         s.get_calibration_from(scalib)
         assert_equal(s.axes_manager.signal_axes[0].scale,
-            energy_axis.scale)
+                     energy_axis.scale)
 
     def test_take_off_angle(self):
         s = self.signal
-        assert_equal(s.get_take_off_angle(),12.886929785732487)
+        assert_equal(s.get_take_off_angle(), 12.886929785732487)
 
 
 class Test_get_intentisity_map:
+
     def setUp(self):
         # Create an empty spectrum
-        s = EDSSEMSpectrum(np.zeros((2,2,3,100)))
+        s = EDSSEMSpectrum(np.zeros((2, 2, 3, 100)))
         energy_axis = s.axes_manager.signal_axes[0]
         energy_axis.scale = 0.04
         energy_axis.units = 'keV'
@@ -144,39 +146,45 @@ class Test_get_intentisity_map:
         sAl = s.get_lines_intensity(["Al_Ka"],
                                     plot_result=False,
                                     integration_window_factor=5)[0]
-        assert_true(np.allclose(1, sAl.data[0,0,0], atol=1e-3))
+        assert_true(np.allclose(1, sAl.data[0, 0, 0], atol=1e-3))
         sAl = s[0].get_lines_intensity(["Al_Ka"],
-                                    plot_result=False,
-                                    integration_window_factor=5)[0]
-        assert_true(np.allclose(1, sAl.data[0,0], atol=1e-3))
-        sAl = s[0,0].get_lines_intensity(["Al_Ka"],
-                                    plot_result=False,
-                                    integration_window_factor=5)[0]
+                                       plot_result=False,
+                                       integration_window_factor=5)[0]
+        assert_true(np.allclose(1, sAl.data[0, 0], atol=1e-3))
+        sAl = s[0, 0].get_lines_intensity(["Al_Ka"],
+                                          plot_result=False,
+                                          integration_window_factor=5)[0]
         assert_true(np.allclose(1, sAl.data[0], atol=1e-3))
-        sAl = s[0,0,0].get_lines_intensity(["Al_Ka"],
-                                    plot_result=False,
-                                    integration_window_factor=5)[0]
+        sAl = s[0, 0, 0].get_lines_intensity(["Al_Ka"],
+                                             plot_result=False,
+                                             integration_window_factor=5)[0]
         assert_true(np.allclose(1, sAl.data, atol=1e-3))
 
+
 class Test_tools_bulk:
+
     def setUp(self):
         s = EDSSEMSpectrum(np.ones(1024))
         s.mapped_parameters.SEM.beam_energy = 5.0
-        s.set_elements(['Al','Zn'])
+        s.set_elements(['Al', 'Zn'])
         s.add_lines()
         self.signal = s
-    def test_range(self):
+
+    def test_electron_range(self):
         s = self.signal
         mp = s.mapped_parameters
-        elec_range = utils.eds.electron_range(mp.Sample.elements[0],
-            mp.SEM.beam_energy,density='auto',tilt=mp.SEM.tilt_stage)
-        assert_equal(elec_range,0.41350651162374225)
+        elec_range = utils.eds.electron_range(
+            mp.Sample.elements[0],
+            mp.SEM.beam_energy,
+            density='auto',
+            tilt=mp.SEM.tilt_stage)
+        assert_equal(elec_range, 0.41350651162374225)
 
-        density = utils.material.density_of_mixture(
-            mp.Sample.elements,[0.8,0.2])
-        xr_range = utils.eds.xray_range(mp.Sample.Xray_lines[0],
-            mp.SEM.beam_energy,density=density)
-        assert_equal(xr_range,0.19002078834049554)
-
-
-
+    def test_xray_range(self):
+        s = self.signal
+        mp = s.mapped_parameters
+        xr_range = utils.eds.xray_range(
+            mp.Sample.Xray_lines[0],
+            mp.SEM.beam_energy,
+            density=4.37499648818)
+        assert_equal(xr_range, 0.19002078834050035)
