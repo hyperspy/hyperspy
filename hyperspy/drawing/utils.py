9b8b42f1b3a70379582fb9885b789d486cd26bb4
Francisco de la Peña <frandelapena@gmail.com>
2014-01-08 14:02:46 +0000

    Style corrections.

--- hyperspy/drawing/utils.py
+++ hyperspy/drawing/utils.py
@@ -21,26 +21,27 @@ import copy
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-                
+
+
 def create_figure(window_title=None,
                   _on_figure_window_close=None,
                   **kwargs):
     """Create a matplotlib figure.
-    
-    This function adds the possibility to execute another function 
+
+    This function adds the possibility to execute another function
     when the figure is closed and to easily set the window title. Any
     keyword argument is passed to the plt.figure function
-    
+
     Parameters
     ----------
     window_title : string
     _on_figure_window_close : function
-    
+
     Returns
     -------
-    
-    fig : plt.figure    
-    
+
+    fig : plt.figure
+
     """
     fig = plt.figure(**kwargs)
     if window_title is not None:
@@ -48,13 +49,14 @@ def create_figure(window_title=None,
     if _on_figure_window_close is not None:
         on_figure_window_close(fig, _on_figure_window_close)
     return fig
-                
+
+
 def on_figure_window_close(figure, function):
-    """Connects a close figure signal to a given function
-    
+    """Connects a close figure signal to a given function.
+
     Parameters
     ----------
-    
+
     figure : mpl figure instance
     function : function
     """
@@ -67,7 +69,7 @@ def on_figure_window_close(figure, function):
         figure._on_window_close = list()
     if function not in figure._on_window_close:
         figure._on_window_close.append(function)
-    
+
     if backend == 'GTKAgg':
         def function_wrapper(*args):
                 function()
@@ -77,13 +79,14 @@ def on_figure_window_close(figure, function):
         # In linux the following code produces a segmentation fault
         # so it is enabled only for Windows
         import wx
+
         def function_wrapper(event):
             # When using WX window.connect does not supports multiple funtions
             for f in figure._on_window_close:
                 f()
             plt.close(figure)
         window.Bind(wx.EVT_CLOSE, function_wrapper)
-        
+
     elif backend == 'TkAgg':
         def function_wrapper(*args):
             # When using TK window.connect does not supports multiple funtions
@@ -95,57 +98,60 @@ def on_figure_window_close(figure, function):
         # PyQt
         # In PyQt window.connect supports multiple funtions
         from IPython.external.qt_for_kernel import QtCore
-        window.connect(window,QtCore.SIGNAL('closing()'), function)
+        window.connect(window, QtCore.SIGNAL('closing()'), function)
     else:
         raise AttributeError("The %s backend is not supported. " % backend)
 
-def plot_RGB_map(im_list, normalization = 'single', dont_plot = False):
-    """Plots 2 or 3 maps in RGB
-    
+
+def plot_RGB_map(im_list, normalization='single', dont_plot=False):
+    """Plot 2 or 3 maps in RGB.
+
     Parameters
     ----------
     im_list : list of Image instances
     normalization : {'single', 'global'}
     dont_plot : bool
-    
+
     Returns
     -------
     array: RGB matrix
     """
 #    from widgets import cursors
-    height,width = im_list[0].data.shape[:2]
-    rgb = np.zeros((height, width,3))
-    rgb[:,:,0] = im_list[0].data.squeeze()
-    rgb[:,:,1] = im_list[1].data.squeeze()
+    height, width = im_list[0].data.shape[:2]
+    rgb = np.zeros((height, width, 3))
+    rgb[:, :, 0] = im_list[0].data.squeeze()
+    rgb[:, :, 1] = im_list[1].data.squeeze()
     if len(im_list) == 3:
-        rgb[:,:,2] = im_list[2].data.squeeze()
+        rgb[:, :, 2] = im_list[2].data.squeeze()
     if normalization == 'single':
         for i in xrange(rgb.shape[2]):
-            rgb[:,:,i] /= rgb[:,:,i].max()
+            rgb[:, :, i] /= rgb[:,:, i].max()
     elif normalization == 'global':
         rgb /= rgb.max()
-    rgb = rgb.clip(0,rgb.max())
+    rgb = rgb.clip(0, rgb.max())
     if not dont_plot:
         figure = plt.figure()
         ax = figure.add_subplot(111)
         ax.frameon = False
         ax.set_axis_off()
-        ax.imshow(rgb, interpolation = 'nearest')
+        ax.imshow(rgb, interpolation='nearest')
 #        cursors.add_axes(ax)
         figure.canvas.draw()
     else:
         return rgb
-        
+
+
 def subplot_parameters(fig):
-    """Returns a list of the subplot paramters of a mpl figure
-    
+    """Returns a list of the subplot parameters of a mpl figure.
+
     Parameters
     ----------
     fig : mpl figure
-    
+
     Returns
     -------
     tuple : (left, bottom, right, top, wspace, hspace)
+
     """
     wspace = fig.subplotpars.wspace
     hspace = fig.subplotpars.hspace
@@ -154,16 +160,20 @@ def subplot_parameters(fig):
     top = fig.subplotpars.top
     bottom = fig.subplotpars.bottom
     return (left, bottom, right, top, wspace, hspace)
-    
+
+
 class ColorCycle():
-    _color_cycle = [mpl.colors.colorConverter.to_rgba(color) for color 
-        in ('b', 'g', 'r', 'c', 'm', 'y', 'k')]
+    _color_cycle = [mpl.colors.colorConverter.to_rgba(color) for color
+                    in ('b', 'g', 'r', 'c', 'm', 'y', 'k')]
+
     def __init__(self):
         self.color_cycle = copy.copy(self._color_cycle)
+
     def __call__(self):
         if not self.color_cycle:
             self.color_cycle = copy.copy(self._color_cycle)
         return self.color_cycle.pop(0)
+
 
 def _make_heatmap_subplot(spectra, ax):
     x_axis = spectra.axes_manager.signal_axes[0]
@@ -188,13 +198,14 @@ def _make_heatmap_subplot(spectra, ax):
     ax.set_xlabel(x_axis.units)
     return(ax)
 
+
 def _make_cascade_subplot(spectra, ax, color='red', reverse_yaxis=False):
     navigation_length = spectra.axes_manager.navigation_size
     if isinstance(color, str):
         if navigation_length == 0:
-            color_array = [color] 
+            color_array = [color]
         else:
-            color_array = [color]*navigation_length
+            color_array = [color] * navigation_length
     else:
         color_array = color
     if spectra.axes_manager.navigation_size == 0:
@@ -202,7 +213,7 @@ def _make_cascade_subplot(spectra, ax, color='red', reverse_yaxis=False):
         data = spectra.data
         ax.plot(x_axis.axis, data, color=color_array[0])
     else:
-        max_value = 0 
+        max_value = 0
         for spectrum in spectra:
             spectrum_max_value = spectrum.data.max()
             if spectrum_max_value > max_value:
@@ -214,7 +225,7 @@ def _make_cascade_subplot(spectra, ax, color='red', reverse_yaxis=False):
             spectra_data = spectra.data
         for spectrum_index, spectrum_data in enumerate(spectra_data):
             x_axis = spectrum.axes_manager.signal_axes[0]
-            data_to_plot = (spectrum_data/float(max_value) + 
+            data_to_plot = (spectrum_data / float(max_value) +
                             y_axis.axis[spectrum_index])
             ax.plot(x_axis.axis, data_to_plot,
                     color=color_array[spectrum_index])
@@ -224,6 +235,7 @@ def _make_cascade_subplot(spectra, ax, color='red', reverse_yaxis=False):
     ax.set_xlabel(x_axis.units)
     return(ax)
 
+
 def _make_mosaic_subplot(spectrum, ax, color='red'):
     x_axis = spectrum.axes_manager.signal_axes[0]
     data = spectrum.data
@@ -231,17 +243,20 @@ def _make_mosaic_subplot(spectrum, ax, color='red'):
     ax.set_xlim(x_axis.low_value, x_axis.high_value)
     ax.set_xlabel(x_axis.units)
     return(ax)
-    
+
+
 def plot_spectra(
-    spectra, 
-    style='cascade', 
-    color='red',
-    reverse_yaxis=False):
-    """Parameters
-    -----------------
+        spectra,
+        style='cascade',
+        color='red',
+        reverse_yaxis=False):
+    """Plot several spectral in the same figure.
+
+    Parameters
+    ----------
     spectra : iterable
-        Ordered spectra list to plot. If style is cascade it is 
-        possible to supply several lists of spectra of the same 
+        Ordered spectra list to plot. If style is cascade it is
+        possible to supply several lists of spectra of the same
         lenght to plot multiple spectra in the same axes.
     style : {'cascade', 'mosaic', 'multiple_files', 'heatmap'}
         The style of the plot. multiple_files will plot every spectra
@@ -251,18 +266,18 @@ def plot_spectra(
         If list of strings: the list must be the same length as the
         navigation length of the spectra to be plotted. Default is red
     reverse_yaxis : bool, optional
-        Reverse the plotting direction of the navigational axis for 
+        Reverse the plotting direction of the navigational axis for
         cascade style plotting.
 
     Returns
-    -----------
+    -------
     fig: Matplotlib figure
-    
+
     """
     navigation_length = spectra.axes_manager.navigation_size
     if isinstance(color, str):
         if navigation_length == 0:
-            color_array = [color] 
+            color_array = [color]
         else:
             color_array = [color] * navigation_length
     else:
@@ -276,7 +291,7 @@ def plot_spectra(
                               reverse_yaxis=reverse_yaxis)
 
     elif style == 'mosaic':
-        #Need to find a way to automatically scale the figure size
+        # Need to find a way to automatically scale the figure size
         fig, subplots = plt.subplots(1, len(spectra))
         for ax, spectrum, color in zip(subplots, spectra, color_array):
             _make_mosaic_subplot(spectrum, ax, color=color)
@@ -284,6 +299,6 @@ def plot_spectra(
     elif style == 'heatmap':
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        _make_heatmap_subplot(spectra, ax) 
-        
+        _make_heatmap_subplot(spectra, ax)
+
     return fig
