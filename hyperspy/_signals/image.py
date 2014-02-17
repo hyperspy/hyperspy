d58ac2a2a49155d6d6b3e26e009358121e1ac5c0
pburdet <pb565@cam.ac.uk>
2014-02-17 16:16:27 +0000

    ENH: create plot isosurface

--- hyperspy/_signals/image.py
+++ hyperspy/_signals/image.py
@@ -17,6 +17,8 @@
 # along with  Hyperspy.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import numpy as np
+
 from hyperspy.signal import Signal
 
 class Image(Signal):
@@ -38,4 +40,74 @@ class Image(Signal):
 
         """
         return self.as_spectrum(0+3j)
+        
+        
+    def plot_3D_iso_surface(self,threshold,
+            color = 'auto',
+            figure='new',
+            scale='auto'):
+        """
+        Generate an iso-surface with Mayavi.
+        
+        Parameters
+        ----------
+            
+        threshold: float
+            Between 0 (min intensity) and 1 (max intensity).
+            If result == quant, 1 == 100%.
+        
+        color: list
+            The color of the surface, (R,G,B). If 'auto', automatically 
+            selected.
+            
+        figure: mayavi.core.scene.Scene 
+            If 'new', generate a new scene/figure.
+        
+        scale: str || list
+            If 'auto', scale with axes_manager.scale. Else, scale with 
+            the given list (x,y,z).            
+          
+        Return
+        ------
+        
+        figure: mayavi.core.scene.Scene
+        
+        src: mayavi.sources.array_source.ArraySource
+        
+        iso: mayavi.modules.iso_surface.IsoSurface        
+            
+        """
+        from mayavi import mlab        
+        
+        if figure=='new':
+            figure = mlab.figure()     
+            
+        img_res = self.deepcopy()
+        
+        img_data = img_res.data        
+        img_data = np.rollaxis(img_data,0,3)
+        img_data = np.rollaxis(img_data,0,2)
+        src = mlab.pipeline.scalar_field(img_data)
+        src.name = img_res.mapped_parameters.title        
+
+        
+        threshold = img_data.max()-threshold*img_data.ptp()
+        
+        if scale=='auto':
+            scale = [1/img_res.axes_manager[i].scale for i in [1,2,0]]
+            src.spacing= scale
+        else:
+            src.spacing = scale           
+        if color != 'auto':
+            iso = mlab.pipeline.iso_surface(src,
+                contours=[threshold, ],color =color)
+        else:
+           iso = mlab.pipeline.iso_surface(src,
+                contours=[threshold, ])
+            
+        iso.compute_normals = False
+        #if color != 'auto':
+         #   iso.actor.property.color = color
+        #iso.actor.property.opacity = 0.5        
+        return figure, src, iso
 
