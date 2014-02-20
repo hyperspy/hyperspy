52a5d4fa2f68be70b61212a7129c5dc6c18b047f
Francisco de la Peña <frandelapena@gmail.com>
2014-02-20 16:41:16 +0000

    Add ability to specify components by name and index to all methods that take components as arguments. Add tests, Model docstring and extra example to the User Guide.

--- hyperspy/tests/model/test_name_call_component.py
+++ hyperspy/tests/model/test_name_call_component.py
@@ -77,3 +77,52 @@ class TestModel:
         m = self.model
         g1 = Gaussian()
         m.extend((g1, g1))
+    def test_remove_component(self):
+        m = self.model
+        g1 = Gaussian()
+        m.append(g1)
+        m.remove(g1)
+        nose.tools.assert_equal(len(m), 0)
+    def test_remove_component_by_index(self):
+        m = self.model
+        g1 = Gaussian()
+        m.append(g1)
+        m.remove(0)
+        nose.tools.assert_equal(len(m), 0)
+    def test_remove_component_by_name(self):
+        m = self.model
+        g1 = Gaussian()
+        m.append(g1)
+        m.remove(g1.name)
+        nose.tools.assert_equal(len(m), 0)
+    def test_get_component_by_name(self):
+        m = self.model
+        g1 = Gaussian()
+        g2 = Gaussian()
+        g2.name = "test"
+        m.extend((g1, g2))
+        nose.tools.assert_is(m._get_component("test"), g2)
+    def test_get_component_by_index(self):
+        m = self.model
+        g1 = Gaussian()
+        g2 = Gaussian()
+        g2.name = "test"
+        m.extend((g1, g2))
+        nose.tools.assert_is(m._get_component(1), g2)
+
+    def test_get_component_by_component(self):
+        m = self.model
+        g1 = Gaussian()
+        g2 = Gaussian()
+        g2.name = "test"
+        m.extend((g1, g2))
+        nose.tools.assert_is(m._get_component(g2), g2)
+
+    @nose.tools.raises(ValueError)
+    def test_get_component_wrong(self):
+        m = self.model
+        g1 = Gaussian()
+        g2 = Gaussian()
+        g2.name = "test"
+        m.extend((g1, g2))
+        m._get_component(1.2)
