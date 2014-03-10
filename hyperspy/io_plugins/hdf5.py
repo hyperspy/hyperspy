9b30d99f18af352aa55c59955296574d9855079f
Francisco de la Peña <frandelapena@gmail.com>
2014-03-10 17:16:41 +0000

    Move TEM.dwell_time and TEM.exposure to TEM.Detector.EELS.

--- hyperspy/io_plugins/hdf5.py
+++ hyperspy/io_plugins/hdf5.py
@@ -218,6 +218,22 @@ def hdfgroup2signaldict(group):
                 "TEM"] = exp["metadata"]["TEM"]
             del exp["metadata"]["TEM"]
             if "EELS" in exp["metadata"]["Acquisition_instrument"]["TEM"]:
+                if "dwell_time" in exp["metadata"]["Acquisition_instrument"]["TEM"]:
+                    exp["metadata"]["Acquisition_instrument"]["TEM"]["EELS"]["dwell_time"] =\
+                        exp["metadata"]["Acquisition_instrument"]["TEM"]["dwell_time"]
+                    del exp["metadata"]["Acquisition_instrument"]["TEM"]["dwell_time"]
+                if "dwell_time_units" in exp["metadata"]["Acquisition_instrument"]["TEM"]:
+                    exp["metadata"]["Acquisition_instrument"]["TEM"]["EELS"]["dwell_time_units"] =\
+                        exp["metadata"]["Acquisition_instrument"]["TEM"]["dwell_time_units"]
+                    del exp["metadata"]["Acquisition_instrument"]["TEM"]["dwell_time_units"]
+                if "exposure" in exp["metadata"]["Acquisition_instrument"]["TEM"]:
+                    exp["metadata"]["Acquisition_instrument"]["TEM"]["EELS"]["exposure"] =\
+                        exp["metadata"]["Acquisition_instrument"]["TEM"]["exposure"]
+                    del exp["metadata"]["Acquisition_instrument"]["TEM"]["exposure"]
+                if "exposure_units" in exp["metadata"]["Acquisition_instrument"]["TEM"]:
+                    exp["metadata"]["Acquisition_instrument"]["TEM"]["EELS"]["exposure_units"] =\
+                        exp["metadata"]["Acquisition_instrument"]["TEM"]["exposure_units"]
+                    del exp["metadata"]["Acquisition_instrument"]["TEM"]["exposure_units"]
                 if "Detector" not in exp["metadata"]["Acquisition_instrument"]["TEM"]:
                     exp["metadata"]["Acquisition_instrument"][
                         "TEM"]["Detector"] = {}
