25a3a6f3a11555326923fe122c8b515fd1467feb
Francisco de la Peña <frandelapena@gmail.com>
2014-03-10 18:09:16 +0000

    Automatic style corrections courtesy of autopep8.

--- hyperspy/tests/signal/test_folding.py
+++ hyperspy/tests/signal/test_folding.py
@@ -4,8 +4,8 @@ import numpy as np
 from hyperspy.signal import Signal
 
 
-
 class TestSignalFolding:
+
     def setUp(self):
         self.s = Signal(np.empty((2, 3, 4, 5)))
         self.s.axes_manager.set_signal_dimension(2)
@@ -14,13 +14,13 @@ class TestSignalFolding:
         s = self.s.deepcopy()
         s.unfold_navigation_space()
         nose.tools.assert_equal(s.axes_manager.navigation_shape,
-                     (self.s.axes_manager.navigation_size,))
+                                (self.s.axes_manager.navigation_size,))
 
     def test_unfold_signal(self):
         s = self.s.deepcopy()
         s.unfold_signal_space()
         nose.tools.assert_equal(s.axes_manager.signal_shape,
-                     (self.s.axes_manager.signal_size,))
+                                (self.s.axes_manager.signal_size,))
 
     def test_unfolded_repr(self):
         self.s.unfold()
