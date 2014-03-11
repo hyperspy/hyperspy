30b9c958f69348afc8a7a9181cc41ec7b9bedfb4
Francisco de la Peña <frandelapena@gmail.com>
2014-03-11 09:34:21 +0000

    Rename metadata._Internal_parameters -> metadata._HyperSpy

--- hyperspy/learn/mva.py
+++ hyperspy/learn/mva.py
@@ -338,7 +338,7 @@ class MVA():
 
             if self._unfolded4decomposition is True:
                 folding = \
-                    self.metadata._Internal_parameters.Folding
+                    self.metadata._HyperSpy.Folding
                 target.original_shape = folding.original_shape
 
             # Reproject
