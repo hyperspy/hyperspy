84c64145f6bd7958cfe46952b227dfc3050d1e03
Francisco de la Peña <frandelapena@gmail.com>
2014-03-12 18:38:36 +0000

    Fix tests

--- hyperspy/tests/signal/test_remove_background.py
+++ hyperspy/tests/signal/test_remove_background.py
@@ -16,6 +16,8 @@ class TestRemoveBackground1D:
         self.signal = signals.Signal(
             gaussian.function(np.arange(0, 20, 0.01)))
         self.signal.axes_manager[0].scale = 0.01
+        self.signal.metadata.Signal.binned = False
+
 
     def test_background_remove_gaussian(self):
         s1 = self.signal.remove_background(
