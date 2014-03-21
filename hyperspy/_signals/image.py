e434deffb9ff55f3c347eca3a408df2045d27bde
pburdet <pb565@cam.ac.uk>
2014-03-21 17:26:30 +0000

    EHN: add function

--- hyperspy/_signals/image.py
+++ hyperspy/_signals/image.py
@@ -40,3 +40,45 @@ class Image(Signal):
 
         """
         return self.as_spectrum(0 + 3j)
+        
+    def plot_orthoview_animated(image):
+        """
+        Plot an orthogonal view of a 3D images
+
+        Parameters
+        ---------
+
+        image: signals.Image
+            An image in 3D.
+
+        isotropic_voxel:
+            If True, generate a new image, scaling z in order to obtain isotropic
+            voxel.
+        """
+        
+        im_xy = image.deepcopy()
+        im_xy.metadata.General.title = 'xy'
+        im_xy.axes_manager.set_signal_dimension(0)
+
+        im_xz = im_xy.deepcopy()
+        im_xz = im_xz.rollaxis(2, 1)
+        im_xz.metadata.General.title = 'xz'
+        im_xz.axes_manager.set_signal_dimension(0)
+
+        im_xz.axes_manager._axes[2] = im_xy.axes_manager._axes[2]
+        im_xz.axes_manager._axes[1] = im_xy.axes_manager._axes[0]
+        im_xz.axes_manager._axes[0] = im_xy.axes_manager._axes[1]
+
+        im_yz = im_xy.deepcopy()
+        im_yz = im_yz.rollaxis(0, 2)
+        im_yz = im_yz.rollaxis(1, 0)
+        im_yz.metadata.General.title = 'yz'
+        im_yz.axes_manager.set_signal_dimension(0)
+
+        im_yz.axes_manager._axes = im_xy.axes_manager._axes[::-1]
+
+        im_xz.axes_manager._update_attributes()
+        im_yz.axes_manager._update_attributes()
+        im_xy.plot()
+        im_xz.plot()
+        im_yz.plot()
