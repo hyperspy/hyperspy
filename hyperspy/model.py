4c7e734b9214784a8a7ece76dbfaae83e456a97d
Tomas Ostasevicius <to266@cam.ac.uk>
2014-03-28 15:18:00 +0000

    Automatic style corrections courtesy of autopep8

--- hyperspy/model.py
+++ hyperspy/model.py
@@ -224,7 +224,8 @@ class Model(list):
         self.channel_switches = np.array([True] * len(self.axis.axis))
 
         if 'free_parameters_boundaries' in dic:
-            self.free_parameters_boundaries = copy.deepcopy(dic['free_parameters_boundaries'])
+            self.free_parameters_boundaries = copy.deepcopy(
+                dic['free_parameters_boundaries'])
         else:
             self.free_parameters_boundaries = None
 
@@ -1800,7 +1801,7 @@ class Model(list):
                         _parameter.value = value
                         _parameter.assign_current_value_to_all()
 
-    def as_dictionary(self, indices = None):
+    def as_dictionary(self, indices=None):
         """Returns a dictionary of the model, including full Signal dictionary,
         all components and all values of their components, and twin functions.
 
@@ -1838,8 +1839,10 @@ class Model(list):
             else:
                 dic['low_loss'] = self._low_loss
         dic['components'] = [c.as_dictionary(indices) for c in self]
-        dic['free_parameters_boundaries'] = copy.deepcopy(self.free_parameters_boundaries)
+        dic['free_parameters_boundaries'] = copy.deepcopy(
+            self.free_parameters_boundaries)
         dic['convolved'] = self.convolved
+
         def remove_empty_numpy_strings(dic):
             for k, v in dic.iteritems():
                 if isinstance(v, dict):
@@ -1854,7 +1857,7 @@ class Model(list):
                     del dic[k]
                     dic[k] = ''
         remove_empty_numpy_strings(dic)
-                        
+
         return dic
 
     def __getitem__(self, value):
