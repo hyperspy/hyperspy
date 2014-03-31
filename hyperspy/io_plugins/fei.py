5e7950e9e81dfba3c9413dd2cc12232e24f61906
pburdet <pb565@cam.ac.uk>
2014-03-31 15:47:16 +0100

    Automatic style corrections courtesy of autopep8

--- hyperspy/io_plugins/fei.py
+++ hyperspy/io_plugins/fei.py
@@ -391,7 +391,6 @@ def ser_reader(filename, objects=None, verbose=False, *args, **kwds):
         axes[-1]['name'] = 'Energy'
         axes[-1]['scale'] *= 0.001
 
-
         array_shape.append(data['ArrayLength'][0])
 
     elif record_by == 'image':
