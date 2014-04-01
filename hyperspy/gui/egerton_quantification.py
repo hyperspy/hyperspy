1ffdd53b3848879cf5e5c42efe40737c684cba43
vidartf <vidartf@gmail.com>
2014-04-01 19:15:05 +0200

    Small fix to remove_background() change

--- hyperspy/gui/egerton_quantification.py
+++ hyperspy/gui/egerton_quantification.py
@@ -142,7 +142,7 @@ class BackgroundRemoval(SpanSelectorInSpectrum):
         return to_return
 
     def span_selector_changed(self):
-        if (self.ss_left_value or self.ss_right_value) is np.nan:
+        if (self.ss_left_value is np.nan) or (self.ss_right_value is np.nan):
             return
         if self.background_estimator is None:
             print("No bg estimator")
