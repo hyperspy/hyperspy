4906b28bfc1369e64a72cf0fc989546663ba305a
Francisco de la Peña <frandelapena@gmail.com>
2014-04-01 10:06:28 +0100

    Automatic style corrections courtesy of autopep8.

--- hyperspy/tests/axes/test_data_axis.py
+++ hyperspy/tests/axes/test_data_axis.py
@@ -34,6 +34,7 @@ class TestDataAxis:
     @nose.tools.raises(ValueError)
     def test_value_range_to_indices_v1_greater_than_v2(self):
         self.axis.value_range_to_indices(2, 1)
+
     def test_deepcopy(self):
         ac = copy.deepcopy(self.axis)
         ac.offset = 100
@@ -43,4 +44,3 @@ class TestDataAxis:
         ac = copy.deepcopy(self.axis)
         ac.offset = 100
         nose.tools.assert_equal(ac.axis[0], ac.offset)
-
