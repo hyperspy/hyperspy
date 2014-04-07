ff41c97192e901edca6a11aa615ea11fbeff72e7
Francisco de la Peña <frandelapena@gmail.com>
2014-04-07 17:18:49 +0100

    BUG_FIX: Signal._assign_subclass was not changing the subclass when setting signal_origin due to a typo.

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -4343,12 +4343,15 @@ class Signal(MVA,
         mp = self.metadata
         current_class = self.__class__
         self.__class__ = hyperspy.io.assign_signal_subclass(
-            record_by=mp.Signal.record_by if "Signal.record_by" in mp
-            else self._record_by,
-            signal_type=mp.Signal.signal_type if "signal_type" in mp.Signal
-            else self._signal_type,
-            signal_origin=mp.Signal.signal_origin if "Signal.signal_origin" in mp.Signal
-            else self._signal_origin)
+            record_by=mp.Signal.record_by
+                if "Signal.record_by" in mp
+                else self._record_by,
+            signal_type=mp.Signal.signal_type
+                if "Signal.signal_type" in mp
+                else self._signal_type,
+            signal_origin=mp.Signal.signal_origin
+                if "Signal.signal_origin" in mp
+                else self._signal_origin)
         self.__init__(**self._to_dictionary())
 
     def set_signal_type(self, signal_type):
