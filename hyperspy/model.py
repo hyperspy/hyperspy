1d35456a73ff7312aa8667753bd91c40929fac2b
Tomas Ostasevicius <to266@cam.ac.uk>
2014-10-09 09:29:08 +0100

    Automatic style corrections courtesy of autopep8

--- hyperspy/model.py
+++ hyperspy/model.py
@@ -2367,7 +2367,7 @@ class Model(list):
                         p_new.free = p_orig.free
                         p_new.std = p_orig.std
                         p_new.ext_bounded = p_orig.ext_bounded
-                        p_new.ext_force_positive = p_orig.ext_force_positive                        
+                        p_new.ext_force_positive = p_orig.ext_force_positive
                         p_new.twin_function = p_orig.twin_function
                         p_new.twin_inverse_function = p_orig.twin_inverse_function
                         p_new.map = p_orig.map[array_slices[:-1]]
