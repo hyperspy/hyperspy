b3f4c7e1d05dbb72eec0e2ea28030be401516c2f
pburdet <pb565@cam.ac.uk>
2014-11-14 15:25:01 +0000

    EHN: multi dimenstion density

--- hyperspy/misc/material.py
+++ hyperspy/misc/material.py
@@ -18,11 +18,16 @@ def weight_to_atomic(elements, weight_percent):
     -------
     atomic_percent : array of float
         Composition in atomic percent.
-    
+
     Calculate the atomic percent of modern bronze given its weight percent:
     >>> utils.material.weight_to_atomic(("Cu", "Sn"), (88, 12))
+    array([ 93.19698614,   6.80301386])
 
     """
+    if len(elements) != len(weight_percent):
+        raise ValueError(
+            'The number of elements must match the size of the first axis'
+            'of weight_percent.')
     atomic_weights = np.array(
         [elements_db[element]['General_properties']['atomic_weight']
             for element in elements])
@@ -53,14 +58,18 @@ def atomic_to_weight(elements, atomic_percent):
     --------
 
     Calculate the weight percent of modern bronze given its atomic percent:
-    >>> utils.material.atomic_to_weight(("Cu", "Sn"), (88, 12))
+    >>> utils.material.atomic_to_weight(("Cu", "Sn"), [93.2, 6.8])
+    array([ 88.00501989,  11.99498011])
 
     """
-
+    if len(elements) != len(atomic_percent):
+        raise ValueError(
+            'The number of elements must match the size of the first axis'
+            'of atomic_percent.')
     atomic_weights = np.array(
         [elements_db[element]['General_properties']['atomic_weight']
             for element in elements])
-    weight_percent = np.array(map(np.divide, atomic_percent, atomic_weights))
+    weight_percent = np.array(map(np.multiply, atomic_percent, atomic_weights))
     sum_atomic = weight_percent.sum(axis=0)/100.
     for i, el in enumerate(elements):
         weight_percent[i] /= sum_atomic
@@ -91,11 +100,19 @@ def density_of_mixture_of_pure_elements(elements, weight_percent):
     --------
 
     Calculate the density of modern bronze given its weight percent:
-    >>> utils.material.density_of_mixture_of_pure_elements(("Cu", "Sn"), (88, 12))
+    >>> utils.material.density_of_mixture_of_pure_elements(("Cu", "Sn"),
+                                                           (88, 12))
     8.6903187973131466
 
     """
+    if len(elements) != len(weight_percent):
+        raise ValueError(
+            'The number of elements must match the size of the first axis'
+            'of weight_percent.')
     densities = np.array(
-        [elements_db[element]['Physical_properties']['density (g/cm^3)'] for element in elements])
-    density = (weight_percent / densities / sum(weight_percent)).sum() ** -1
-    return density
+        [elements_db[element]['Physical_properties']['density (g/cm^3)']
+            for element in elements])
+    sum_densities = np.zeros_like(weight_percent, dtype='float')
+    for i, weight in enumerate(weight_percent):
+        sum_densities[i] = weight / densities[i]
+    return np.sum(weight_percent, axis=0) / sum_densities.sum(axis=0)
