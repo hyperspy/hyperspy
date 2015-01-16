6647dc4d982612ccc5ba8c60e17de09aa9bbf3e0
vidartf <vidartf@gmail.com>
2015-01-16 18:46:03 +0100

    Implemented event suppression + tests
    Implemented event suppression through a boolean attribute on the Event
    itself, and a property on the Events container that temporarily
    suppresses all its Events when used with a 'with' statement.
    
    Also added tests to check to the event suppression behaves as expected.

--- hyperspy/events.py
+++ hyperspy/events.py
@@ -1,3 +1,32 @@
+import sys
+
+class EventSuppressionContext(object):
+    """
+    Context manager for event suppression. When passed an Events class,
+    it will suppress all the events in that container when activated by
+    using it in a 'with' statement. The previous suppression state will be 
+    restored when the 'with' block completes.
+    """
+    def __init__(self, events):
+        self.events = events
+        self.old = {}
+        
+    def __enter__(self):
+        self.old = {}
+        try:
+            for e in self.events.__dict__.itervalues():
+                self.old[e] = e.suppress
+                e.suppress = True
+        except e:
+            self.__exit__(*sys.exc_info())
+            raise
+        return self
+        
+    def __exit__(self, type, value, tb):
+        for e, oldval in self.old.iteritems():
+            e.suppress = oldval
+        # Never suppress events
+
 class Events(object):
     """
     Events container.
@@ -6,13 +35,27 @@ class Events(object):
 
     """
 
-    pass
+    @property
+    def suppress(self):
+        """
+        Use this property with a 'with' statement to temporarily suppress all
+        events in the container. When the 'with' vlock completes, the old 
+        suppression values will be restored.
+        
+        Example usage pattern:
+        with obj.events.suppress:
+            obj.val_a = a
+            obj.val_b = b
+        obj.events.values_changed.trigger()
+        """
+        return EventSuppressionContext(self)
 
 
 class Event(object):
 
     def __init__(self):
         self.connected = set()
+        self.suppress = False
 
     def connect(self, function):
         if not callable(function):
@@ -23,8 +66,9 @@ class Event(object):
         self.connected.remove(function)
 
     def trigger(self, *args, **kwargs):
-        for f in self.connected:
-            f(*args, **kwargs)
+        if not self.suppress:
+            for f in self.connected:
+                f(*args, **kwargs)
 
     def __deepcopy__(self, memo):
         dc = type(self)()
