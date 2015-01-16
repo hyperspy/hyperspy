4a68ffcdbbfc5d6331f3822177b9116c6a0ddb7c
Francisco de la Peña <frandelapena@gmail.com>
2015-01-16 15:55:10 +0000

    Remove signal registration from ROI.

--- hyperspy/roi.py
+++ hyperspy/roi.py
@@ -7,15 +7,9 @@ class RectangularROI(t.HasTraits):
 
     def __init__(self, top, bottom, left, right):
         super(RectangularROI, self).__init__()
-        self._signals = []
         self.events = Events()
         self.events.roi_changed = Event()
         self.top, self.bottom, self.left, self.right = top, bottom, left, right
-
-    def register_signals(self, signals):
-        for signal in signals:
-            self._signals.append((signal,
-                                 signal._deepcopy_with_new_data(signal.data)))
 
     def _top_changed(self, old, new):
         if self.bottom is not t.Undefined and new >= self.bottom:
@@ -42,24 +36,17 @@ class RectangularROI(t.HasTraits):
             self.update()
 
     def update(self):
-        if (not self._signals or
-                t.Undefined in (self.top, self.bottom, self.left, self.right)):
-            return
-        for signal, out in self._signals:
-            self(signal, out=out)
-            out.events.data_changed.trigger()
-        self.events.roi_changed.trigger()
+        if t.Undefined not in (self.top, self.bottom, self.left, self.right):
+            self.events.roi_changed.trigger()
 
-    def __call__(self, sig, register=False, out=None):
+    def __call__(self, signal, out=None):
         if out is None:
-            roi = sig[self.left:self.right, self.top:self.bottom]
-            if register:
-                self._signals.append((sig, roi))
+            roi = signal[self.left:self.right, self.top:self.bottom]
             return roi
         else:
-            sig.__getitem__((slice(self.left, self.right),
-                             slice(self.top, self.bottom)),
-                            out=out)
+            signal.__getitem__((slice(self.left, self.right),
+                                slice(self.top, self.bottom)),
+                               out=out)
 
     def __repr__(self):
         return "%s(top=%f, bottom=%f, left=%f, right=%f)" % (
