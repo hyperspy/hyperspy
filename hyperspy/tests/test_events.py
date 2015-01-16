589aff1a00c4d2eb2d49c85bc8207a09681acd37
vidartf <vidartf@gmail.com>
2015-01-16 18:46:03 +0100

    Implemented event suppression + tests
    Implemented event suppression through a boolean attribute on the Event
    itself, and a property on the Events container that temporarily
    suppresses all its Events when used with a 'with' statement.
    
    Also added tests to check to the event suppression behaves as expected.

---
+++ hyperspy/tests/test_events.py
@@ -0,0 +1,91 @@
+import nose.tools
+import hyperspy.events as he
+
+class TriggeredCorrectly: pass
+
+class TestEventsSuppression:
+        
+    def on_trigger(self, should_trigger):
+        if should_trigger:
+            raise TriggeredCorrectly()
+        else:
+            nose.tools.assert_true(False, "This event should not trigger")
+            
+    def trigger_check(self, trigger, should_trigger):
+        if should_trigger:
+            nose.tools.assert_raises(TriggeredCorrectly, trigger, True)
+        else:
+            trigger(False)
+        
+    def setUp(self):
+        self.events = he.Events()
+        
+    def reset(self):
+        self.events.a = he.Event()
+        self.events.b = he.Event()
+        self.events.c = he.Event()
+        
+        self.events.a.connect(self.on_trigger)
+        self.events.b.connect(self.on_trigger)
+        self.events.c.connect(self.on_trigger)
+        
+    def test_simple_suppression(self):
+        self.reset()
+        
+        self.events.a.suppress = True
+        self.trigger_check(self.events.a.trigger, False)
+        self.trigger_check(self.events.b.trigger, True)
+        self.events.a.suppress = False
+        
+        with self.events.suppress:
+            self.trigger_check(self.events.a.trigger, False)
+            self.trigger_check(self.events.b.trigger, False)
+            self.trigger_check(self.events.c.trigger, False)
+        
+        self.trigger_check(self.events.a.trigger, True)
+        self.trigger_check(self.events.b.trigger, True)
+        self.trigger_check(self.events.c.trigger, True)
+        
+    def test_suppression_restore(self):
+        self.reset()
+        
+        self.events.a.suppress = True
+        self.events.b.suppress = False
+        self.events.c.suppress = False
+        
+        with self.events.suppress:
+            self.trigger_check(self.events.a.trigger, False)
+            self.trigger_check(self.events.b.trigger, False)
+            self.trigger_check(self.events.c.trigger, False)
+            
+        self.trigger_check(self.events.a.trigger, False)
+        self.trigger_check(self.events.b.trigger, True)
+        self.trigger_check(self.events.c.trigger, True)
+        
+    def test_suppresion_nesting(self):
+        self.reset()
+        
+        self.events.a.suppress = True
+        self.events.b.suppress = False
+        self.events.c.suppress = False
+        
+        with self.events.suppress:
+            self.events.c.suppress = False
+            self.trigger_check(self.events.a.trigger, False)
+            self.trigger_check(self.events.b.trigger, False)
+            self.trigger_check(self.events.c.trigger, True)
+            
+            with self.events.suppress:
+                self.trigger_check(self.events.a.trigger, False)
+                self.trigger_check(self.events.b.trigger, False)
+                self.trigger_check(self.events.c.trigger, False)
+                
+            self.trigger_check(self.events.a.trigger, False)
+            self.trigger_check(self.events.b.trigger, False)
+            self.trigger_check(self.events.c.trigger, True)
+            
+        self.trigger_check(self.events.a.trigger, False)
+        self.trigger_check(self.events.b.trigger, True)
+        self.trigger_check(self.events.c.trigger, True)
+        
+        
