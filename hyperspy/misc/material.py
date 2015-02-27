2263096a1e0c0fef55b9520f8917c194e3251a51
pburdet <pb565@cam.ac.uk>
2015-02-27 11:44:27 +0000

    NEW: import from edslab

--- hyperspy/misc/material.py
+++ hyperspy/misc/material.py
@@ -84,3 +84,87 @@ def density_of_mixture_of_pure_elements(elements, weight_percent):
         [elements_db[element]['Physical_properties']['density (g/cm^3)'] for element in elements])
     density = (weight_percent / densities / sum(weight_percent)).sum() ** -1
     return density
+    
+def mass_absorption_coefficient(element, energies):
+    """
+    Get the mass absorption coefficient of a X-ray(s)
+
+    In a pure material for a Xray(s) of given energy(ies) or given name(s)
+
+    Parameters
+    ----------
+    element: str
+        The element symbol of the absorber, e.g. 'Al'.
+    energies: {float or list of float or str or list of str}
+        The energy or energies of the Xray in keV, or the name eg 'Al_Ka'
+
+    Return
+    ------
+    mass absorption coefficient(s) in cm^2/g
+    """
+
+    energies_db = np.array(ffast_mac[element].energies_keV)
+    macs = np.array(ffast_mac[element].mass_absorption_coefficient_cm2g)
+    if isinstance(energies, str):
+        energies = utils_eds._get_energy_xray_line(energies)
+    elif hasattr(energies, '__iter__'):
+        if isinstance(energies[0], str):
+            for i, energy in enumerate(energies):
+                energies[i] = utils_eds._get_energy_xray_line(energy)
+
+    index = np.searchsorted(energies_db, energies)
+
+    mac_res = np.exp(np.log(macs[index - 1])
+                     + np.log(macs[index] / macs[index - 1])
+                     * (np.log(energies / energies_db[index - 1])
+                     / np.log(energies_db[index] / energies_db[index - 1])))
+    return np.nan_to_num(mac_res)
+
+
+def mass_absorption_coefficient_of_mixture_of_pure_elements(elements,
+                                                            weight_fraction,
+                                                            energies):
+    """Calculate the mass absorption coefficients a mixture of elements.
+
+    A compund is a mixture of pure elements
+
+    Parameters
+    ----------
+    elements: list of str
+        The list of element symbol of the absorber, e.g. ['Al','Zn'].
+    weight_fraction: np.array
+        dim = {el,z,y,x} The fraction of elements in the sample by weight
+    energies: {float or list of float or str or list of str}
+        The energy or energies of the Xray in keV, or the name eg 'Al_Ka'
+
+    Examples
+    --------
+    >>> utils.material.mass_absorption_coefficient_of_mixture_of_pure_elements(
+    >>>     ['Al','Zn'],[0.5,0.5],'Al_Ka')
+
+    Return
+    ------
+    mass absorption coefficient(s) in cm^2/g
+
+    See also
+    --------
+    utils.material.mass_absorption_coefficient
+    """
+    if len(elements) != len(weight_fraction):
+        raise ValueError(
+            "Elements and weight_fraction should have the same lenght")
+
+    if hasattr(weight_fraction[0], '__iter__'):
+
+        weight_fraction = np.array(weight_fraction)
+        mac_res = np.zeros(weight_fraction.shape[1:])
+        for element, weight in zip(elements, weight_fraction):
+            mac_re = mass_absorption_coefficient(
+                element, energies)
+            mac_res += mac_re * weight
+        return mac_res
+    else:
+        mac_res = np.array([mass_absorption_coefficient(
+            el, energies) for el in elements])
+        mac_res = np.dot(weight_fraction, mac_res)
+        return mac_res
