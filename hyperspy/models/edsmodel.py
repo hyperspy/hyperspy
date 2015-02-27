a5c477f0f97ea6b1c24b6f6f112cc2d5a4a52f2f
pburdet <pb565@cam.ac.uk>
2015-02-27 16:42:36 +0000

    EHN: calibration

--- hyperspy/models/edsmodel.py
+++ hyperspy/models/edsmodel.py
@@ -19,7 +19,7 @@
 from __future__ import division
 
 import numpy as np
-# import math
+import math
 
 from hyperspy.model import Model
 from hyperspy._signals.eds import EDSSpectrum
@@ -41,6 +41,12 @@ def _get_iweight(element, line, weight_line=None):
         weight_line = elements_db[
             element]['Atomic_properties']['Xray_lines'][line]['weight']
     return lambda x: x / weight_line
+
+
+def _get_sigma(E, E_ref, units_factor):
+    # 2.5 from Goldstein, / 1000 eV->keV, / 2.355^2 for FWHM -> sigma
+    return lambda sig_ref: math.sqrt(abs(
+        4.5077 * 1e-4 * (E - E_ref) * units_factor + np.power(sig_ref, 2)))
 
 
 class EDSModel(Model):
@@ -271,3 +277,111 @@ class EDSModel(Model):
         self.reset_signal_range()
         self.enable_xray_lines()
         self.fix_background()
+        
+    def free_energy_resolution(self, xray_lines):
+        """
+        Free the energy resolution of the main X-ray lines
+
+        Resolutions of the different peak are twinned
+
+        """
+        if xray_lines == 'all_alpha':
+            xray_lines = [compo.name for compo in self.xray_lines]
+
+        for i, xray_line in enumerate(xray_lines):
+            component = self[xray_line]
+            if i == 0:
+                component_ref = component
+                component_ref.sigma.free = True
+                E_ref = component_ref.centre.value
+            else:
+                component.sigma.free = True
+                E = component.centre.value
+                component.sigma.twin_function = _get_sigma(
+                    E, E_ref, self.units_factor)
+                component.sigma.twin_inverse_function = _get_sigma(
+                    E_ref, E, self.units_factor)
+                component.sigma.twin = component_ref.sigma
+
+    def fix_energy_resolution(self, xray_lines):
+        """
+        Fix and remove twin of X-ray lines sigma
+        """
+
+        if xray_lines == 'all_alpha':
+            xray_lines = [compo.name for compo in self.xray_lines]
+        for i, xray_line in enumerate(xray_lines):
+            component = self[xray_line]
+            component.sigma.twin = None
+            component.sigma.free = False
+
+    def set_energy_resolution(self, xray_lines):
+        """
+        Set the fitted energy resolution to the spectrum and
+        adjust the FHWM for all lines
+        """
+        if xray_lines == 'all_alpha':
+            xray_lines = [compo.name for compo in self.xray_lines]
+        energy_Mn_Ka = self.spectrum._get_line_energy('Mn_Ka')
+        get_sigma_Mn_Ka = _get_sigma(
+            energy_Mn_Ka, self[xray_lines[0]].centre.value, self.units_factor)
+        FWHM_MnKa = get_sigma_Mn_Ka(self[xray_lines[0]].sigma.value
+                                    ) * 1000. / self.units_factor * 2.355
+        if FWHM_MnKa < 110:
+            print "FWHM_MnKa of " + str(FWHM_MnKa) + " smaller than " + \
+                "physically possible"
+        else:
+
+            self.spectrum.set_microscope_parameters(
+                energy_resolution_MnKa=FWHM_MnKa)
+            print 'FWHM_MnKa ' + str(FWHM_MnKa)
+
+            for component in self:
+                if component.isbackground is False:
+                    line_energy, line_FWHM = self.spectrum._get_line_energy(
+                        component.name, FWHM_MnKa='auto')
+                    component.sigma.value = line_FWHM / 2.355
+
+    def calibrate_energy_axis(self,
+                              calibrate='resolution',
+                              xray_lines='all_alpha',
+                              kind='single',
+                              spread_to_all_lines=True,
+                              **kwargs):
+        """
+        Fit the calibration
+
+        energy scaling of the spectrum
+
+        Parameters
+        ----------
+        calibrate: 'resolution' or 'scale' or 'offset'
+            If 'resolution', calibrate by fitting the peak width given by
+            `energy_resolution_MnKa` in `metdata`
+            If 'scale', calibrate the scale of the energy axes
+            If 'scale', calibrate the offset of the energy axes
+        xray_lines: {list of str | 'all_alpha'}
+            The Xray lines. If 'all_alpha', fit all using all alpha lines
+        kind : {'single', 'multi'}
+            If 'single' fit only the current location. If 'multi'
+            use multifit.
+        spread_to_all_lines: bool
+            if True, change the calibration value of the spectrum
+        **kwargs : extra key word arguments
+            All extra key word arguments are passed to fit or
+        multifit, depending on the value of kind.
+
+        """
+        if calibrate == 'resolution':
+            self.free_energy_resolution(xray_lines=xray_lines)
+            if kind == 'single':
+                self.fit(**kwargs)
+            if kind == 'multi':
+                self.multifit(**kwargs)
+            self.fix_energy_resolution(xray_lines=xray_lines)
+            if spread_to_all_lines:
+                self.set_energy_resolution(xray_lines=xray_lines)
+        elif calibrate == 'scale':
+            print 'not done yet'
+        elif calibrate == 'offset':
+            print 'not done yet'
