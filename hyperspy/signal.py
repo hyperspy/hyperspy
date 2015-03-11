BUGFIX: Variance now does not have its own axes_manager, instead shares the same one with the original signal As a consequence, now changing axes scale and/or offset is correctly reflected in the variance signal, allowing for correct slicing

hyperspy/signal.py
@@ -4301,8 +4301,8 @@ class Signal(MVA,
         variance = (dc * gain_factor + gain_offset) * correlation_factor
         # The lower bound of the variance is the gaussian noise.
         variance = np.clip(variance, gain_offset * correlation_factor, np.inf)
-        variance = type(self)(variance,
-                              axes=self.axes_manager._get_axes_dicts())
+        variance = type(self)(variance)
+        variance.axes_manager = self.axes_manager
         variance.metadata.General.title = ("Variance of " +
                                            self.metadata.General.title)
         self.metadata.set_item(