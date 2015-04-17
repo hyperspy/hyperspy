2c43724b792ef7a19b616064698d6939c7fad17e
vidartf <vidartf@gmail.com>
2015-04-17 19:31:26 +0200

    ENH: Order of FEI data
    I don't have a lot of FEI data files to test this on, but for me the FEI
    files where loaded with two out of three axes inverted. This fixes the
    problem for me, but test should be run with other files.

--- hyperspy/io_plugins/fei.py
+++ hyperspy/io_plugins/fei.py
@@ -425,7 +425,7 @@ def ser_reader(filename, objects=None, verbose=False, *args, **kwds):
 
     dc = dc.reshape(array_shape)
     if record_by == 'image':
-        dc = dc[::-1]
+        dc = dc[..., ::-1, :]
     if ordict:
         original_metadata = OrderedDict()
     else:
