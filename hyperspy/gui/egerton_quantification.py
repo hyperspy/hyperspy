Fix for power law scaling issue when removing background

hyperspy/gui/egerton_quantification.py
@@ -103,9 +103,10 @@ class BackgroundRemoval(SpanSelectorInSpectrum):
         self.bg_line.data_function = self.bg_to_plot
         self.bg_line.set_line_properties(
             color='blue',
-            type='line')
-        self.bg_line.autoscale = False
+            type='line',
+            scaley=False)
         self.signal._plot.signal_plot.add_line(self.bg_line)
+        self.bg_line.autoscale = False
         self.bg_line.plot()
 
     def bg_to_plot(self, axes_manager=None, fill_with=np.nan):