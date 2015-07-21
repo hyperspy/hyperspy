9488ee8f4a4ea6af43c4cee0a3846d966af5820e
Magnus <magnus.nord@ntnu.no>
2015-07-21 19:26:37 +0200

    Add test for loading dm-files created using stackbuilder plugin

---
+++ hyperspy/tests/io/test_dm_stackbuilder_plugin.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2007-2011 The HyperSpy developers
+#
+# This file is part of  HyperSpy.
+#
+#  HyperSpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+#  HyperSpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+
+from nose.tools import assert_equal
+from hyperspy.io import load
+
+my_path = os.path.dirname(__file__)
+
+class TestStackBuilder():
+    
+    def test_load_stackbuilder_imagestack(self):
+        image_stack = load(my_path + "/dm_stackbuilder_plugin/test_stackbuilder_imagestack.dm3")
+        data_dimensions = len(image_stack.data.shape)
+        axes_dimensions = len(image_stack.axes_manager.shape)
+        assert_equal(data_dimensions, axes_dimensions)
