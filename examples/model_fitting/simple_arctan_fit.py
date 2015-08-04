b1794c1ceceb671d35ed38237652b318a19be905
Tomas Ostasevicius <to266@cam.ac.uk>
2015-08-04 14:39:09 +0100

    examples updated

--- examples/model_fitting/simple_arctan_fit.py
+++ examples/model_fitting/simple_arctan_fit.py
@@ -1,9 +1,11 @@
 """Creates a spectrum, and fits an arctan to it."""
 
 import numpy as np
+import hyperspy.hspy as hs
+import matplotlib.pyplot as plt
 
 # Generate the data and make the spectrum
-s = signals.SpectrumSimulation(
+s = hs.signals.SpectrumSimulation(
     np.arctan(np.arange(-500, 500)))
 s.axes_manager[0].offset = -500
 s.axes_manager[0].units = ""
@@ -13,7 +15,7 @@ s.metadata.General.title = "Simple arctan fit"
 s.add_gaussian_noise(0.1)
 
 # Make the arctan component for use in the model
-arctan_component = components.Arctan()
+arctan_component = hs.components.Arctan()
 
 # Create the model and add the arctan component
 m = s.create_model()
