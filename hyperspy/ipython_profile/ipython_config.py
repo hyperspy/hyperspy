c83b8d7c646bc0f01359a387a9c46214b3aa54f2
Tomas Ostasevicius <to266@cam.ac.uk>
2015-08-05 10:42:29 +0100

    Automatic style corrections courtesy of autopep8

--- hyperspy/ipython_profile/ipython_config.py
+++ hyperspy/ipython_profile/ipython_config.py
@@ -3,6 +3,7 @@ from hyperspy import Release
 # Configuration file for ipython.
 c = get_config()
 c.TerminalIPythonApp.ignore_old_config = True
-c.TerminalInteractiveShell.banner2 = Release.info + "\nHyperspy is loaded in hs.* namespace\n"
+c.TerminalInteractiveShell.banner2 = Release.info + \
+    "\nHyperspy is loaded in hs.* namespace\n"
 
-c.InteractiveShellApp.exec_lines = [ 'import hyperspy.hspy as hs',]
+c.InteractiveShellApp.exec_lines = ['import hyperspy.hspy as hs', ]
