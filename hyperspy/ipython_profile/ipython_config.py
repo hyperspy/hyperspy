3a4cdf74b5df2a5d1aa05ab680e6076b9e405cba
Tomas Ostasevicius <to266@cam.ac.uk>
2015-08-05 15:35:52 +0100

    changed banner message

--- hyperspy/ipython_profile/ipython_config.py
+++ hyperspy/ipython_profile/ipython_config.py
@@ -4,8 +4,6 @@ from hyperspy import Release
 c = get_config()
 c.TerminalIPythonApp.ignore_old_config = True
 c.TerminalInteractiveShell.banner2 = Release.info + \
-    "\n----------------------------------------" + \
-    "\n! Hyperspy is loaded in hs.* namespace !\n" +\
-    "----------------------------------------"
+    "\n\n** Hyperspy is loaded in hs.* namespace **\n\n"
 
 c.InteractiveShellApp.exec_lines = ['import hyperspy.hspy as hs', ]
