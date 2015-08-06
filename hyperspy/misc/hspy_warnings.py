d8134f53ccf7358c6ae321d651fc9f8374702390
Francisco de la Peña <frandelapena@gmail.com>
2015-08-06 18:10:09 +0100

    Add forgotten file

---
+++ hyperspy/misc/hspy_warnings.py
@@ -0,0 +1,7 @@
+class VisibleDeprecationWarning(UserWarning):
+    """Visible deprecation warning.
+    By default, python will not show deprecation warnings, so this class
+    provides a visible one.
+
+    """
+    pass
