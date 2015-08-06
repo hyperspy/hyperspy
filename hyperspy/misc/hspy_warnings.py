3575e8e892de2ee6dc5e879d786f80fbdd7a93d9
Francisco de la Peña <frandelapena@gmail.com>
2015-08-06 18:10:09 +0100

    Automatic style corrections courtesy of autopep8

--- hyperspy/misc/hspy_warnings.py
+++ hyperspy/misc/hspy_warnings.py
@@ -1,4 +1,5 @@
 class VisibleDeprecationWarning(UserWarning):
+
     """Visible deprecation warning.
     By default, python will not show deprecation warnings, so this class
     provides a visible one.
