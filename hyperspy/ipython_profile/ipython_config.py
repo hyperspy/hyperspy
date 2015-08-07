71f4ddf22ea6784d935f331531735f72ed2df6ae
Francisco de la Peña <frandelapena@gmail.com>
2015-08-07 10:54:15 +0100

    Make message more explicit.

--- hyperspy/ipython_profile/ipython_config.py
+++ hyperspy/ipython_profile/ipython_config.py
@@ -1,13 +1,13 @@
 from hyperspy import Release
 from hyperspy.defaults_parser import preferences
 
-__version__ = 1.0
+__version__ = 1.1
 
 # Configuration file for ipython.
 c = get_config()
 c.TerminalIPythonApp.ignore_old_config = True
 c.TerminalInteractiveShell.banner2 = Release.info + \
-    "\n\n** Hyperspy is loaded in hs.* namespace **\n\n"
+    "\n\n** hyperspy.api imported as hs in user namespace **\n\n"
 
 if preferences.General.import_hspy:
     c.InteractiveShellApp.exec_lines = [
