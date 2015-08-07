947266b26de0c6ad67ddde4979a56b48c2facb1d
Tomas Ostasevicius <to266@cam.ac.uk>
2015-08-07 11:52:23 +0100

    Automatic style corrections courtesy of autopep8

--- hyperspy/tests/signal/test_1D_tools.py
+++ hyperspy/tests/signal/test_1D_tools.py
@@ -199,7 +199,8 @@ class TestInterpolateInBetween:
         s.data[12] *= 10
         s.interpolate_in_between(8, 12, delta=2, kind='cubic')
         print s.data[8:12]
-        np.testing.assert_allclose(s.data[8:12], np.array([  44., 95.4,139.6,155. ]))
+        np.testing.assert_allclose(
+            s.data[8:12], np.array([44., 95.4, 139.6, 155.]))
 
     def test_delta_float(self):
         s = self.s[0]
@@ -207,7 +208,8 @@ class TestInterpolateInBetween:
         s.data[12] *= 10.
         s.interpolate_in_between(8, 12, delta=0.31, kind='cubic')
         print s.data[8:12]
-        np.testing.assert_allclose(s.data[8:12], np.array([  45.09388598,  104.16170809, 155.48258721,170.33564422]))
+        np.testing.assert_allclose(
+            s.data[8:12], np.array([45.09388598, 104.16170809, 155.48258721, 170.33564422]))
 
 
 class TestEstimatePeakWidth:
