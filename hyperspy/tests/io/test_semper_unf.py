40fe2adc1bce17df5a2fac40f95726f24d89e83c
Tomas Ostasevicius <to266@cam.ac.uk>
2015-12-03 14:57:27 +0000

    changed the line-endings to LF

--- hyperspy/tests/io/test_semper_unf.py
+++ hyperspy/tests/io/test_semper_unf.py
@@ -24,7 +24,7 @@ data_spectrum = np.arange(10, dtype=np.float32) / 2.
 data_image_int = np.arange(16, dtype=np.byte).reshape((4, 4))
 data_image_int16 = np.arange(16, dtype=np.int16).reshape((4, 4))
 data_image_int32 = np.arange(16, dtype=np.int32).reshape((4, 4))
-data_image_complex = (data_image_int + 1j*data_image).astype(np.complex64)
+data_image_complex = (data_image_int + 1j * data_image).astype(np.complex64)
 test_title = 'This is a test!'
 
 
@@ -43,28 +43,44 @@ def test_image_2d_loading():
 
 
 def test_spectrum_1d_loading():
-    signal = load(os.path.join(my_path, 'unf_files', 'example_spectrum_1d.unf'))
+    signal = load(
+        os.path.join(
+            my_path,
+            'unf_files',
+            'example_spectrum_1d.unf'))
     np.testing.assert_equal(signal.data, data_spectrum)
     np.testing.assert_equal(signal.original_metadata.IFORM, 2)  # float
     nt.assert_is_instance(signal, Spectrum)
 
 
 def test_image_int16_loading():
-    signal = load(os.path.join(my_path, 'unf_files', 'example_image_int16.unf'))
+    signal = load(
+        os.path.join(
+            my_path,
+            'unf_files',
+            'example_image_int16.unf'))
     np.testing.assert_equal(signal.data, data_image_int16)
     np.testing.assert_equal(signal.original_metadata.IFORM, 1)  # int16
     nt.assert_is_instance(signal, Image)
 
 
 def test_image_int32_loading():
-    signal = load(os.path.join(my_path, 'unf_files', 'example_image_int32.unf'))
+    signal = load(
+        os.path.join(
+            my_path,
+            'unf_files',
+            'example_image_int32.unf'))
     np.testing.assert_equal(signal.data, data_image_int32)
     np.testing.assert_equal(signal.original_metadata.IFORM, 4)  # int32
     nt.assert_is_instance(signal, Image)
 
 
 def test_image_complex_loading():
-    signal = load(os.path.join(my_path, 'unf_files', 'example_image_complex.unf'))
+    signal = load(
+        os.path.join(
+            my_path,
+            'unf_files',
+            'example_image_complex.unf'))
     np.testing.assert_equal(signal.data, data_image_complex)
     np.testing.assert_equal(signal.original_metadata.IFORM, 3)  # complex
     nt.assert_is_instance(signal, Image)
@@ -90,10 +106,17 @@ class TestCaseSaveAndRead():
     def test_save_and_read(self):
         signal_ref = Image(data_image)
         signal_ref.metadata.General.title = 'This is a test!'
-        signal_ref.save(os.path.join(my_path, 'unf_files', 'example_temp.unf'), overwrite=True)
+        signal_ref.save(
+            os.path.join(
+                my_path,
+                'unf_files',
+                'example_temp.unf'),
+            overwrite=True)
         signal = load(os.path.join(my_path, 'unf_files', 'example_temp.unf'))
         np.testing.assert_equal(signal.data, signal_ref.data)
-        np.testing.assert_equal(signal.metadata.General.title, 'This is a test!')
+        np.testing.assert_equal(
+            signal.metadata.General.title,
+            'This is a test!')
         nt.assert_is_instance(signal, Image)
 
     def tearDown(self):
