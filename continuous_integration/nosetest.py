e346c8375070ed416b957b0c7b64d738c8dec196
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-12-06 23:45:06 +0100

    Switch method again

--- continuous_integration/nosetest.py
+++ continuous_integration/nosetest.py
@@ -4,7 +4,12 @@
 if __name__ == '__main__':
     import sys
     from nose import run_exit
+    import warnings
     from traits.etsconfig.api import ETSConfig
 
+    warnings.simplefilter('error')
+    warnings.filterwarnings(
+        'ignore', "Failed to import the optional scikit image package",
+        UserWarning)
     ETSConfig.toolkit = "null"
     sys.exit(run_exit())
