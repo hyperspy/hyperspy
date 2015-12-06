f96bd17ed0712d51086010d78bcf50a8f2bb0f09
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-12-06 23:53:57 +0100

    Fix warning filters order

--- continuous_integration/nosetest.py
+++ continuous_integration/nosetest.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     import warnings
     from traits.etsconfig.api import ETSConfig
 
+    ETSConfig.toolkit = "null"
     warnings.simplefilter('error')
     warnings.filterwarnings(
         'ignore', "Failed to import the optional scikit image package",
         UserWarning)
-    ETSConfig.toolkit = "null"
     sys.exit(run_exit())
