469d4acf34d5209559b3c7d9b38dd50b2ca6901a
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-12-06 23:31:39 +0100

    Switch method

--- continuous_integration/nosetest.py
+++ continuous_integration/nosetest.py
@@ -3,11 +3,8 @@
 
 if __name__ == '__main__':
     import sys
-    import os
     from nose import run_exit
     from traits.etsconfig.api import ETSConfig
 
     ETSConfig.toolkit = "null"
-    env = os.environ.copy()
-    env['PYTHONWARNINGS'] = 'error'
-    sys.exit(run_exit(env=env))
+    sys.exit(run_exit())
