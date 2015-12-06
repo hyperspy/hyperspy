fc6e0d72193f13822037653a007790b724e65412
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-12-06 23:56:11 +0100

    Test dependency deprecation filter

--- continuous_integration/nosetest.py
+++ continuous_integration/nosetest.py
@@ -12,4 +12,8 @@ if __name__ == '__main__':
     warnings.filterwarnings(
         'ignore', "Failed to import the optional scikit image package",
         UserWarning)
+    warnings.filterwarnings(
+        'ignore',
+        "BaseException\.message has been deprecated as of Python 2\.6",
+        DeprecationWarning)
     sys.exit(run_exit())
