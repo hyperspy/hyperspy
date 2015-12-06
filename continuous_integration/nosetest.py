d093423b0ed09fb7f822ccceaa3f85c780a29c7f
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-12-06 20:52:37 +0100

    Make CI fail on warnings

--- continuous_integration/nosetest.py
+++ continuous_integration/nosetest.py
@@ -3,8 +3,11 @@
 
 if __name__ == '__main__':
     import sys
+    import os
     from nose import run_exit
     from traits.etsconfig.api import ETSConfig
 
     ETSConfig.toolkit = "null"
+    env = os.environ.copy()
+    env['PYTHONWARNINGS'] = 'error,ignore::ImportWarning'
     sys.exit(run_exit())
