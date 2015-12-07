61535dabb674b75dcf583a52210c047bc9f700e7
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-12-07 14:43:26 +0100

    Alternative builds

--- continuous_integration/nosetest.py
+++ continuous_integration/nosetest.py
@@ -4,16 +4,7 @@
 if __name__ == '__main__':
     import sys
     from nose import run_exit
-    import warnings
     from traits.etsconfig.api import ETSConfig
 
     ETSConfig.toolkit = "null"
-    warnings.simplefilter('error')
-    warnings.filterwarnings(
-        'ignore', "Failed to import the optional scikit image package",
-        UserWarning)
-    warnings.filterwarnings(
-        'ignore',
-        "BaseException\.message has been deprecated as of Python 2\.6",
-        DeprecationWarning)
     sys.exit(run_exit())
