0790a7ca5997e3daed318fe0cd0c89345fcbef20
vidartf <vidartf@gmail.com>
2015-12-10 17:24:42 +0100

    Update Events tests

--- hyperspy/tests/test_events.py
+++ hyperspy/tests/test_events.py
@@ -16,77 +16,70 @@ class TestEventsSuppression(EventsBase):
 
     def setUp(self):
         self.events = he.Events()
-        
+
         self.events.a = he.Event()
         self.events.b = he.Event()
         self.events.c = he.Event()
-        
+
         self.events.a.connect(self.on_trigger)
         self.events.b.connect(self.on_trigger)
         self.events.c.connect(self.on_trigger)
 
     def test_simple_suppression(self):
-        self.events.a.suppress = True
-        self.trigger_check(self.events.a.trigger, False)
-        self.trigger_check(self.events.b.trigger, True)
-        self.events.a.suppress = False
-        
-        with self.events.suppress:
+        with self.events.a.suppress():
+            self.trigger_check(self.events.a.trigger, False)
+            self.trigger_check(self.events.b.trigger, True)
+
+        with self.events.suppress():
             self.trigger_check(self.events.a.trigger, False)
             self.trigger_check(self.events.b.trigger, False)
             self.trigger_check(self.events.c.trigger, False)
-        
+
         self.trigger_check(self.events.a.trigger, True)
         self.trigger_check(self.events.b.trigger, True)
         self.trigger_check(self.events.c.trigger, True)
 
     def test_suppression_restore(self):
-        self.events.a.suppress = True
-        self.events.b.suppress = False
-        self.events.c.suppress = False
-        
-        with self.events.suppress:
-            self.trigger_check(self.events.a.trigger, False)
-            self.trigger_check(self.events.b.trigger, False)
-            self.trigger_check(self.events.c.trigger, False)
-            
-        self.trigger_check(self.events.a.trigger, False)
-        self.trigger_check(self.events.b.trigger, True)
-        self.trigger_check(self.events.c.trigger, True)
-
-    def test_suppresion_nesting(self):
-        self.events.a.suppress = True
-        self.events.b.suppress = False
-        self.events.c.suppress = False
-        
-        with self.events.suppress:
-            self.events.c.suppress = False
-            self.trigger_check(self.events.a.trigger, False)
-            self.trigger_check(self.events.b.trigger, False)
-            self.trigger_check(self.events.c.trigger, True)
-            
-            with self.events.suppress:
+        with self.events.a.suppress():
+            with self.events.suppress():
                 self.trigger_check(self.events.a.trigger, False)
                 self.trigger_check(self.events.b.trigger, False)
                 self.trigger_check(self.events.c.trigger, False)
-                
+
             self.trigger_check(self.events.a.trigger, False)
-            self.trigger_check(self.events.b.trigger, False)
+            self.trigger_check(self.events.b.trigger, True)
             self.trigger_check(self.events.c.trigger, True)
-            
-        self.trigger_check(self.events.a.trigger, False)
-        self.trigger_check(self.events.b.trigger, True)
-        self.trigger_check(self.events.c.trigger, True)
+
+    def test_suppresion_nesting(self):
+        with self.events.a.suppress():
+            with self.events.suppress():
+                self.events.c._suppress = False
+                self.trigger_check(self.events.a.trigger, False)
+                self.trigger_check(self.events.b.trigger, False)
+                self.trigger_check(self.events.c.trigger, True)
+
+                with self.events.suppress():
+                    self.trigger_check(self.events.a.trigger, False)
+                    self.trigger_check(self.events.b.trigger, False)
+                    self.trigger_check(self.events.c.trigger, False)
+
+                self.trigger_check(self.events.a.trigger, False)
+                self.trigger_check(self.events.b.trigger, False)
+                self.trigger_check(self.events.c.trigger, True)
+
+            self.trigger_check(self.events.a.trigger, False)
+            self.trigger_check(self.events.b.trigger, True)
+            self.trigger_check(self.events.c.trigger, True)
 
 
 class TestEventsSignatures(EventsBase):
-    
+
     def setUp(self):
         self.events = he.Events()
-        
+
         self.events.a = he.Event()
-        
-        
+
+
     def test_basic_triggers(self):
         self.events.a.connect(lambda *args, **kwargs: 0)
         self.events.a.connect(lambda: 0, None)
@@ -97,7 +90,7 @@ class TestEventsSignatures(EventsBase):
         self.events.a.connect(lambda x, y=988: \
                                  nose.tools.assert_not_equal(y, 988), 2)
         self.events.a.trigger(2, 5)
-        
+
         nose.tools.assert_raises(ValueError, self.events.a.trigger)
         nose.tools.assert_raises(ValueError, self.events.a.trigger, 2)
         self.events.a.trigger(2,5,8)
