ac8b7c2a6f2a4712157742215980c59ad37f2991
Francisco de la Peña <frandelapena@gmail.com>
2015-12-15 14:47:46 +0000

    Fix a couple of issues with this test. Fixes #798
    1. The test was sometimes failing randomly. This was traced down to `np.empty`.
    2. The `test_no_evr` was broken but passing because it was still raising an AttributeError due to a typo (!).

--- hyperspy/tests/mva/test_decomposition.py
+++ hyperspy/tests/mva/test_decomposition.py
@@ -70,20 +70,21 @@ class TestNdAxes:
                                              s2.learning_results.loadings)
 
 
-class TestGetExplainedVarinaceRation:
+class TestGetExplainedVarinaceRatio:
 
     def setUp(self):
         s = signals.Signal(np.empty(1))
-        s.learning_results.explained_variance_ratio = np.empty(10)
         self.s = s
 
     def test_data(self):
-        assert_true((self.s.get_explained_variance_ratio().data ==
-                     self.s.learning_results.explained_variance_ratio).all())
+        self.s.learning_results.explained_variance_ratio = np.asarray([2,4])
+        np.testing.assert_array_equal(
+            self.s.get_explained_variance_ratio().data,
+            np.asarray([2,4]))
 
     @raises(AttributeError)
     def test_no_evr(self):
-        self.s.get_explained_variance_ration()
+        self.s.get_explained_variance_ratio()
 
 
 class TestReverseDecompositionComponent:
