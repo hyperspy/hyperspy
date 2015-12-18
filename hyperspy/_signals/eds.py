e10187979b61d54db686053db60a7b39753ae8be
vidartf <vidartf@gmail.com>
2015-12-18 17:47:28 +0100

    PEP8

--- hyperspy/_signals/eds.py
+++ hyperspy/_signals/eds.py
@@ -816,10 +816,12 @@ class EDSSpectrum(Spectrum):
         for xray_line in xray_lines:
             line_energy, line_FWHM = self._get_line_energy(xray_line,
                                                            FWHM_MnKa='auto')
-            tmp = [line_energy - line_FWHM * line_width[0] - line_FWHM * windows_width,
+            tmp = [(line_energy - line_FWHM * line_width[0] -
+                    line_FWHM * windows_width),
                    line_energy - line_FWHM * line_width[0],
                    line_energy + line_FWHM * line_width[1],
-                   line_energy + line_FWHM * line_width[1] + line_FWHM * windows_width]
+                   (line_energy + line_FWHM * line_width[1] +
+                    line_FWHM * windows_width)]
             windows_position.append(tmp)
         windows_position = np.array(windows_position)
         # merge ovelapping windows
