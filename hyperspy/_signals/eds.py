687e1e5c28bc0fa3f5ae60a85f93541a7c33dfa0
vidartf <vidartf@gmail.com>
2015-12-18 17:47:14 +0100

    Fixed `EDSSignal.sum` for nav/signal summing.

--- hyperspy/_signals/eds.py
+++ hyperspy/_signals/eds.py
@@ -153,7 +153,7 @@ class EDSSpectrum(Spectrum):
                 xray_lines_not_in_range.append(xray_line)
         return xray_lines_in_range, xray_lines_not_in_range
 
-    def sum(self, axis):
+    def sum(self, axis="navigation"):
         """Sum the data over the given axis.
 
         Parameters
@@ -184,8 +184,10 @@ class EDSSpectrum(Spectrum):
         else:
             mp = s.metadata.Acquisition_instrument.TEM
         if mp.has_item('Detector.EDS.live_time'):
-            mp.Detector.EDS.live_time = mp.Detector.EDS.live_time * \
-                self.axes_manager[axis].size
+            iaxes = self._get_iaxes(axis)
+            for ax in iaxes:
+                mp.Detector.EDS.live_time = mp.Detector.EDS.live_time * \
+                    self.axes_manager[ax+3j].size
         return s
 
     def rebin(self, new_shape):
