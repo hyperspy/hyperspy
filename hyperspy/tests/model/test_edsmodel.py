38d1f0a4393f5c03eefb88a74e2a07caf2161843
Vidar Tonaas Fauske <vidartf@gmail.com>
2015-12-19 11:10:50 +0100

    Improved choice of testing routines.
    These give a more informative output when failing.

--- hyperspy/tests/model/test_edsmodel.py
+++ hyperspy/tests/model/test_edsmodel.py
@@ -24,9 +24,9 @@ class TestlineFit:
         s = self.s
         m = s.create_model()
         m.fit()
-        nt.assert_true(np.allclose([i.data for i in
+        np.testing.assert_allclose([i.data for i in
                                    m.get_lines_intensity()],
-                                   [[0.5], [0.2], [0.3]], atol=10-4))
+                                   [[0.5], [0.2], [0.3]], atol=10-4)
 
     def test_calibrate_energy_resolution(self):
         s = self.s
@@ -37,9 +37,9 @@ class TestlineFit:
             energy_resolution_MnKa,
         s.set_microscope_parameters(energy_resolution_MnKa=150)
         m.calibrate_energy_axis(calibrate='resolution')
-        nt.assert_true(np.allclose(
+        np.testing.assert_allclose(
             s.metadata.Acquisition_instrument.TEM.Detector.EDS.
-            energy_resolution_MnKa, reso, atol=1))
+            energy_resolution_MnKa, reso, atol=1)
 
     def test_calibrate_energy_scale(self):
         s = self.s
@@ -48,8 +48,8 @@ class TestlineFit:
         scale = s.axes_manager[-1].scale
         s.axes_manager[-1].scale += 0.0004
         m.calibrate_energy_axis('scale')
-        nt.assert_true(np.allclose(s.axes_manager[-1].scale,
-                                   scale, atol=1e-3))
+        np.testing.assert_allclose(s.axes_manager[-1].scale,
+                                   scale, atol=1e-3)
 
     def test_calibrate_energy_offset(self):
         s = self.s
@@ -58,8 +58,8 @@ class TestlineFit:
         offset = s.axes_manager[-1].offset
         s.axes_manager[-1].offset += 0.04
         m.calibrate_energy_axis('offset')
-        nt.assert_true(np.allclose(s.axes_manager[-1].offset,
-                                   offset, atol=1e-1))
+        np.testing.assert_allclose(s.axes_manager[-1].offset,
+                                   offset, atol=1e-1)
 
     def test_calibrate_xray_energy(self):
         s = self.s
@@ -68,9 +68,9 @@ class TestlineFit:
         m['Fe_Ka'].centre.value = 6.39
         m.calibrate_xray_lines(calibrate='energy', xray_lines=['Fe_Ka'],
                                bound=100)
-        nt.assert_true(np.allclose(
+        np.testing.assert_allclose(
             m['Fe_Ka'].centre.value, elements_db['Fe']['Atomic_properties'][
-                'Xray_lines']['Ka']['energy (keV)'], atol=1e-6))
+                'Xray_lines']['Ka']['energy (keV)'], atol=1e-6)
 
     def test_calibrate_xray_weight(self):
         s = self.s
@@ -85,8 +85,8 @@ class TestlineFit:
         m.fit()
         m.calibrate_xray_lines(calibrate='sub_weight',
                                xray_lines=['Fe_Ka'], bound=100)
-        nt.assert_true(np.allclose(0.0347, m['Fe_Kb'].A.value,
-                       atol=1e-3))
+        np.testing.assert_allclose(0.0347, m['Fe_Kb'].A.value,
+                                   atol=1e-3)
 
     def test_calibrate_xray_width(self):
         s = self.s
@@ -96,5 +96,5 @@ class TestlineFit:
         m['Fe_Ka'].sigma.value = 0.065
         m.calibrate_xray_lines(calibrate='energy', xray_lines=['Fe_Ka'],
                                bound=10)
-        nt.assert_true(np.allclose(sigma, m['Fe_Ka'].sigma.value,
-                                   atol=1e-2))
+        np.testing.assert_allclose(sigma, m['Fe_Ka'].sigma.value,
+                                   atol=1e-2)
