007b248ae002fbad74eb17a93fb02c8595712258
Francisco de la Peña <frandelapena@gmail.com>
2016-01-29 17:51:32 +0000

    Automatic style corrections courtesy of autopep8

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -3313,8 +3313,8 @@ class Signal(FancySlicing,
         self._plot.plot(**kwargs)
         self.events.data_changed.connect(self.update_plot, [])
         if self._plot.signal_plot:
-            self._plot.signal_plot.events.closed.connect( lambda:
-                self.events.data_changed.disconnect(self.update_plot), [])
+            self._plot.signal_plot.events.closed.connect(lambda:
+                                                         self.events.data_changed.disconnect(self.update_plot), [])
 
     def save(self, filename=None, overwrite=None, extension=None,
              **kwds):
