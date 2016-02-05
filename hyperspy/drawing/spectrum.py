234b6ed5e1393e34cc7d11b275f98334cac864b1
Tomas Ostasevicius <to266@cam.ac.uk>
2016-02-05 19:28:02 +0000

    plotting line profiles works as expected, updates nicely

--- hyperspy/drawing/spectrum.py
+++ hyperspy/drawing/spectrum.py
@@ -331,7 +331,16 @@ class SpectrumLine(object):
             ydata = self.data_function(axes_manager=self.axes_manager).real
         else:
             ydata = self.data_function(axes_manager=self.axes_manager).imag
-        self.line.set_ydata(ydata)
+
+        new_axis = self.axes_manager.signal_axes[0].axis
+        if len(new_axis) != len(self.axis) or \
+           (len(new_axis) == len(self.axis) and
+                np.any(np.not_equal(new_axis, self.axis))):
+            self.axis = new_axis
+            self.ax.set_xlim(self.axis[0], self.axis[-1])
+        self.line._xorig = self.axis
+        self.line._yorig = ydata
+        self.line.recache(True)
 
         if self.autoscale is True:
             self.ax.relim()
