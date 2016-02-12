271dbf6789638b7fdcd30c50e43f1a91daf347cb
Francisco de la Peña <frandelapena@gmail.com>
2016-02-12 16:04:42 +0000

    Test if signal id does not change for in-place operations

--- hyperspy/tests/signal/test_signal_operators.py
+++ hyperspy/tests/signal/test_signal_operators.py
@@ -24,7 +24,6 @@ from hyperspy.signal import Signal
 
 
 class TestBinaryOperators:
-
     def setUp(self):
         self.s1 = Signal(np.ones((2, 3)))
         self.s2 = Signal(np.ones((2, 3)))
@@ -35,8 +34,10 @@ class TestBinaryOperators:
         assert_array_equal(s.data, self.s1.data * 3)
 
     def test_sum_in_place_same_shape_signals(self):
+        s1 = self.s1
         self.s1 += self.s2
         assert_array_equal(self.s1.data, np.ones((2, 3)) * 3)
+        nt.assert_is(s1, self.s1)
 
     def test_sum_same_shape_signals_not_aligned(self):
         s1 = self.s1
@@ -53,6 +54,8 @@ class TestBinaryOperators:
     def test_sum_in_place_same_shape_signals_not_aligned(self):
         s1 = self.s1
         s2 = Signal(2 * np.ones((3, 2)))
+        s1c = s1
+        s2c = s2
         s1.axes_manager._axes[0].navigate = False
         s1.axes_manager._axes[1].navigate = True
         s2.axes_manager._axes[1].navigate = False
@@ -61,6 +64,8 @@ class TestBinaryOperators:
         assert_array_equal(s1.data, np.ones((3, 2)) * 3)
         s2 += s2
         assert_array_equal(s2.data, np.ones((3, 2)) * 4)
+        nt.assert_is(s1, s1c)
+        nt.assert_is(s2, s2c)
 
     @nt.raises(ValueError)
     def test_sum_wrong_shape(self):
@@ -70,11 +75,11 @@ class TestBinaryOperators:
 
     def test_broadcast_missing_sig_and_nav(self):
         s1 = self.s1
-        s2 = self.s2
+        s2 = self.s2.as_image((0, 1))  # (|3, 2)
         s1.axes_manager.set_signal_dimension(0)  # (3, 2|)
-        s2.axes_manager.set_signal_dimension(2)  # (|3, 2)
         s = s1 + s2
         assert_array_equal(s.data, 3 * np.ones((2, 3, 2, 3)))
+        assert_equal(s.metadata.Signal.record_by, "image")
 
     def test_broadcast_missing_sig(self):
         s1 = self.s1
@@ -100,11 +105,13 @@ class TestBinaryOperators:
         s1 = self.s1
         s1.axes_manager.set_signal_dimension(1)  # (3|2)
         s2 = Signal(np.ones((4, 2, 4, 3)))
+        s2c = s2
         s2.axes_manager.set_signal_dimension(2)  # (3, 4| 2, 4)
         print s2
         print s1
         s2 += s1
         assert_array_equal(s2.data, 2 * np.ones((4, 2, 4, 3)))
+        nt.assert_is(s2, s2c)
 
 
 class TestUnaryOperators:
