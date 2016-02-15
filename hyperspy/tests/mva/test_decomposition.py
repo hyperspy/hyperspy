8ac78c325f0d01eeeff3faabbd01e8610c3f13e2
Francisco de la Peña <frandelapena@gmail.com>
2016-02-15 15:16:01 +0000

    Add test

--- hyperspy/tests/mva/test_decomposition.py
+++ hyperspy/tests/mva/test_decomposition.py
@@ -55,6 +55,7 @@ class TestNdAxes:
 
     def test_consistensy_poissonian(self):
         s1 = self.s1
+        s1n000 = self.s1.inav[0, 0, 0]
         s2 = self.s2
         s12 = self.s12
         s1.decomposition(normalize_poissonian_noise=True)
@@ -68,6 +69,8 @@ class TestNdAxes:
                                              s2.learning_results.factors)
         np.testing.assert_array_almost_equal(s1.learning_results.factors,
                                              s2.learning_results.loadings)
+        # Check that views of the data don't change. See #871
+        np.testing.assert_array_equal(s1.inav[0, 0, 0].data, s1n000.data)
 
 
 class TestGetExplainedVarinaceRatio:
