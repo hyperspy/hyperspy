d62bd504956de99a74646376c2159cada51332bd
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-02-21 14:02:58 +0100

    Add logging code to init and toolkit code

--- hyperspy/gui/__init__.py
+++ hyperspy/gui/__init__.py
@@ -16,21 +16,29 @@
 # You should have received a copy of the GNU General Public License
 # along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+
 import matplotlib
 from traits.etsconfig.api import ETSConfig
 
 from hyperspy.defaults_parser import current_toolkit
 from hyperspy.misc.ipython_tools import get_ipython
 
+_logger = logging.getLogger(__name__)
+
 
 def set_ets_toolkit(toolkit):
     try:
         ETSConfig.toolkit = toolkit
+        _logger.debug('Current ETS toolkit set to: %s', toolkit)
     except ValueError:
         pass
 
 # Get the backend from matplotlib
 backend = matplotlib.rcParams["backend"]
+_logger.debug('Loading hyperspy.gui')
+_logger.debug('Current MPL backend: %s', backend)
+_logger.debug('Current ETS toolkit: %s', current_toolkit)
 if ("WX" not in backend and
         "Qt" not in backend):
     if current_toolkit in ("wx", "qt4"):
