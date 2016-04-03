6dd53a073357e3a7263d2b93f336fbbf75db34d7
petras <klavishas@gmail.com>
2016-04-03 18:43:28 +0200

    update testing bcf

--- hyperspy/tests/io/test_bcf.py
+++ hyperspy/tests/io/test_bcf.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from nose.tools import assert_true
 from hyperspy.io import load
+from hyperspy.io_plugins import bcf
 
 test_files = ['P45_instructively_packed_16bit_compressed.bcf',
               'P45_12bit_packed_8bit.bcf']  # for not compressed testing
@@ -18,4 +19,11 @@ def test_load():
         bse, sei, hype = s
         assert_true(bse.data.dtype == np.uint16)
         assert_true(sei.data.dtype == np.uint16)
-        assert_true(hype.data.dtype == np.uint16)
+
+def test_py_parsing():
+    bcf.fast_unbcf = False  #force to ignore cython code
+    for thingy in test_files:
+        my_path = os.path.dirname(__file__)
+        filename = os.path.join(my_path, 'bcf_data', thingy)
+        print('loading bcf test file...')
+        s = load(filename, downsample=2, cutoff_at_kV=10)
