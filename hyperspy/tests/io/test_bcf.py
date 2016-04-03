2e5c4e4db8c34bf79cb876c027efa38476e5fb36
petras <klavishas@gmail.com>
2016-04-03 18:28:41 +0200

    add: one more test file add some extra keywords in load test forcing larger coverage

--- hyperspy/tests/io/test_bcf.py
+++ hyperspy/tests/io/test_bcf.py
@@ -5,13 +5,17 @@ import numpy as np
 from nose.tools import assert_true
 from hyperspy.io import load
 
+test_files = ['P45_instructively_packed_16bit_compressed.bcf',
+              'P45_12bit_packed_8bit.bcf']  # for not compressed testing
+
+
 def test_load():
-    my_path = os.path.dirname(__file__)
-    filename = os.path.join(my_path, 'bcf_data',
-                        'P45_instructively_packed_16bit_compressed.bcf')
-    print('loading bcf test file...')
-    s = load(filename)
-    bse, sei, hype = s
-    assert_true(bse.data.dtype == np.uint16)
-    assert_true(sei.data.dtype == np.uint16)
-    assert_true(hype.data.dtype == np.uint16)
+    for thingy in test_files:
+        my_path = os.path.dirname(__file__)
+        filename = os.path.join(my_path, 'bcf_data', thingy)
+        print('loading bcf test file...')
+        s = load(filename, downsample=2, cutoff_at_kV=10)
+        bse, sei, hype = s
+        assert_true(bse.data.dtype == np.uint16)
+        assert_true(sei.data.dtype == np.uint16)
+        assert_true(hype.data.dtype == np.uint16)
