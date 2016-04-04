d1490dcdf036d783bda505eadeb6c85763fc4fd0
petras <klavishas@gmail.com>
2016-04-04 17:41:44 +0200

    update test_bcf ...again...

--- hyperspy/tests/io/test_bcf.py
+++ hyperspy/tests/io/test_bcf.py
@@ -22,18 +22,20 @@ def test_load_16bit():
     bse, sei, hype = s
     assert_true(bse.data.dtype == np.uint16)
     np_filename = os.path.join(my_path, 'bcf_data', np_file[0])
-    assert_true(np.all(hype.data[:22, :22, 222] == np.load(np_filename)))
+    np.testing.assert_array_equal(hype.data[:22, :22, 222],
+                                  np.load(np_filename))
     assert_true(hype.data.shape == (75, 100, 2048))
 
 
 def test_load_16bit_reduced():
     my_path = os.path.dirname(__file__)
     filename = os.path.join(my_path, 'bcf_data', test_files[0])
-    print('testing bcf instructivele packed16bit...')
-    s = load(filename, downsample=2, cutoff_at_kV=10)
+    print('testing downsampled 16bit bcf...')
+    s = load(filename, downsample=4, cutoff_at_kV=10)
     bse, sei, hype = s
     np_filename = os.path.join(my_path, 'bcf_data', np_file[1])
-    assert_true(np.all(hype.data[:2, :2, 222] == np.load(np_filename)))
+    np.testing.assert_array_equal(hype.data[:2, :2, 222],
+                                  np.load(np_filename))
     assert_true(hype.data.shape == (18, 25, 1047))
 
 
@@ -41,9 +43,10 @@ def test_load_8bit():
     for bcffile in test_files[1:]:
         my_path = os.path.dirname(__file__)
         filename = os.path.join(my_path, 'bcf_data', bcffile)
-        print('testing bcf instructivele packed16bit...')
+        print('testing simple 8bit bcf...')
         s = load(filename)
         bse, sei, hype = s
+
 
 #def test_fast_bcf():
 #    from hyperspy.io_plugins import unbcf_fast
