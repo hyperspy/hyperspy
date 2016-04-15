058599be11583b1140146d7514fa9051d73ab588
petras <klavishas@gmail.com>
2016-04-15 16:27:10 +0200

    fix: update test error messages

--- hyperspy/tests/misc/test_cython.py
+++ hyperspy/tests/misc/test_cython.py
@@ -1,5 +1,4 @@
 import nose.tools as nt
-import warnings
 
 import os
 
@@ -7,18 +6,27 @@ from nose.plugins.skip import SkipTest
 
 my_path = os.path.dirname(__file__)
 
+
 def test_cythonization():
     try:
         import Cython
     except ImportError:
         raise SkipTest
-    
+
     if not os.path.exists(my_path + '/cython/test_cython_integration.c'):
-        raise RuntimeError('deprecation of cythonization of pyx extensions detected!')
+        raise RuntimeError("""Automatic cythonization of extension failed
+try to run 'python setup.py recythonize' to find out the bug introduced""")
+
 
 def test_extensions_built():
     try:
         import hyperspy.tests.misc.cython.test_cython_integration as test_cy
         nt.assert_equal(7, test_cy.testing_cython())
     except ImportError:
-        raise RuntimeError('deprecation of building extensions detected!')
+        raise RuntimeError("""Compiled code is missing in source directory:
+1. Check out that testing environment compiles code in the source directory
+  by calling 'python setup.py build_ext --inplace', before running the test.
+2. Check out you have compiling environment (gcc/clang/msvc..)
+  same as used python version on your OS.
+3. Check out that cythonization of code works. I.e. try running
+  'python setup.py recythonize' """)
