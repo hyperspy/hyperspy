113e3e54b657cd201ead79c225fa6f5f7b4a55c2
Joshua Taillon <jat255@gmail.com>
2016-04-20 12:42:53 -0400

    Some initial work on tests (still not passing) and some corrections to edax.py bytestrings

--- hyperspy/io_plugins/edax.py
+++ hyperspy/io_plugins/edax.py
@@ -24,6 +24,7 @@ import os
 import numpy as np
 from hyperspy.misc.array_tools import sarray2dict
 import traits.api as t
+from hyperspy.misc.elements import elements_db
 
 # Plugin characteristics
 # ----------------------
@@ -49,27 +50,17 @@ writes = False
 spd_extensions = ('spd', 'SPD')
 spc_extensions = ('spc', 'SPC')
 
-atomic_num_dict = {1: 'H', 2: 'He', 3: 'Li', 4: 'Be', 5: 'B', 6: 'C', 7: 'N',
-                   8: 'O', 9: 'F', 10: 'Ne', 11: 'Na', 12: 'Mg', 13: 'Al',
-                   14: 'Si', 15: 'P', 16: 'S', 17: 'Cl', 18: 'Ar', 19: 'K',
-                   20: 'Ca', 21: 'Sc', 22: 'Ti', 23: 'V', 24: 'Cr', 25: 'Mn',
-                   26: 'Fe', 27: 'Co', 28: 'Ni', 29: 'Cu', 30: 'Zn', 31: 'Ga',
-                   32: 'Ge', 33: 'As', 34: 'Se', 35: 'Br', 36: 'Kr', 37: 'Rb',
-                   38: 'Sr', 39: 'Y', 40: 'Zr', 41: 'Nb', 42: 'Mo', 43: 'Tc',
-                   44: 'Ru', 45: 'Rh', 46: 'Pd', 47: 'Ag', 48: 'Cd', 49: 'In',
-                   50: 'Sn', 51: 'Sb', 52: 'Te', 53: 'I', 54: 'Xe', 55: 'Cs',
-                   56: 'Ba', 57: 'La', 58: 'Ce', 59: 'Pr', 60: 'Nd', 61: 'Pm',
-                   62: 'Sm', 63: 'Eu', 64: 'Gd', 65: 'Tb', 66: 'Dy', 67: 'Ho',
-                   68: 'Er', 69: 'Tm', 70: 'Yb', 71: 'Lu', 72: 'Hf', 73: 'Ta',
-                   74: 'W', 75: 'Re', 76: 'Os', 77: 'Ir', 78: 'Pt', 79: 'Au',
-                   80: 'Hg', 81: 'Tl', 82: 'Pb', 83: 'Bi', 84: 'Po', 85: 'At',
-                   86: 'Rn', 87: 'Fr', 88: 'Ra', 89: 'Ac', 90: 'Th', 91: 'Pa',
-                   92: 'U', 93: 'Np', 94: 'Pu', 95: 'Am', 96: 'Cm', 97: 'Bk',
-                   98: 'Cf', 99: 'Es', 100: 'Fm', 101: 'Md', 102: 'No',
-                   103: 'Lr', 104: 'Rf', 105: 'Db', 106: 'Sg', 107: 'Bh',
-                   108: 'Hs', 109: 'Mt', 110: 'Ds', 111: 'Rg', 112: 'Cp',
-                   113: 'Uut', 114: 'Uuq', 115: 'Uup', 116: 'Uuh', 117: 'Uus',
-                   118: 'Uuo'}
+# read dictionary of atomic numbers from HyperSpy, and add the elements that
+# do not currently exist in the database (in case anyone is doing EDS on
+# Ununpentium...)
+atomic_num_dict = dict(((p.General_properties.Z, e) for (e, p) in elements_db))
+atomic_num_dict.update({93: 'Np', 94: 'Pu', 95: 'Am', 96: 'Cm', 97: 'Bk',
+                        98: 'Cf', 99: 'Es', 100: 'Fm', 101: 'Md', 102: 'No',
+                        103: 'Lr', 104: 'Rf', 105: 'Db', 106: 'Sg', 107: 'Bh',
+                        108: 'Hs', 109: 'Mt', 110: 'Ds', 111: 'Rg', 112: 'Cp',
+                        113: 'Uut', 114: 'Uuq', 115: 'Uup', 116: 'Uuh',
+                        117: 'Uus',
+                        118: 'Uuo'})
 
 
 def get_spd_dtype_list(endianess='<'):
@@ -813,7 +804,7 @@ def spd_reader(filename,
         ipr_fname = os.path.splitext(
             os.path.basename(
                 original_metadata['spd_header'][
-                    'fName']))[0] + '.ipr'
+                    'fName']))[0] + b'.ipr'
     read_ipr = os.path.isfile(ipr_fname)
     if not read_ipr:
         print('Could not find .ipr file named {}.\n' \
