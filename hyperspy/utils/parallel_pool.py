952fbe60f82f63c27c88e981373101eea806cb4c
Tomas Ostasevicius <to266@cam.ac.uk>
2016-05-04 17:35:58 +0100

    parallel_pool documentation

--- hyperspy/utils/parallel_pool.py
+++ hyperspy/utils/parallel_pool.py
@@ -28,11 +28,58 @@ _logger = logging.getLogger(__name__)
 
 
 class ParallelPool:
+    """ Creates a ParallelPool by either looking for a ipyparallel client and
+    then creating a load_balanced_view, or by creating a multiprocessing pool
+
+    Methods
+    -------
+    setup
+        sets up the requested pool
+    sleep
+        sleeps for the requested (or timeout) time
+
+    Attributes
+    ----------
+
+    has_pool: Bool
+        Boolean if the pool is available and active.
+    pool: {ipyparallel.load_balanced_view, multiprocessing.Pool}
+        The pool object.
+    ipython_kwargs: dict
+        The dictionary with Ipyparallel connection arguments.
+    timeout: float
+        Timeout for either pool when waiting for results.
+    num_workers: int
+        The number of workers actually created (may be less than requested, but
+        can't be more).
+    timestep: float
+        Can be used as "ticks" to adjust CPU load when building upon this
+        class.
+    is_ipyparallel: bool
+        If the pool is ipyparallel-based
+    is_multiprocessing: bool
+        If the pool is multiprocessing-based
+
+    """
 
     _timestep = 0
 
     def __init__(self, num_workers=None, ipython_kwargs=None,
                  ipyparallel=None):
+        """Creates the ParallelPool and sets it up.
+
+        Parameters
+        ----------
+        num_workers: {None, int}
+            the (max) number of workers to create. If less are available,
+            smaller number is actually created.
+        ipyparallel: {None, bool}
+            which pool to set up. True - ipyparallel. False - multiprocessing.
+            None - try ipyparallel, then multiprocessing if failed.
+        ipython_kwargs: {None, dict}
+            arguments that will be passed to the ipyparallel.Client when
+            creating. Not None implies ipyparallel=True.
+        """
         if ipython_kwargs is None:
             ipython_kwargs = {}
         else:
@@ -41,7 +88,7 @@ class ParallelPool:
         self.ipython_kwargs = {'timeout': self.timeout}
         self.ipython_kwargs.update(ipython_kwargs)
         self.pool = None
-        self.num_workers = num_workers
+        self.num_workers = int(num_workers)
         self.timestep = 0.001
         self.setup(ipyparallel=ipyparallel)
 
@@ -57,14 +104,17 @@ class ParallelPool:
 
     @property
     def is_ipyparallel(self):
+        """Returns bool if the pool is ipyparallel-based"""
         return hasattr(self.pool, 'client')
 
     @property
     def is_multiprocessing(self):
+        """Returns bool if the pool is multiprocessing-based"""
         return isinstance(self.pool, Pool_type)
 
     @property
     def has_pool(self):
+        """Returns bool if the pool is ready and set-up"""
         return self.is_ipyparallel or self.is_multiprocessing and \
             self.pool._state is 0
 
@@ -87,6 +137,15 @@ class ParallelPool:
         return True
 
     def setup(self, ipyparallel=None):
+        """Sets up the pool.
+
+        Parameters
+        ----------
+        ipyparallel: {None, bool}
+            if True, only tries to set up the ipyparallel pool. If False - only
+            the multiprocessing. If None, first tries ipyparallel, and it does
+            not succeed, then multiprocessing.
+        """
         _logger.debug('Calling setup with ipyparallel={}'.format(ipyparallel))
         if not self.has_pool:
             if ipyparallel is True:
@@ -105,6 +164,14 @@ class ParallelPool:
                                  'type')
 
     def sleep(self, howlong=None):
+        """Sleeps for the required number of seconds.
+
+        Parameters
+        ----------
+        howlong: {None, float}
+            How long the pool should sleep for in seconds. If None (default),
+            sleeps for "timestep"
+        """
         if howlong is None:
             howlong = self.timestep
         time.sleep(howlong)
