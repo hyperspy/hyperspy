a5ef1d5a711e5a93f052bddbafe628b5613bc06a
tjof2 <tjof2@cam.ac.uk>
2016-06-17 08:53:10 +0100

    Fixed typo

--- hyperspy/learn/rpca.py
+++ hyperspy/learn/rpca.py
@@ -16,11 +16,11 @@
 # (at your option) any later version.
 #
 #  HyperSpy is distributed in the hope that it will be useful,
-# but WITHOUT ANdata WARRANTdata; without even the implied warranty of
-# MERCHANTABILITdata or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# dataou should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU General Public License
 # along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
