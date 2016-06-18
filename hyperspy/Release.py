ced0bce0b205e6d9b29ef26e718ef10edc030bab
Francisco de la Peña <frandelapena@gmail.com>
2016-06-18 12:51:29 +0100

    Try something else

--- hyperspy/Release.py
+++ hyperspy/Release.py
@@ -1,30 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2007-2016 The HyperSpy developers
-#
-# This file is part of  HyperSpy.
-#
-#  HyperSpy is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-#  HyperSpy is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
-
-
-# The commit following to a release must update the version number
-# to the version number of the release followed by "+dev", e.g.
-# if the version of the last release is 0.4.1 the version of the
-# next development version afterwards must be 0.4.1+dev.
-# When running setup.py the "+dev" string will be replaced (if possible)
-# by the output of "git describe" if git is available or the git
-# hash if .git is present.
-
 description = "Multidimensional data analysis toolbox"
 version = '1.0'
 version += '+dev'
