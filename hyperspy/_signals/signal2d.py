0f955e7695426088bacccfa90be335ce7725746e
bm424 <bm424@cam.ac.uk>
2016-06-20 15:36:57 +0100

    Update `find_peaks_blob` to latest (working) version

--- hyperspy/_signals/signal2d.py
+++ hyperspy/_signals/signal2d.py
@@ -508,9 +508,10 @@ def find_peaks_masiel(z, subpixel=False, peak_width=10, medfilt_radius=5,
     return peaks
 
 
-def find_peaks_blob(z, threshold=5., **kwargs):
+def find_peaks_blob(z, **kwargs):
     """
     Finds peaks via the difference of Gaussian Matrices method in scikit-image.
+
     Parameters
     ----------
     z : ndarray
@@ -519,19 +520,29 @@ def find_peaks_blob(z, threshold=5., **kwargs):
         lower than minimum peak intensity.
     kwargs : Additional parameters to be passed to the algorithm. See 'blob_dog'
         documentation for details.
+
     Returns
     -------
     ndarray
         (n_peaks, 2)
         Array of peak coordinates.
+
     Notes
     -----
     While highly effective at finding even very faint peaks, this method is
         sensitive to fluctuations in intensity near the edges of the image.
+
     """
     from skimage.feature import blob_dog
-    blobs = blob_dog(z, threshold=threshold, **kwargs)
-    return blobs[:, :2]
+    blobs = blob_dog(z, **kwargs)
+    centers = blobs[:, :2]
+    clean_centers = []
+    for center in centers:
+        if len(np.intersect1d(center, (0,) + z.shape + tuple(
+                        c - 1 for c in z.shape))) > 0:
+            continue
+        clean_centers.append(center)
+    return np.array(clean_centers)
 
 
 def subpix_locate(z, peaks, peak_width, scale=None):
