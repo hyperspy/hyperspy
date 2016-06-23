abd34c1a5c4169c5e655adaae3797873e071e590
Tom Furnival <tjof2@cam.ac.uk>
2016-06-23 17:25:23 +0100

    Updated as per comments

--- hyperspy/learn/rpca.py
+++ hyperspy/learn/rpca.py
@@ -81,16 +81,17 @@ def orpca(X, rank, fast=False, lambda1=None,
     Parameters
     ----------
     X : numpy array
-        is the [m x n] matrix of observations.
+        is the [nfeatures x nsamples] matrix of observations.
     rank : int
         The model dimensionality.
-    lambda1 : float
-        Nuclear norm regularization parameter.
-    lambda2 : float
-        Sparse error regularization parameter.
-    method : 'CF' | 'BCD'
-        CF  - Closed-form (default)
-        BCD - Block-coordinate descent
+    lambda1 : float | None
+        Nuclear norm regularization parameter. If None, set to 1/sqrt(nfeatures)
+    lambda2 : float | None
+        Sparse error regularization parameter. If None, set to 1/sqrt(nfeatures)
+    method : 'CF' | 'BCD' | None
+        If none, set to 'CF'
+        'CF'  - Closed-form
+        'BCD' - Block-coordinate descent
     mask : numpy array
         is an initial estimate of the sparse error matrix
     seed : int
@@ -99,9 +100,9 @@ def orpca(X, rank, fast=False, lambda1=None,
     Returns
     -------
     L : numpy array
-        is the [m x r] basis array
+        is the [nfeatures x rank] basis array
     R : numpy array
-        is the [r x n] coefficient array
+        is the [rank x nsamples] coefficient array
     E : numpy array
         is the sparse error
     U, S, V : numpy arrays
@@ -121,15 +122,15 @@ def orpca(X, rank, fast=False, lambda1=None,
     # Check options if None
     if method is None:
         _logger.warning("No method specified. Defaulting to "
-                        "closed-form solver")
+                        "'CF' (closed-form solver)")
         method = 'CF'
     if lambda1 is None:
         _logger.warning("Nuclear norm regularization parameter "
-                        "is set to default.")
+                        "is set to default: 1/sqrt(nfeatures)")
         lambda1 = 1.0 / np.sqrt(n)
     if lambda2 is None:
         _logger.warning("Sparse regularization parameter "
-                        "is set to default.")
+                        "is set to default: 1/sqrt(nfeatures)")
         lambda2 = 1.0 / np.sqrt(n)
 
     # Check options are valid
