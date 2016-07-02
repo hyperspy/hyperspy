1a75e941cc325f3636daf6777f02cae8aad721d7
Francisco de la Peña <frandelapena@gmail.com>
2016-07-02 17:15:12 +0100

    Fix bounded fitting for param dim > 1

--- hyperspy/model.py
+++ hyperspy/model.py
@@ -601,13 +601,24 @@ class Model(list):
                 for param in component.free_parameters:
                     bmin = -np.inf if param.bmin is None else param.bmin
                     bmax = np.inf if param.bmax is None else param.bmax
-                    if not bmin <= param.value <= bmax:
-                        min_d = np.abs(param.value - bmin)
-                        max_d = np.abs(param.value - bmax)
-                        if min_d < max_d:
-                            param.value = bmin
-                        else:
-                            param.value = bmax
+                    if param._number_of_elements == 1:
+                        if not bmin <= param.value <= bmax:
+                            min_d = np.abs(param.value - bmin)
+                            max_d = np.abs(param.value - bmax)
+                            if min_d < max_d:
+                                param.value = bmin
+                            else:
+                                param.value = bmax
+                    else:
+                        values = np.array(param.value)
+                        if param.bmin is not None:
+                            minmask = values < bmin
+                            values[minmask] = bmin
+                        if param.bmax is not None:
+                            maxmask = values > bmax
+                            values[maxmask] = bmax
+                        param.value = tuple(values)
+
 
     def store_current_values(self):
         """ Store the parameters of the current coordinates into the
