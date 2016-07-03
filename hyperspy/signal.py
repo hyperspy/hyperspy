a7a80950ebc2f84cbc4181e8e36d3f0d9fbb456c
Francisco de la Peña <frandelapena@gmail.com>
2016-07-03 16:26:23 +0100

    Fix #1004

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -2144,9 +2144,9 @@ class BaseSignal(FancySlicing,
 
         if i1 is not None:
             axis.offset = new_offset
-        self.events.data_changed.trigger(obj=self)
         self.get_dimensions_from_data()
         self.squeeze()
+        self.events.data_changed.trigger(obj=self)
 
     def swap_axes(self, axis1, axis2):
         """Swaps the axes.
