Fixed slow-down due to scaling

hyperspy/learn/rpca.py
@@ -124,11 +124,11 @@ def orpca(X, rank, fast=False, lambda1=None,
         method = 'CF'
     if lambda1 is None:
         _logger.warning("Nuclear norm regularization parameter "
-                        "is set to default: 1/sqrt(nfeatures)")
+                        "is set to default: 1/sqrt(nsamples)")
         lambda1 = 1.0 / np.sqrt(n)
     if lambda2 is None:
         _logger.warning("Sparse regularization parameter "
-                        "is set to default: 1/sqrt(nfeatures)")
+                        "is set to default: 1/sqrt(nsamples)")
         lambda2 = 1.0 / np.sqrt(n)
 
     # Check options are valid
@@ -178,14 +178,9 @@ def orpca(X, rank, fast=False, lambda1=None,
             L = _updatecol(L, A, B, I)
 
     # Rescale
-    #L = (L * X_max) + X_min
     Xhat = np.dot(L, R)
     Xhat = (Xhat * X_max) + X_min
     E = (E * X_max) + X_min
-
-    print('Xmax is %f, Xmin is %f' % (X_max, X_min))
-    print('Emax is %f, Emin is %f' % (np.max(E), np.min(E)))
-    print('Xhatmax is %f, Xhatmin is %f' % (np.max(Xhat), np.min(Xhat)))
 
     # Do final SVD
     U, S, Vh = svd(Xhat)

hyperspy/tests/mva/test_orpca.py
@@ -6,40 +6,27 @@ from nose.plugins.skip import SkipTest
 
 from hyperspy.learn.rpca import orpca
 
-def _ev(U, L, atol=1e-3):
-    # Check the similarity between the original
-    # subspace basis and the OR-PCA results using
-    # the "Expressed Variance" metric.
-    # Perfect recovery of subspace: ev = 1
-    C = np.dot(U, U.T)
-    numer = np.trace(np.dot(L.T, np.dot(C, L)))
-    denom = np.trace(C)
-    return (1. - (numer / denom) < atol)
-
 class TestORPCA:
     def setUp(self):
         # Define shape etc.
         m = 128  # Dimensionality
         n = 1024 # Number of samples
-        r = 3
+        r = 5
         s = 0.01
 
         # Low-rank and sparse error matrices
-        rng = np.random.RandomState(101)
+        rng = np.random.RandomState(123)
         U = scipy.linalg.orth(rng.randn(m, r))
         V = rng.randn(n, r)
         A = np.dot(U, V.T)
         E = 100 * rng.binomial(1, s, (m, n))
         X = A + E
-        print('Amax is %f, Amin is %f' % (np.max(A),np.min(A)))
-        print('Emax is %f, Emin is %f'% (np.max(E),np.min(E)))
-        print('Xmax is %f, Xmin is %f'% (np.max(X),np.min(X)))
 
         self.m = m
         self.n = n
         self.rank = r
-        self.lambda1 = 1 / np.sqrt(m)
-        self.lambda2 = 1 / np.sqrt(m)
+        self.lambda1 = 1.0 / np.sqrt(n)
+        self.lambda2 = 1.0 / np.sqrt(n)
         self.U = U
         self.A = A
         self.E = E
@@ -50,55 +37,50 @@ class TestORPCA:
 
     def test_default(self):
         L, R, E, U, S, V = orpca(self.X, rank=self.rank)
+        A = np.dot(L, R)
 
-        # Check the low-rank component
-        normA = np.linalg.norm(np.dot(L, R) - self.A) / (self.m * self.n)
+        # Check the low-rank component MSE
+        normA = np.linalg.norm(A - self.A) / (self.m * self.n)
         nt.assert_true(normA < self.tol)
 
-        # Check the expressed variance of the
-        # recovered subspace
-        nt.assert_true(_ev(self.U, L, self.tol))
+        # Check the low-rank component rank
+        rankA = np.linalg.matrix_rank(A, self.tol)
+        np.testing.assert_almost_equal(rankA, self.rank)
 
     def test_mask(self):
         L, R, E, U, S, V = orpca(self.X, rank=self.rank,
                                  mask=self.E)
+        A = np.dot(L, R)
 
-        # Check the low-rank component
-        normA = np.linalg.norm(np.dot(L, R) - self.A) / (self.m * self.n)
+        # Check the low-rank component MSE
+        normA = np.linalg.norm(A - self.A) / (self.m * self.n)
         nt.assert_true(normA < self.tol)
 
-        # Check the expressed variance of the
-        # recovered subspace
-        nt.assert_true(_ev(self.U, L, self.tol))
+        # Check the low-rank component rank
+        rankA = np.linalg.matrix_rank(A, self.tol)
+        np.testing.assert_almost_equal(rankA, self.rank)
+
+    def test_method(self):
+        L, R, E, U, S, V = orpca(self.X, rank=self.rank, method='BCD')
+        A = np.dot(L, R)
+
+        # Check the low-rank component MSE
+        normA = np.linalg.norm(A - self.A) / (self.m * self.n)
+        nt.assert_true(normA < self.tol)
+
+        # Check the low-rank component rank
+        rankA = np.linalg.matrix_rank(A, self.tol)
+        np.testing.assert_almost_equal(rankA, self.rank)
 
     def test_regularization(self):
         L, R, E, U, S, V = orpca(self.X, rank=self.rank,
                                  lambda1=self.lambda1, lambda2=self.lambda2)
+        A = np.dot(L, R)
 
-        # Check the low-rank component
-        normA = np.linalg.norm(np.dot(L, R) - self.A) / (self.m * self.n)
+        # Check the low-rank component MSE
+        normA = np.linalg.norm(A - self.A) / (self.m * self.n)
         nt.assert_true(normA < self.tol)
 
-        # Check the expressed variance of the
-        # recovered subspace
-        nt.assert_true(_ev(self.U, L, self.tol))
-
-    def test_method(self):
-        L, R, E, U, S, V = orpca(self.X, rank=self.rank, method='BCD')
-
-        #  Check the low-rank component
-        normA = np.linalg.norm(np.dot(L, R) - self.A) / (self.m * self.n)
-        nt.assert_true(normA < self.tol)
-
-        # Check the expressed variance of the
-        # recovered subspace
-        nt.assert_true(_ev(self.U, L, self.tol))
-
-
-if __name__ == "__main__":
-    test = TestORPCA()
-    test.setUp()
-    test.test_default()
-    test.test_mask()
-    test.test_method()
-    test.test_regularization()
+        # Check the low-rank component rank
+        rankA = np.linalg.matrix_rank(A, self.tol)
+        np.testing.assert_almost_equal(rankA, self.rank)