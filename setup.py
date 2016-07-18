6af7454996abeb6c1a1cf20d38735e2e00c153c2
Eric Prestat <eric.prestat@gmail.com>
2016-07-18 23:16:41 +0100

    add pint>=0.7 dependency

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ install_req = ['scipy',
                'dill',
                'h5py',
                'python-dateutil',
-               'ipyparallel']
+               'ipyparallel',
+               'pint>=0.7']
 
 #the hack to deal with setuptools + installing the package in ReadTheDoc:
 if 'readthedocs.org' in sys.executable:
