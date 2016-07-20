aec0eb72a7af193379569f388554fc2d83ac30c3
Francisco de la Peña <frandelapena@gmail.com>
2016-07-20 14:42:33 +0100

    Fix #1153

--- hyperspy/docstrings/plot.py
+++ hyperspy/docstrings/plot.py
@@ -68,9 +68,7 @@ PLOT2D_DOCSTRING = \
             For example, the low and high bounds of a value of 1 are the 0.5%
             and 99.5% percentiles. It must be in the [0, 100] range.
         vmin, vmax : scalar, optional
-            `vmin` and `vmax` are used to normalize luminance data. If at
-            least one of them is given `auto_contrast` is set to False and any
-            missing values are calculated automatically.
+            `vmin` and `vmax` are used to normalize luminance data.
         no_nans : bool, optional
             If True, set nans to zero for plotting.
         centre_colormap : {"auto", True, False}
@@ -80,8 +78,9 @@ PLOT2D_DOCSTRING = \
 
 COMPLEX_DOCSTRING = \
     """representation : {'cartesian' or 'polar'}
-            Determines if the real and imaginary part of the complex data is plotted ('cartesian',
-            default), or if the amplitude and phase should be used ('polar')."""
+            Determines if the real and imaginary part of the complex data is
+            plotted ('cartesian', default), or if the amplitude and phase should
+            be used ('polar')."""
 
 KWARGS_DOCSTRING = \
     """**kwargs, optional
