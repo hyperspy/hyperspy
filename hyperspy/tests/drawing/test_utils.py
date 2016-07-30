37fc939f3b5d087d850f95438d05a2013a9282f6
Eric Prestat <eric.prestat@gmail.com>
2016-07-30 14:19:50 +0100

    attempt to fix drawing/tests_utils.py not working in travis and appveyor...

--- hyperspy/tests/drawing/test_utils.py
+++ hyperspy/tests/drawing/test_utils.py
@@ -18,17 +18,40 @@
 import matplotlib
 from matplotlib.testing.decorators import cleanup
 import nose.tools as nt
+import warnings
+
 import hyperspy.drawing.utils as utils
+from hyperspy.misc.test_utils import assert_warns
+
 
 @cleanup
 def test_create_figure():
     def dummy_function():
-        global a
-        a = 10
+        global test
+        test = 10
+        print('dummy_function have been called after closing windows')
     window_title = 'test title'
     fig = utils.create_figure(window_title=window_title,
                               _on_figure_window_close=dummy_function)
     nt.assert_true(isinstance(fig, matplotlib.figure.Figure))
-    
-    matplotlib.pyplot.close()
-    nt.assert_equal(a, 10)
+
+    matplotlib.pyplot.close(fig)
+    nt.assert_equal(test, 10)
+
+
+@cleanup
+def test_create_figure2():
+    dummy_warning = 'Dummy_function have been called after closing windows'
+
+    def dummy_function():
+        warnings.warn(dummy_warning, UserWarning)
+        print(dummy_warning)
+
+    with assert_warns(
+            message=dummy_warning,
+            category=UserWarning):
+        window_title = 'test title'
+        fig = utils.create_figure(window_title=window_title,
+                                  _on_figure_window_close=dummy_function)
+        nt.assert_true(isinstance(fig, matplotlib.figure.Figure))
+        matplotlib.pyplot.close(fig)
