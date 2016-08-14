78fe640003cc12f5bcac5aed9bb4142d14730508
vidartf <vidartf@gmail.com>
2016-08-14 22:24:49 +0200

    Don't call set_log_level if preference is 'NOTSET'

--- hyperspy/api.py
+++ hyperspy/api.py
@@ -79,7 +79,10 @@ from hyperspy.utils import *
 from hyperspy import datasets
 from hyperspy.logger import set_log_level
 
-set_log_level(preferences.General.logging_level)
+if preferences.General.logging_level == 'NOTSET':
+    logging.basicConfig()  # Does nothing if already configured
+else:
+    set_log_level(preferences.General.logging_level)
 
 
 def get_configuration_directory_path():
