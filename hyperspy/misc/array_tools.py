87ccf52896c0134611cae045b229cca19de3053e
k8macarthur <k8macarthur@gmail.com>
2016-09-08 15:21:39 +0200

    Merge remote-tracking branch 'hyperspy/master'
    Linear_bin function added

--- hyperspy/misc/array_tools.py
+++ hyperspy/misc/array_tools.py
@@ -101,6 +101,69 @@ def rebin(a, new_shape):
     return eval(''.join(evList))
 
 
+def linear_bin(originalSpectrum, scale):
+
+    """
+    Binning of the spectrum image by a non-integer pixel value.
+
+    Parameters
+    ----------
+    originalSpectrum: numpy.array
+        the original spectrum
+    step: a list of floats for each dimension specify the new:old pixel
+    ratio
+    e.g. a ratio of 1 is no binning
+         a ratio of 2 means that each pixel in the new spectrum is
+         twice the size of the pixels in the old spectrum.
+
+    Return
+    ------
+    numpy.array of the spectrum with new dimensions width/step.
+    """
+
+    shape = originalSpectrum.data.shape
+    if len(shape) != len(scale):
+        raise ValueError(
+           'The list of bins must match the number of dimensions, including the energy dimension.'
+            'In order to not bin in any of these dimensions specifically, simply set the value in shape to 1')
+
+    spectrum = originalSpectrum.deepcopy()
+    s = spectrum.data
+
+    for k, step in enumerate(scale):
+        shape = s.shape
+        newSpectrum = np.zeros((math.ceil(shape[0]/step), shape[1], shape[2]), dtype='float')
+        if k != 0:
+            s = np.swapaxes(s, 0, k)
+            shape = s.shape
+            newSpectrum = np.zeros((math.ceil(shape[0]/step), shape[1], shape[2]), dtype='float')
+        for j in range(0, math.ceil(shape[0]/step)):
+            bottomPos = (j*step)
+            topPos = ((1 + j) * step)
+            if topPos > shape[0]:
+                topPos = shape[0]
+            while (topPos - bottomPos) >= 1:
+                if math.ceil(bottomPos) - bottomPos != 0:
+                    newSpectrum[j] = newSpectrum[j] + s[math.floor(bottomPos)] * (math.ceil(bottomPos) - bottomPos)
+                    bottomPos = math.ceil(bottomPos)
+                else:
+                    newSpectrum[j] = newSpectrum[j] + s[bottomPos]
+                    bottomPos += 1
+            if topPos != bottomPos:
+                newSpectrum[j] = newSpectrum[j] + s[math.floor(bottomPos)] *(topPos - bottomPos)
+        if k != 0:
+            newSpectrum = np.swapaxes(newSpectrum, 0, k)
+        spectrum = spectrum._deepcopy_with_new_data(newSpectrum)
+        s = spectrum.data
+
+    spectrum.get_dimensions_from_data()
+    print(scale)
+    for s, step in zip(spectrum.axes_manager._axes, scale):
+        s.scale /= step
+
+    return spectrum
+
+
 def sarray2dict(sarray, dictionary=None):
     """Converts a struct array to an ordered dictionary
 
