9587d8d31b334014f938016d00c7c217c810574e
Tomas Ostasevicius <t.ostasevicius@gmail.com>
2016-09-15 15:38:48 +0100

    samfire fixes

--- hyperspy/samfire.py
+++ hyperspy/samfire.py
@@ -238,15 +238,18 @@ class Samfire:
         num_of_strat = len(self.strategies)
         total_size = self.model.axes_manager.navigation_size - self.pixels_done
         self._progressbar = progressbar(total=total_size)
-
-        while True:
-            self._run_active_strategy()
-            self.plot()
-            if self._active_strategy_ind == num_of_strat - 1:
-                # last one just finished running
-                break
-
-            self.change_strategy(self._active_strategy_ind + 1)
+        try:
+            while True:
+                self._run_active_strategy()
+                self.plot()
+                if self._active_strategy_ind == num_of_strat - 1:
+                    # last one just finished running
+                    break
+                self.change_strategy(self._active_strategy_ind + 1)
+        except KeyboardInterrupt:
+            if self.pool is not None:
+                _logger.warning('Collecting already started pixels, please wait')
+                self.pool.collect_results()
 
     def append(self, strategy):
         """appends the given strategy to the end of the strategies list
@@ -320,8 +323,8 @@ class Samfire:
             if isgood:
                 self._progressbar.update(1)
             self.active_strategy.update(ind, isgood)
-            self._plot()
-            self._save()
+            self.plot(on_count=True)
+            self.backup(on_count=True)
 
     def backup(self, filename=None, on_count=True):
         """Backs-up the samfire results in a file
