0a23bb6a8c9ee57367c0c6c5144112656c150a03
Tomas Ostasevicius <t.ostasevicius@gmail.com>
2016-09-22 15:49:46 +0100

    add shift to SFP2D

--- hyperspy/_components/scalable_fixed_pattern2d.py
+++ hyperspy/_components/scalable_fixed_pattern2d.py
@@ -51,11 +51,13 @@ class ScalableFixedPattern2D(Component2D):
     def __init__(self, signal2D,
                  d11=1., d12=0.,
                  d21=0., d22=1.,
+                 t1=0., t2=0.,
                  order=3
                  ):
 
         Component2D.__init__(self, ['d11', 'd12',
-                                  'd21', 'd22'])
+                                    'd21', 'd22',
+                                    't1', 't2'])
 
         self._whitelist['signal2D'] = ('init,sig', signal2D)
         self.signal = signal2D
@@ -64,6 +66,8 @@ class ScalableFixedPattern2D(Component2D):
         self.d12.value = d12
         self.d21.value = d21
         self.d22.value = d22
+        self.t1.value = t1
+        self.t2.value = t2
 
     def function(self, x, y):
 
@@ -73,9 +77,11 @@ class ScalableFixedPattern2D(Component2D):
         d12 = self.d12.value
         d21 = self.d21.value
         d22 = self.d22.value
+        t1 = self.t1.value
+        t2 = self.t2.value
 
-        D = np.array([[d11, d12, 0.],
-                      [d21, d22, 0.],
+        D = np.array([[d11, d12, t1],
+                      [d21, d22, t2],
                       [0., 0., 1.]])
 
         shifty, shiftx = np.array(signal2D.shape[:2]) / 2
