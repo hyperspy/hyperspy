4d6c65e32cec99abd89b0dc5868d391d3f932a00
Tomas Ostasevicius <t.ostasevicius@gmail.com>
2016-09-23 14:20:40 +0100

    not overwrite normal data

--- hyperspy/samfire_utils/samfire_worker.py
+++ hyperspy/samfire_utils/samfire_worker.py
@@ -70,7 +70,8 @@ class Worker:
         for k, v in dct.items():
             if isinstance(v, BaseSignal):
                 v.data = v.data.copy()
-                self.parameters[k] = None
+                if k not in ['signal', 'image', 'spectrum']:
+                    self.parameters[k] = None
             if isinstance(v, np.ndarray):
                 dct[k] = v.copy()
 
