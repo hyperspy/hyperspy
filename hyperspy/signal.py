7d41343441ce7d5830ca1eaeab7d05833b3f27dd
Thomas Aarholt <thomasaarholt@gmail.com>
2016-10-13 19:10:54 +0100

    Added overwriteable-method to BaseSignal
    I’m going to ask the rest of the Hyperspy group if this is bad
    practice, but I think it should be fine and certainly works alright. If
    a signal has type EDS, the method in the class overwrites the
    BaseSignal one. So this way all signals have access.

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -55,6 +55,7 @@ from hyperspy.docstrings.plot import (
 from hyperspy.events import Events, Event
 from hyperspy.interactive import interactive
 from hyperspy.misc.signal_tools import are_signals_aligned
+from hyperspy.misc.array_tools import _linear_bin
 
 import warnings
 
@@ -2269,6 +2270,60 @@ class BaseSignal(FancySlicing,
             out.events.data_changed.trigger(obj=out)
 
     rebin.__doc__ %= OUT_ARG
+
+    def linear_bin(self, scale):
+
+        """
+        Binning of the spectrum image by a non-integer pixel value.
+
+        Parameters
+        ----------
+        self: numpy.array
+            the original spectrum
+        scale: a list of floats for each dimension specify the new:old pixel
+        ratio
+        e.g. [1, 1, 2]
+            a ratio of 1 is no binning in the x and y directions.
+             a ratio of 2 means that each pixel in the new spectrum is
+             twice the width of the pixels in the old spectrum, in the energy,
+             dimension.
+
+        Return
+        ------
+        numpy.array of the spectrum with new dimensions width/step.
+
+
+        Examples
+        --------
+        Input:
+        spectrum = hs.signals.EDSTEMSpectrum(np.ones([4, 4, 10]))
+        spectrum.data[1, 2, 9] = 5
+        print(spectrum)
+        print ('Sum = ', sum(sum(sum(spectrum.data))))
+        scale = [2, 2, 5]
+        test = spectrum.linear_bin(scale)
+        print(test)
+        print('Sum = ', sum(sum(sum(test.data))))
+
+        Output:
+        <EDSTEMSpectrum, title: , dimensions: (4, 4|10)>
+        Sum =  164.0
+        <EDSTEMSpectrum, title: , dimensions: (2, 2|2)>
+        Sum =  164.0
+
+        """
+        spectrum = self.data
+        newSpectrum = np.zeros(spectrum.shape)
+        newSpectrum = spectrum[:]
+        newSpectrum = _linear_bin(spectrum, scale)
+
+        m = self._deepcopy_with_new_data(newSpectrum)
+
+        m.get_dimensions_from_data()
+        for s, step in zip(m.axes_manager._axes, scale):
+            s.scale /= step
+
+        return m
 
     def split(self,
               axis='auto',
