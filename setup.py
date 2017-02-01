05789c2dda5bb6a42a009283dbf6ad7cdb4a7155
Francisco de la Peña <frandelapena@gmail.com>
2017-02-01 11:21:32 +0000

    Add missing edax testing files to setup

--- setup.py
+++ setup.py
@@ -345,6 +345,7 @@ with update_version_when_dev() as version:
                 'tests/io/dm4_2D_data/*.dm4',
                 'tests/io/dm4_3D_data/*.dm4',
                 'tests/io/dm3_locale/*.dm3',
+                'tests/io/edax_files/*',
                 'tests/io/FEI_new/*.emi',
                 'tests/io/FEI_new/*.ser',
                 'tests/io/FEI_new/*.npy',
