b2921707a1d8d86fb85b9891213d7f163cd0266f
Petras Jokubauskas <klavishas@gmail.com>
2017-03-15 17:04:20 +0100

    typo fix

--- hyperspy/tests/io/test_bcf.py
+++ hyperspy/tests/io/test_bcf.py
@@ -16,7 +16,7 @@ test_files = ['P45_instructively_packed_16bit_compressed.bcf',
               'test_TEM.bcf',
               'Hitachi_TM3030Plus.bcf',
               'over16bit.bcf',
-              'bcf_v2_50x50pix.bcf']
+              'bcf_v2_50x50px.bcf']
 np_file = ['P45_16bit.npy', 'P45_16bit_ds.npy']
 
 my_path = os.path.dirname(__file__)
