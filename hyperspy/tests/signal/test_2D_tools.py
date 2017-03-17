08f0f1430b97979190ce9cad8e06e9a278dec232
dnjohnstone <duncanjohnstone@live.co.uk>
2017-03-17 18:54:18 +0000

    fix testing for pytest

--- hyperspy/tests/signal/test_2D_tools.py
+++ hyperspy/tests/signal/test_2D_tools.py
@@ -18,6 +18,7 @@
 
 import sys
 from unittest import mock
+import pytest
 
 
 import numpy.testing as npt
@@ -142,90 +143,82 @@ def test_add_ramp_lazy():
     npt.assert_almost_equal(s.data.compute(), 0)
 
 
-class TestFindPeaks2D:
+peak_methods = ['skimage', 'max', 'minmax', 'zaefferer', 'stat', 'laplacian_of_gaussians', 'difference_of_gaussians']
 
-    def setUp(self):
-        coefficients = np.array(
-            [350949.04890400 + 0.j, -22003.98742841 + 51494.56650429j,
-             37292.52741553 + 38067.97686711j, 37292.52741553 - 38067.97686711j,
-             -22003.98742841 - 51494.56650429j]
-        )
-        coordinates = np.array([[0, 26, 30, 994, 998],
-                                [0, 1003, 39, 985, 21]]
-        )
-        dense = np.zeros((1024, 1024), dtype=complex)
-        dense[coordinates[0], coordinates[1]] = coefficients
-        dense = hs.signals.Signal2D(np.real(np.fft.ifft2(dense)))
-        self.dense = dense.isig[500:550, 500:550]
+coefficients = np.array(
+    [350949.04890400 + 0.j, -22003.98742841 + 51494.56650429j,
+     37292.52741553 + 38067.97686711j, 37292.52741553 - 38067.97686711j,
+     -22003.98742841 - 51494.56650429j]
+)
+coordinates = np.array([[0, 26, 30, 994, 998],
+                        [0, 1003, 39, 985, 21]]
+)
+dense = np.zeros((1024, 1024), dtype=complex)
+dense[coordinates[0], coordinates[1]] = coefficients
+dense = hs.signals.Signal2D(np.real(np.fft.ifft2(dense)))
+dense = dense.isig[500:550, 500:550]
 
-        coefficients = np.array(
-            [10, 5, 86, 221, 6, 95, 70, 12, 255, 5, 255, 3, 23,
-             24, 77, 255, 11, 255, 8, 35, 195, 165, 27, 255, 8, 14,
-             255, 21, 53, 107, 255, 18, 255, 4, 26, 255, 39, 27, 255,
-             6, 255, 7, 13, 37, 35, 9, 83]
-        )
-        coordinates = np.array(
-            [[3, 40],    [3, 138],  [9, 67],   [14, 95],   [20, 23],
-             [20, 122],  [26, 51],  [26, 100], [31, 78],   [31, 128],
-             [37, 107],  [38, 7],   [43, 34],  [43, 84],   [43, 134],
-             [49, 62],   [49, 112], [54, 90],  [60, 17],   [60, 67],
-             [60, 118],  [66, 45],  [66, 96],  [72, 73],   [72, 124],
-             [77, 51],   [77, 101], [83, 28],  [83, 79],   [83, 130],
-             [89, 57],   [89, 107], [95, 85],  [101, 12],  [101, 62],
-             [101, 113], [106, 40], [107, 91], [112, 68],  [113, 119],
-             [119, 97],  [124, 23], [124, 74], [124, 125], [130, 51],
-             [130, 103], [136, 80]])
-        sparse = np.zeros((144, 144))
-        xs, ys = np.ogrid[:144, :144]
-        for (x0, y0), a in zip(coordinates, coefficients):
-            sparse += a * sp.stats.norm.pdf(xs, x0)*sp.stats.norm.pdf(ys, y0)
-        sparse = sparse[50:100, 50:100]
-        self.sparse0d = hs.signals.Signal2D(sparse)
-        self.sparse1d = hs.signals.Signal2D(np.array([sparse for i in range(2)]))
-        self.sparse2d = hs.signals.Signal2D(np.array([[sparse for i in range(2)] for j in range(2)]))
-        xref, yref = 72, 72
-        ref = np.zeros((144, 144))
-        ref += 100 * sp.stats.norm.pdf(xs, xref)*sp.stats.norm.pdf(ys, yref)
+coefficients = np.array(
+    [10, 5, 86, 221, 6, 95, 70, 12, 255, 5, 255, 3, 23,
+     24, 77, 255, 11, 255, 8, 35, 195, 165, 27, 255, 8, 14,
+     255, 21, 53, 107, 255, 18, 255, 4, 26, 255, 39, 27, 255,
+     6, 255, 7, 13, 37, 35, 9, 83]
+)
+coordinates = np.array(
+    [[3, 40],    [3, 138],  [9, 67],   [14, 95],   [20, 23],
+     [20, 122],  [26, 51],  [26, 100], [31, 78],   [31, 128],
+     [37, 107],  [38, 7],   [43, 34],  [43, 84],   [43, 134],
+     [49, 62],   [49, 112], [54, 90],  [60, 17],   [60, 67],
+     [60, 118],  [66, 45],  [66, 96],  [72, 73],   [72, 124],
+     [77, 51],   [77, 101], [83, 28],  [83, 79],   [83, 130],
+     [89, 57],   [89, 107], [95, 85],  [101, 12],  [101, 62],
+     [101, 113], [106, 40], [107, 91], [112, 68],  [113, 119],
+     [119, 97],  [124, 23], [124, 74], [124, 125], [130, 51],
+     [130, 103], [136, 80]])
+sparse = np.zeros((144, 144))
+xs, ys = np.ogrid[:144, :144]
+for (x0, y0), a in zip(coordinates, coefficients):
+    sparse += a * sp.stats.norm.pdf(xs, x0)*sp.stats.norm.pdf(ys, y0)
+sparse = sparse[50:100, 50:100]
+sparse0d = hs.signals.Signal2D(sparse)
+sparse1d = hs.signals.Signal2D(np.array([sparse for i in range(2)]))
+sparse2d = hs.signals.Signal2D(np.array([[sparse for i in range(2)] for j in range(2)]))
+xref, yref = 72, 72
+ref = np.zeros((144, 144))
+ref += 100 * sp.stats.norm.pdf(xs, xref)*sp.stats.norm.pdf(ys, yref)
 
-        self.ref = hs.signals.Signal2D(ref)
-        ans = np.empty((1,), dtype=object)
-        ans[0] = np.array([[xref, yref]])
-        self.ans = ans
+ref = hs.signals.Signal2D(ref)
+ans = np.empty((1,), dtype=object)
+ans[0] = np.array([[xref, yref]])
 
-        self.methods = ['skimage', 'max', 'minmax', 'zaefferer', 'stat', 'laplacian_of_gaussians', 'difference_of_gaussians']
-        self.datasets = {'dense': self.dense, 'sparse0d': self.sparse0d, 'sparse1d': self.sparse1d, 'sparse2d': self.sparse2d}
-        self.thetests = {'creates_array': self.creates_array, 'peaks_match_input': self.peaks_match_input, 'peaks_are_coordinates': self.peaks_are_coordinates}
 
-    def test_properties(self):
-        self.setUp()  # Test generation does not explicitly call setUp()
-        for method in self.methods:
-            for dataset in self.datasets:
-                for each_test in self.thetests:
-                    yield self.thetests[each_test], method, self.datasets[dataset]
+datasets = [dense, sparse0d, sparse1d, sparse2d]
 
-    def test_answers(self):
-        self.setUp()  # Test generation does not explicitly call setUp()
-        for method in self.methods:
-            yield self.gets_right_answer, method, self.ref, self.ans
+@pytest.mark.parametrize('method', peak_methods)
+@pytest.mark.parametrize('dataset', datasets)
+def test_creates_signal(method, dataset):
+    peaks = dataset.find_peaks2D(method=method)
+    assert isinstance(peaks, hs.signals.BaseSignal)
 
-    def creates_signal(self, method, dataset):
-        peaks = dataset.find_peaks2D(method=method)
-        assert isinstance(peaks, hs.signal.BaseSignal)
+@pytest.mark.parametrize('method', peak_methods)
+@pytest.mark.parametrize('dataset', datasets)
+def test_peaks_match_input(method, dataset):
+    peaks = dataset.find_peaks2D(method=method)
+    signal_shape = dataset.axes_manager.navigation_shape[::-1] if dataset.axes_manager.navigation_size > 0 else (1,)
+    peaks_shape = peaks.axes_manager.navigation_shape[::-1] if peaks.axes_manager.navigation_size > 0 else (1,)
+    npt.assert_equal(peaks_shape, signal_shape)
 
-    def peaks_match_input(self, method, dataset):
-        peaks = dataset.find_peaks2D(method=method)
-        signal_shape = dataset.axes_manager.navigation_shape[::-1] if dataset.axes_manager.navigation_size > 0 else (1,)
-        peaks_shape = peaks.axes_manager.navigation_shape[::-1] if peaks.axes_manager.navigation_size > 0 else (1,)
-        npt.assert_equal(peaks_shape, signal_shape)
+@pytest.mark.parametrize('method', peak_methods)
+@pytest.mark.parametrize('dataset', datasets)
+def test_peaks_are_coordinates(method, dataset):
+    peaks = dataset.find_peaks2D(method=method)
+    peak_shapes = np.array([peak.shape for peak in peaks.data.flatten()])
+    assert np.all(peak_shapes[:, 1] == 2)
 
-    def peaks_are_coordinates(self, method, dataset):
-        peaks = dataset.find_peaks2D(method=method)
-        peak_shapes = np.array([peak.shape for peak in peaks.data.flatten()])
-        npt.assert_true(np.all(peak_shapes[:, 1] == 2))
-
-    def gets_right_answer(self, method, dataset, answer):
-        peaks = dataset.find_peaks2D()
-        npt.assert_true(np.all(peaks.data[0] == answer[0]))
+@pytest.mark.parametrize('method', peak_methods)
+def test_gets_right_answer(method):
+    peaks = ref.find_peaks2D()
+    assert np.all(peaks.data[0] == ans[0])
 
 if __name__ == '__main__':
     import pytest
