1faa532ba371e3df998ab80a09f1a882509d0580
Francisco de la Peña <frandelapena@gmail.com>
2017-03-22 18:02:11 +0000

    Add labelme_sandwich

--- hyperspy/gui_ipywidgets/utils.py
+++ hyperspy/gui_ipywidgets/utils.py
@@ -12,5 +12,20 @@ FORM_ITEM_LAYOUT = ipywidgets.Layout(
 def labelme(label, widget):
     if label is Undefined:
         label = ""
+    if not isinstance(label, ipywidgets.Label):
+        label = ipywidgets.Label(label)
     return ipywidgets.HBox(
-        [ipywidgets.Label(label), widget], layout=FORM_ITEM_LAYOUT)
+        [label, widget], layout=FORM_ITEM_LAYOUT)
+
+def labelme_sandwich(label1, widget, label2):
+    if label1 is Undefined:
+        label1 = ""
+    if label2 is Undefined:
+        label2 = ""
+    if not isinstance(label1, ipywidgets.Label):
+        label1 = ipywidgets.Label(label1)
+    if not isinstance(label2, ipywidgets.Label):
+        label2 = ipywidgets.Label(label2)
+    return ipywidgets.HBox(
+        [label1, widget, label2],
+        layout=FORM_ITEM_LAYOUT)
