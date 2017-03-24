9d3aa397f3dcff2817c3c24bddf58ff53685f241
Francisco de la Peña <frandelapena@gmail.com>
2017-03-24 17:39:51 +0000

    Add ui_registry

---
+++ hyperspy/ui_registry.py
@@ -0,0 +1,14 @@
+'''Registry of user interface widgets.
+
+Format {"tool_key" : {"widget_name" : <function(obj, display, **kwargs)>}}
+
+The ``tool_key` is defined by the "model function" to which the widget provides
+and user interface. That function gets the widget function from this registry
+and executes it passing the ``obj``, ``display`` and any extra keyword
+arguments. When ``display`` is true, ``function`` displays the widget. If
+``False`` it returns a dictionary with whatever is needed to display the
+widgets externally (usually for testing or customisation purposes). 
+
+'''
+
+ui_registry = {}
