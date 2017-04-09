56bc7eb6f269b67f4762de8c5fc55b4869cf56fc
Francisco de la Peña <frandelapena@gmail.com>
2017-04-09 17:10:37 +0100

    Add wdict to ipywidgets tools

--- hyperspy/gui_ipywidgets/tools.py
+++ hyperspy/gui_ipywidgets/tools.py
@@ -13,6 +13,7 @@ from hyperspy.signal_tools import SPIKES_REMOVAL_INSTRUCTIONS
 @add_display_arg
 def interactive_range_ipy(obj, **kwargs):
     # Define widgets
+    wdict = {}
     axis = obj.axis
     left = ipywidgets.FloatText(disabled=True)
     right = ipywidgets.FloatText(disabled=True)
@@ -28,6 +29,12 @@ def interactive_range_ipy(obj, **kwargs):
     apply = ipywidgets.Button(
         description="Apply",
         tooltip="Perform the operation using the selected range.")
+    wdict["left"] = left
+    wdict["right"] = right
+    wdict["units"] = units
+    wdict["help"] = help
+    wdict["close_button"] = close
+    wdict["apply_button"] = apply
 
     # Connect
     link_traits((obj, "ss_left_value"), (left, "value"))
@@ -53,13 +60,17 @@ def interactive_range_ipy(obj, **kwargs):
         obj.span_selector_switch(False)
         box.close()
     close.on_click(on_close_clicked)
-    return box
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
 
 
 @register_ipy_widget(toolkey="Signal1D.calibrate")
 @add_display_arg
 def calibrate_ipy(obj, **kwargs):
     # Define widgets
+    wdict = {}
     axis = obj.axis
     left = ipywidgets.FloatText(disabled=True)
     right = ipywidgets.FloatText(disabled=True)
@@ -115,12 +126,28 @@ def calibrate_ipy(obj, **kwargs):
         obj.span_selector_switch(False)
         box.close()
     close.on_click(on_close_clicked)
-    return box
+
+    wdict["left"] = left
+    wdict["right"] = right
+    wdict["offset"] = offset
+    wdict["scale"] = scale
+    wdict["new_left"] = new_left
+    wdict["new_right"] = new_right
+    wdict["units"] = units
+    wdict["help"] = help
+    wdict["close_button"] = close
+    wdict["apply_button"] = apply
+
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
 
 
 @register_ipy_widget(toolkey="Signal1D.smooth_savitzky_golay")
 @add_display_arg
 def smooth_savitzky_golay_ipy(obj, **kwargs):
+    wdict = {}
     window_length = OddIntSlider(
         value=3, step=2, min=3, max=max(int(obj.axis.size * 0.25), 3))
     polynomial_order = ipywidgets.IntSlider(value=3, min=1,
@@ -152,6 +179,13 @@ def smooth_savitzky_golay_ipy(obj, **kwargs):
         ipywidgets.HBox((apply, close))
     ])
 
+    wdict["window_length"] = window_length
+    wdict["polynomial_order"] = polynomial_order
+    wdict["differential_order"] = differential_order
+    wdict["color"] = color
+    wdict["close_button"] = close
+    wdict["apply_button"] = apply
+
     def on_apply_clicked(b):
         obj.apply()
     apply.on_click(on_apply_clicked)
@@ -160,12 +194,16 @@ def smooth_savitzky_golay_ipy(obj, **kwargs):
         obj.close()
         box.close()
     close.on_click(on_close_clicked)
-    return box
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
 
 
 @register_ipy_widget(toolkey="Signal1D.smooth_lowess")
 @add_display_arg
 def smooth_lowess_ipy(obj, **kwargs):
+    wdict = {}
     smoothing_parameter = ipywidgets.FloatSlider(min=0, max=1)
     number_of_iterations = ipywidgets.IntText()
     color = ipywidgets.ColorPicker()
@@ -184,6 +222,11 @@ def smooth_lowess_ipy(obj, **kwargs):
         labelme("Color", color),
         ipywidgets.HBox((apply, close))
     ])
+    wdict["smoothing_parameter"] = smoothing_parameter
+    wdict["number_of_iterations"] = number_of_iterations
+    wdict["color"] = color
+    wdict["close_button"] = close
+    wdict["apply_button"] = apply
 
     def on_apply_clicked(b):
         obj.apply()
@@ -193,12 +236,16 @@ def smooth_lowess_ipy(obj, **kwargs):
         obj.close()
         box.close()
     close.on_click(on_close_clicked)
-    return box
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
 
 
 @register_ipy_widget(toolkey="Signal1D.smooth_total_variation")
 @add_display_arg
 def smooth_tv_ipy(obj, **kwargs):
+    wdict = {}
     smoothing_parameter = ipywidgets.FloatSlider(min=0.1, max=1000)
     smoothing_parameter_max = ipywidgets.FloatText(
         value=smoothing_parameter.max)
@@ -213,6 +260,11 @@ def smooth_tv_ipy(obj, **kwargs):
     link_traits((smoothing_parameter_max, "value"),
                 (smoothing_parameter, "max"))
     link_traits((obj, "line_color_ipy"), (color, "value"))
+    wdict["smoothing_parameter"] = smoothing_parameter
+    wdict["smoothing_parameter_max"] = smoothing_parameter_max
+    wdict["color"] = color
+    wdict["close_button"] = close
+    wdict["apply_button"] = apply
     box = ipywidgets.VBox([
         labelme("Weight", smoothing_parameter),
         labelme("Weight max", smoothing_parameter_max),
@@ -228,12 +280,16 @@ def smooth_tv_ipy(obj, **kwargs):
         obj.close()
         box.close()
     close.on_click(on_close_clicked)
-    return box
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
 
 
 @register_ipy_widget(toolkey="Signal1D.smooth_butterworth")
 @add_display_arg
 def smooth_butterworth(obj, **kwargs):
+    wdict = {}
     cutoff = ipywidgets.FloatSlider(min=0.01, max=1.)
     order = ipywidgets.IntText()
     type_ = ipywidgets.Dropdown(options=("low", "high"))
@@ -247,6 +303,11 @@ def smooth_butterworth(obj, **kwargs):
     link_traits((obj, "cutoff_frequency_ratio"), (cutoff, "value"))
     link_traits((obj, "type"), (type_, "value"))
     link_traits((obj, "order"), (order, "value"))
+    wdict["cutoff"] = cutoff
+    wdict["order"] = order
+    wdict["type_"] = type_
+    wdict["close_button"] = close
+    wdict["apply_button"] = apply
     box = ipywidgets.VBox([
         labelme("Cutoff frequency ration", cutoff),
         labelme("Type", type_),
@@ -262,18 +323,23 @@ def smooth_butterworth(obj, **kwargs):
         obj.close()
         box.close()
     close.on_click(on_close_clicked)
-    return box
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
 
 
 @register_ipy_widget(toolkey="Signal1D.contrast_editor")
 @add_display_arg
 def image_constast_editor_ipy(obj, **kwargs):
+    wdict = {}
     left = ipywidgets.FloatText(disabled=True)
     right = ipywidgets.FloatText(disabled=True)
     help = ipywidgets.HTML(
         "Click on the histogram figure and drag to the right to select a"
         "range. Press `Apply` to set the new contrast limits, `Reset` to reset "
         "them or `Close` to cancel.",)
+    wdict["help"] = help
     help = ipywidgets.Accordion(children=[help])
     help.set_title(0, "Help")
     close = ipywidgets.Button(
@@ -285,6 +351,11 @@ def image_constast_editor_ipy(obj, **kwargs):
     reset = ipywidgets.Button(
         description="Reset",
         tooltip="Reset the contrast to the previous value.")
+    wdict["left"] = left
+    wdict["right"] = right
+    wdict["close_button"] = close
+    wdict["apply_button"] = apply
+    wdict["reset_button"] = reset
 
     # Connect
     link_traits((obj, "ss_left_value"), (left, "value"))
@@ -309,12 +380,16 @@ def image_constast_editor_ipy(obj, **kwargs):
         obj.close()
         box.close()
     close.on_click(on_close_clicked)
-    return box
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
 
 
 @register_ipy_widget(toolkey="Signal1D.remove_background")
 @add_display_arg
 def remove_background_ipy(obj, **kwargs):
+    wdict = {}
     fast = ipywidgets.Checkbox()
     help = ipywidgets.HTML(
         "Click on the signal figure and drag to the right to select a"
@@ -322,6 +397,7 @@ def remove_background_ipy(obj, **kwargs):
         "If fast is checked, the background parameters are estimated using a "
         "fast (analytical) method that can compromise accuray. When unchecked "
         "non linear least squares is employed instead.",)
+    wdict["help"] = help
     help = ipywidgets.Accordion(children=[help])
     help.set_title(0, "Help")
     close = ipywidgets.Button(
@@ -352,6 +428,10 @@ def remove_background_ipy(obj, **kwargs):
     enable_poly_order(change=Dummy())
     link_traits((obj, "polynomial_order"), (polynomial_order, "value"))
     link_traits((obj, "fast"), (fast, "value"))
+    wdict["fast"] = fast
+    wdict["polynomial_order"] = polynomial_order
+    wdict["background_type"] = background_type
+    wdict["apply_button"] = apply
     box = ipywidgets.VBox([
         labelme("Background type", background_type),
         labeled_polyorder,
@@ -369,19 +449,24 @@ def remove_background_ipy(obj, **kwargs):
         obj.span_selector_switch(False)
         box.close()
     close.on_click(on_close_clicked)
-    return box
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
 
 
 @register_ipy_widget(toolkey="Signal1D.spikes_removal_tool")
 @add_display_arg
 def spikes_removal_ipy(obj, **kwargs):
+    wdict = {}
     threshold = ipywidgets.FloatText()
     add_noise = ipywidgets.Checkbox()
     default_spike_width = ipywidgets.IntText()
     interpolator_kind = enum2dropdown(obj.traits()["interpolator_kind"])
     spline_order = ipywidgets.IntSlider(min=1, max=10)
     progress_bar = ipywidgets.IntProgress(max=len(obj.coordinates) - 1)
-    help = ipywidgets.HTML(value=SPIKES_REMOVAL_INSTRUCTIONS.replace('\n', '<br/>'))
+    help = ipywidgets.HTML(
+        value=SPIKES_REMOVAL_INSTRUCTIONS.replace('\n', '<br/>'))
     help = ipywidgets.Accordion(children=[help])
     help.set_title(0, "Help")
 
@@ -401,6 +486,17 @@ def spikes_removal_ipy(obj, **kwargs):
     remove = ipywidgets.Button(
         description="Remove spike",
         tooltip="Remove spike and find next one.")
+    wdict["threshold"] = threshold
+    wdict["add_noise"] = add_noise
+    wdict["default_spike_width"] = default_spike_width
+    wdict["interpolator_kind"] = interpolator_kind
+    wdict["spline_order"] = spline_order
+    wdict["progress_bar"] = progress_bar
+    wdict["show_diff_button"] = show_diff
+    wdict["close_button"] = close
+    wdict["next_button"] = next
+    wdict["previous_button"] = previous
+    wdict["remove_button"] = remove
 
     def on_show_diff_clicked(b):
         obj._show_derivative_histogram_fired()
@@ -461,4 +557,7 @@ def spikes_removal_ipy(obj, **kwargs):
         obj.span_selector_switch(False)
         box.close()
     close.on_click(on_close_clicked)
-    return box
+    return {
+        "widget": box,
+        "wdict": wdict,
+        }
