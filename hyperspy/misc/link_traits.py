d3908bfbfbabfed6b54e91222324f82b83cc7ab8
Francisco de la Peña <frandelapena@gmail.com>
2017-04-10 19:23:38 +0100

    New directional_link

--- hyperspy/misc/link_traits.py
+++ hyperspy/misc/link_traits.py
@@ -13,6 +13,9 @@ def has_traitlets(obj):
 class link_traits(traitlets.link):
     """Link traits from different objects together so they remain in sync.
 
+    This is a sublclass of traitlets.links that adds support for linking
+    traitlets traits and enthought traits.
+
     Parameters
     ----------
     source : (object / attribute name) pair
@@ -23,6 +26,11 @@ class link_traits(traitlets.link):
 
     >>> c = link((src, 'value'), (tgt, 'value'))
     >>> src.value = 5  # updates other objects as well
+
+    See Also
+    --------
+    directional_link
+
     """
 
     def __init__(self, source, target):
@@ -77,4 +85,62 @@ class link_traits(traitlets.link):
                 self._update_source_traits, name=self.target[1], remove=True)
         else:
             self.target[0].unobserve(self._update_source, names=self.target[1])
+        self.source, self.target = None, None
+
+class directional_link(traitlets.dlink):
+    """Link the trait of a source object with traits of target objects.
+
+    This is a sublclass of traitlets.directional_link that adds support for
+    linking traitlets traits and enthought traits.
+
+    Parameters
+    ----------
+    source : (object, attribute name) pair
+    target : (object, attribute name) pair
+    transform: callable (optional)
+        Data transformation between source and target.
+    Examples
+    --------
+    >>> c = directional_link((src, 'value'), (tgt, 'value'))
+    >>> src.value = 5  # updates target objects
+    >>> tgt.value = 6  # does not update source object
+
+    See Also
+    --------
+    link
+
+    """
+    def __init__(self, source, target, transform=None):
+        self._transform = transform if transform else lambda x: x
+        self.source, self.target = source, target
+        try:
+            source_value = getattr(source[0], source[1])
+            if source_value not in (t.Undefined, traitlets.Undefined):
+                setattr(target[0], target[1], self._transform(source_value))
+        finally:
+            if has_traits(source[0]):
+                source[0].on_trait_change(
+                    self._update_traits, name=source[1])
+            elif has_traitlets(source[0]):
+                source[0].observe(self._update, names=source[1])
+            else:
+                raise ValueError(
+                    "source must contains either enthought traits or "
+                    "traitlets.")
+
+    def _update_traits(self, name, new):
+        if self.updating:
+            return
+        with self._busy_updating():
+            if new is t.Undefined and has_traitlets(self.target[0]):
+                new = self.target[0].traits()[self.target[1]].default_value
+            setattr(self.target[0], self.target[1], self._transform(new))
+
+
+    def unlink(self):
+        if isinstance(self.source[0], t.HasTraits):
+            self.source[0].on_trait_change(
+                self._update_target_traits, name=self.source[1], remove=True)
+        else:
+            self.source[0].unobserve(self._update_target, names=self.source[1])
         self.source, self.target = None, None
