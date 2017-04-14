1e746208cd38c93bd7a56179ba07ec1994550c87
Francisco de la Peña <frandelapena@gmail.com>
2017-04-14 09:44:06 +0100

    Automatic style corrections courtesy of autopep8

--- hyperspy/_signals/signal1d.py
+++ hyperspy/_signals/signal1d.py
@@ -978,7 +978,7 @@ SignalDimensionError if the signal dimension is not 1.
         if cutoff_frequency_ratio is not None:
             smoother.cutoff_frequency_ratio = cutoff_frequency_ratio
             smoother.type = type
-            smoother.order =order
+            smoother.order = order
             smoother.apply()
         else:
             return smoother.gui(display=display, toolkit=toolkit)
