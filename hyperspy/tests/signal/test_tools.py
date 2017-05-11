7786e2abef734bac5247525f18f7fba364165ca4
Francisco de la Peña <frandelapena@gmail.com>
2017-05-11 18:16:17 +0100

    Automatic style corrections courtesy of autopep8

--- hyperspy/tests/signal/test_tools.py
+++ hyperspy/tests/signal/test_tools.py
@@ -857,7 +857,7 @@ def test_spikes_removal_tool():
     sr.threshold = 1.5
     sr.find()
     assert s.axes_manager.indices == (0, 1)
-    sr.threshold  = 0.5
+    sr.threshold = 0.5
     assert s.axes_manager.indices == (0, 0)
     sr.find()
     assert s.axes_manager.indices == (2, 0)
