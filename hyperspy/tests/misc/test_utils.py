49cf8fdf518a9e7ae97da56334641c13f6c2e291
Francisco de la Peña <frandelapena@gmail.com>
2017-05-17 12:11:35 +0100

    Automatic style corrections courtesy of autopep8

--- hyperspy/tests/misc/test_utils.py
+++ hyperspy/tests/misc/test_utils.py
@@ -2,7 +2,6 @@ from hyperspy.misc.utils import signal_range_from_roi
 from hyperspy import roi
 
 
-
 def test_signal_range_from_roi():
     sr = roi.SpanROI(20, 50)
     left, right = signal_range_from_roi(sr)
