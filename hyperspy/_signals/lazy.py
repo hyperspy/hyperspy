4c64d0c8dc9f3e57874faea9993f01c5400b34ab
Francisco de la Peña <frandelapena@gmail.com>
2017-05-25 20:18:17 +0100

    Raise an error when lazy rebinning is not supported

--- hyperspy/_signals/lazy.py
+++ hyperspy/_signals/lazy.py
@@ -231,36 +231,43 @@ class LazySignal(BaseSignal):
         raise lazyerror
 
     def rebin(self, new_shape=None, scale=None, crop=False, out=None):
-        #Series of if statements to check that only one out of new_shape or scale
-        #has been given. New_shape is then converted to scale. If both or neither
-        #are given the function raises and error and wont run.
-        if new_shape == None and scale == None:
+        if new_shape is None and scale is None:
             raise ValueError("One of new_shape, or scale must be specified")
-        elif new_shape != None and scale != None:
+        elif new_shape is None and scale is None:
             raise ValueError("Only one out of new_shape or scale should be specified.\
                             Not both.")
-        elif new_shape != None:
-            scale = []
-            for i, axis in enumerate(a.shape):
-                scale.append(a.shape[i]/new_shape[i])
+        elif new_shape:
+            if len(new_shape) != len(self.data.shape):
+                raise ValueError("Wrong new_shape size")
+            new_shape_in_array = []
+            for axis in self.axes_manager._axes:
+                new_shape_in_array.append(new_shape[axis.index_in_axes_manager])
+            if (np.array(self.data.shape) % np.array(new_shape_in_array)).any():
+                raise NotImplementedError(
+                    "Lazy rebin requires that the new shape is a divisor "
+                    "of the original signal shape e.g. if original shape "
+                    "(10| 6), new_shape=(5| 3) is valid, (3 | 4) is not.")
+            factors = (np.array(self.data.shape) / np.array(new_shape_in_array))
         else:
-            new_shape = new_shape
-            scale = scale
-
-        new_shape_in_array = []
-        new_shape = np.zeros_like(scale)
-        for axis, s in enumerate(scale):
-            dim = (math.floor(self.data.shape[axis] / s) if crop
-            else math.ceil(self.data.shape[axis] / s))
-            new_shape[axis] = dim
-        for axis in self.axes_manager._axes:
-            new_shape_in_array.append(new_shape[axis.index_in_axes_manager])
-        factors = np.asarray(scale)
+            if len(scale) != len(self.data.shape):
+                raise ValueError("Wrong shape size")
+            factors  = []
+            for axis in self.axes_manager._axes:
+                factors.append(scale[axis.index_in_axes_manager])
+            factors = np.array(factors)
+            if (factors < 1).any():
+                raise NotImplementedError(
+                    "Lazy rebin requires scale to be > 1 and divisor of the "
+                    "original signal shape")
+            elif (np.array(self.data.shape) % factors).any():
+                raise NotImplementedError(
+                    "Lazy rebin requires scale to divisor of the "
+                    "original signal shape")
         axis = {ax.index_in_array: ax
                 for ax in self.axes_manager._axes}[factors.argmax()]
         self._make_lazy(axis=axis)
-        return super().rebin(scale=scale, crop=crop, out=out)
-
+        return super().rebin(new_shape=new_shape,
+                             scale=scale, crop=crop, out=out)
     rebin.__doc__ = BaseSignal.rebin.__doc__
 
     def __array__(self, dtype=None):
