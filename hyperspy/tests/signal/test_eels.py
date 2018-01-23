088bd853c6676afc61bb6318cf4457e57b56bfed
Francisco de la Peña <frandelapena@gmail.com>
2018-01-23 13:33:33 +0100

    Disable debug print_stats and lazy comp

--- hyperspy/tests/signal/test_eels.py
+++ hyperspy/tests/signal/test_eels.py
@@ -157,12 +157,10 @@ class TestAlignZLP:
         mask.data[[3, 5]] = (True, True)
         s.align_zero_loss_peak(
             calibrate=True,
-            print_stats=True,
+            print_stats=False,
             show_progressbar=None,
             mask=mask)
         zlpc = s.estimate_zero_loss_peak_centre(mask=mask)
-        if zlpc._lazy:
-            zlpc.compute()
         np.testing.assert_allclose(np.nanmean(zlpc.data), 0,
                                    atol=np.finfo(float).eps)
         np.testing.assert_allclose(np.nanstd(zlpc.data), 0,
