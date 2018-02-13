0cf90919a19b59b44c835dfa13652af2d79a61a0
Petras Jokubauskas <klavishas@gmail.com>
2018-02-13 16:46:19 +0100

    revert back oversimplified single file system file tree method reader, so that EDSdatabase from EBSD (containing dozens of files) would be able to load.

--- hyperspy/io_plugins/bcf.py
+++ hyperspy/io_plugins/bcf.py
@@ -335,9 +335,27 @@ class SFS_reader(object):
         SFSTreeItem
         """
         with open(self.filename, 'rb') as fn:
-            # file tree do not exceed one chunk in bcf:
-            fn.seek(self.chunksize * self.tree_address + 0x138)
-            raw_tree = fn.read(0x200 * self.n_tree_items)
+            #check if file tree do not exceed one chunk:
+            n_file_tree_chunks = ceil((self.n_tree_items * 0x200) /
+                                      (self.chunksize - 0x20))
+            if n_file_tree_chunks == 1:
+                # file tree do not exceed one chunk in bcf:
+                fn.seek(self.chunksize * self.tree_address + 0x138)
+                raw_tree = fn.read(0x200 * self.n_tree_items)
+            else:
+                temp_str = io.BytesIO()
+                tree_address = self.tree_address
+                tree_items_in_chunk = (self.chunksize - 0x20) // 0x200
+                for i in range(n_file_tree_chunks):
+                    # jump to tree/list address:
+                    fn.seek(self.chunksize * tree_address + 0x118)
+                    # next tree/list address:
+                    tree_address = strct_unp('<I', fn.read(4))[0]
+                    fn.seek(28, 1)
+                    temp_str.write(fn.read(tree_items_in_chunk * 0x200))
+                temp_str.seek(0)
+                raw_tree = temp_str.read(self.n_tree_items * 0x200)
+                temp_str.close()
             temp_item_list = [SFSTreeItem(raw_tree[i * 0x200:(i + 1) * 0x200],
                                           self) for i in range(self.n_tree_items)]
             # temp list with parents of items
