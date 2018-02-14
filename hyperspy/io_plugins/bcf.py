c368410249592dc6b1d3c4900d9c82df8e674a4d
Petras Jokubauskas <klavishas@gmail.com>
2018-02-14 16:21:31 +0100

    make the overview marker optional as some Esprit implementation saves no rectangle info

--- hyperspy/io_plugins/bcf.py
+++ hyperspy/io_plugins/bcf.py
@@ -663,16 +663,17 @@ class HyperHeader(object):
                 #"@Type='TRTRectangleOverlayElement' and "
                 "@Name='Map']/TRTSolidOverlayElement/"
                 "TRTBasicLineOverlayElement/TRTOverlayElement")
-            over_rect = dictionarize(rect_node)['TRTOverlayElement']['Rect']
-            rect = {'y1': over_rect['Top'] * self.y_res,
-                    'x1': over_rect['Left'] * self.x_res,
-                    'y2': over_rect['Bottom'] * self.y_res,
-                    'x2': over_rect['Right'] * self.x_res}
-            over_dict = {'marker_type': 'Rectangle',
-                         'plot_on_signal': True,
-                         'data': rect,
-                         'marker_properties': {'color': 'yellow',
-                                               'linewidth': 2}}
+            if rect_node is not None:
+                over_rect = dictionarize(rect_node)['TRTOverlayElement']['Rect']
+                rect = {'y1': over_rect['Top'] * self.y_res,
+                        'x1': over_rect['Left'] * self.x_res,
+                        'y2': over_rect['Bottom'] * self.y_res,
+                        'x2': over_rect['Right'] * self.x_res}
+                over_dict = {'marker_type': 'Rectangle',
+                            'plot_on_signal': True,
+                            'data': rect,
+                            'marker_properties': {'color': 'yellow',
+                                                'linewidth': 2}}
         image = Container()
         image.width = int(xml_node.find('./Width').text)  # in pixels
         image.height = int(xml_node.find('./Height').text)  # in pixels
@@ -692,7 +693,7 @@ class HyperHeader(object):
                 item['metadata']['General'] = {'title': detector_name}
                 item['metadata']['Signal'] = {'signal_type': detector_name,
                                               'record_by': 'image'}
-                if overview:
+                if overview and (rect_node is not None):
                     item['metadata']['Markers'] = {'overview': over_dict}
                 image.images.append(item)
         return image
