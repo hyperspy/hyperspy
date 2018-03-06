74e71c4e5ffae410bd1854cf6d9166e7c88ce34f
Zanetta Pierre-marie <pierre-marie.zanetta@hotmail.fr>
2018-03-06 18:04:37 +0100

    Update physical_background.py

--- hyperspy/_components/physical_background.py
+++ hyperspy/_components/physical_background.py
@@ -26,8 +26,21 @@ from hyperspy.misc.eds.detector_efficiency import detector_efficiency
 from scipy.interpolate import interp1d
 
 def Wpercent(model,E0,quantification):
-    
-    if quantification is None :                 #if quantification is None, this function calculate an  approximation of a quantification based on peaks ratio 
+ """
+Return an array of weight percent for each elements
+
+Parameters
+----------
+model: EDS model
+E0: int
+	The Beam energy
+quantification: None or a list or an array
+	if quantification is None, this function calculate an  approximation of a quantification based on peaks ratio thanks to the function s.get_lines_intensity(). 
+	if quantification is the result of the hyperspy quantification function. This function only convert the result in an array with the same navigation shape than the model and a length equal to the number of elements 
+	if quantification is already an array of weight percent, directly keep the array
+ """
+ 	
+    if quantification is None :                 
         model.signal.set_lines([])
         intensity=model.signal.get_lines_intensity(only_one=True)
         if len(np.shape(intensity[0]))>1 and np.shape(intensity[0])[1]>1 : #for 3D Data
@@ -82,11 +95,11 @@ def Wpercent(model,E0,quantification):
             t=u.sum() 
             for i in range (0,len(u)):
                 weight[i] =u[i] /t*100
-    elif type(quantification) is np.ndarray: # if quantification is already an array of weight percent, directly keep the array
+    elif type(quantification) is np.ndarray: 
         weight=quantification
 
     else:
-        result=quantification # if quantification is the result of the hyperspy quantification function. This function convert the result in an array with the same navigation shape than the model and a length equal to the number of elements 
+        result=quantification
         if 'atomic percent' in result[0].metadata.General.title:
             result=atomic_to_weight(result)
         else:
@@ -107,24 +120,45 @@ def Wpercent(model,E0,quantification):
             for i in range (0,len(result)):            
                 weight[i] =result[i].data
 		    
-
     return weight
 
 
 
 def Mucoef(model,quanti): # this function calculate the absorption coefficient for all energy. This, correspond to the Mu parameter in the function
+ """
+Calculate the mass absorption coefficient for all energy of the model axis for each pixel. This is based on the weigth percent array defined by the Wpercent function
+Return the Mu parameter as a signal (all energy) with same number of elements than the model
+This parameter is calculated at each iteration during the fit
+Parameters
+----------
+model: EDS model
+quanti: Array
+	Must contain an array of weight percent for each elements
+	This array is automaticaly created through the Wpercent function
+ """	
     weight=quanti
     t=(np.linspace(model._signal.axes_manager[-1].offset,model._signal.axes_manager[-1].size*model._signal.axes_manager[-1].scale,model._signal.axes_manager[-1].size/5))
     
     Ac=mass_absorption_mixture(elements=model._signal.metadata.Sample.elements ,weight_percent=weight, energies=t)    
     b=(model._signal.axes_manager.signal_axes[-1].axis)-0.035
-    Ac=np.interp(b,t,Ac)
+    Ac=np.interp(b,t,Ac) # Interpolation allows to gain some time
     
     return Ac
 
 
-def Windowabsorption(model,detector): # this function interpolate the detector efficiency based on dictionnary (create from personnal data) and the signal length. This correspond to the Window parameter  
- 
+def Windowabsorption(model,detector): 
+ """
+Return the detector efficiency as a signal based on a dictionnary (create from personnal data) and the signal length. This correspond to the Window parameter of the physical background class  
+To obtain the same signal length compare to the model, data are interpolated
+
+Parameters
+----------
+model: EDS model
+detector: str
+	The Acquisition detector which correspond to the Dataset
+	String can be 'Polymer_C' / 'Super_X' / '12µm_BE' / '25µm_BE' / '100µm_BE' / 'Polymer_C2' / 'Polymer_C3' 
+	Data are contain in a dictionnary
+ """	
     a=np.array(detector_efficiency[detector])
     b=(model._signal.axes_manager.signal_axes[-1].axis)-0.035
     x =a[:,0]
@@ -138,9 +172,15 @@ class Physical_background(Component):
     Background component based on kramer's law and absorption coefficients
     Attributes
     ----------
-    a : float
-    b : float
-    E0 : int 
+    coefficients : float (length = 2) 
+    	The only two free parameters of this component. If the function fix_background is used those two coefficients are fixed
+    E0 : int
+    	The beam energy of the acquisition
+    Window : int 
+    	Contain the signal of the detector efficiency (calculated thanks to the function Windowabsorption())
+    quanti: dictionnary
+    	Contain the referenced variable quantification (none, result of CL quantification or array) 
+	This dictionnary is call in the function Wpercent() to calculate an array of weight percent with the same dimension than the model and a length which correspond to the number of elements renseigned in the metadata
     """
 
     def __init__(self, E0, detector, quantification, coefficients=1, Window=0, quanti=0):
@@ -203,6 +243,6 @@ class Physical_background(Component):
         Window=Window[self.model.channel_switches]
 	
         emission=(a*100*((E0-x)/x)) #kramer's law 
-	absorption=((1-np.exp(-2*Mu*b*10**-5 ))/((2*Mu*b*10**-5))) #love and scott model
+	absorption=((1-np.exp(-2*Mu*b*10**-5 ))/((2*Mu*b*10**-5))) #love and scott model could be cool to impplement CL model too
 	
         return np.where((x>0.17) & (x<(E0)),(emission*absorption*Window),0) #implement choice for coating correction
