188d50a11fcfdb16571c8885a03a869a9ec67775
Francisco de la Peña <frandelapena@gmail.com>
2018-03-15 15:20:57 +0100

    Add FEI stream readers tests

---
+++ hyperspy/tests/misc/test_fei_stream_readers.py
@@ -0,0 +1,36 @@
+import numpy as np
+import dask.array as da
+
+from hyperspy.misc.io.fei_stream_readers import (
+    array_to_stream, stream_to_sparse_COO_array)
+
+
+def test_dense_stream():
+    arr = np.random.randint(0, 65535, size=(2, 3, 4, 5)).astype("uint16")
+    stream = array_to_stream(arr)
+    arrs = da.from_array(stream_to_sparse_COO_array(
+        stream, spatial_shape=(3, 4), sum_frames=False, channels=5,
+        last_frame=2), chunks=(1, 1, 2, 5))
+    arrs = arrs.compute()
+    assert (arrs == arr).all()
+
+def test_empty_stream():
+    arr = np.zeros((2, 3, 4, 5), dtype="uint16")
+    stream = array_to_stream(arr)
+    arrs = da.from_array(stream_to_sparse_COO_array(
+        stream, spatial_shape=(3, 4), sum_frames=False, channels=5,
+        last_frame=2), chunks=(1, 1, 2, 5))
+    arrs = arrs.compute()
+    assert not arrs.any()
+
+def test_sparse_stream():
+    arr = np.zeros((2, 3, 4, 5), dtype="uint16")
+    arr[0, 0, 0, 0] = 1
+    arr[-1, -1, -1, -1] = 2
+    arr[1, 1, 3, 3] = 3
+    stream = array_to_stream(arr)
+    arrs = da.from_array(stream_to_sparse_COO_array(
+        stream, spatial_shape=(3, 4), sum_frames=False, channels=5,
+        last_frame=2), chunks=(1, 1, 2, 5))
+    arrs = arrs.compute()
+    assert (arrs == arr).all()
