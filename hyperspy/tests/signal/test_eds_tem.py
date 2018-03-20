32aa6b01f13c3564580a5256dee60a9578447534
k8macarthur <k8macarthur@gmail.com>
2018-03-20 16:14:36 +0100

    Set azitmuth_angle to 0

--- hyperspy/tests/signal/test_eds_tem.py
+++ hyperspy/tests/signal/test_eds_tem.py
@@ -148,7 +148,7 @@ class Test_quantification:
         energy_axis.name = "Energy"
         s.set_microscope_parameters(beam_energy=200,
                                     live_time=2.5, tilt_stage=0.0,
-                                    azimuth_angle=None, elevation_angle=35,
+                                    azimuth_angle=0, elevation_angle=35,
                                     energy_resolution_MnKa=130,
                                     beam_current=0.05)
         elements = ['Al', 'Zn']
