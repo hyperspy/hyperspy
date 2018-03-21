d75a24f3e98ce4573e0f621ee4f8ed7192a65c61
k8macarthur <k8macarthur@gmail.com>
2018-03-21 08:37:33 +0100

    Re-add real_time test to checking setting real_time works even if not used for TEM_quant.

--- hyperspy/tests/signal/test_eds_tem.py
+++ hyperspy/tests/signal/test_eds_tem.py
@@ -178,10 +178,11 @@ class Test_quantification:
         np.testing.assert_approx_equal(
             TEM_md.Detector.EDS.energy_resolution_MnKa, 130)
 
+        self.signal.set_microscope_parameters(real_time=3.1)
         self.signal.set_microscope_parameters(probe_area=1.2)
-        TEM_md = self.signal.metadata.Acquisition_instrument.TEM
         np.testing.assert_approx_equal(
             TEM_md.probe_area, 1.2)
+        np.testing.assert_approx_equal(TEM_md.real_time, 3.1)
 
     def test_quant_lorimer(self):
         s = self.signal
