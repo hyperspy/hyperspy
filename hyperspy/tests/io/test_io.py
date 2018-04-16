4226a25591bbfc0fe24f98a92a2601fbb1d9dcbe
Eric Prestat <eric.prestat@gmail.com>
2018-04-16 15:16:34 +0100

    Use hash to test if the file has been written instead of reloading the data.

--- hyperspy/tests/io/test_io.py
+++ hyperspy/tests/io/test_io.py
@@ -17,16 +17,17 @@
 # along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import hashlib
 import numpy as np
 import pytest
 from unittest.mock import patch
 
 from hyperspy.signals import Signal1D
-from hyperspy.io import load
 
-dirpath = os.path.dirname(__file__)
-filename = 'test_io_overwriting.hspy'
-fullfilename = os.path.join(dirpath, filename)
+
+DIRPATH = os.path.dirname(__file__)
+FILENAME = 'test_io_overwriting.hspy'
+FULLFILENAME = os.path.join(DIRPATH, FILENAME)
 
 
 class TestIOOverwriting:
@@ -36,44 +37,51 @@ class TestIOOverwriting:
         self.new_s = Signal1D(np.ones(5))
         # make sure we start from a clean state
         self._clean_file()
-        self.s.save(fullfilename)
+        self.s.save(FULLFILENAME)
+        self.s_file_hashed = self._hash_file(FULLFILENAME)
+
+    def _hash_file(self, filename):
+        with open(filename, 'rb') as file:
+            md5_hash = hashlib.md5(file.read())
+            file_hashed = md5_hash.hexdigest()
+        return file_hashed
 
     def _clean_file(self):
-        if os.path.exists(fullfilename):
-            os.remove(fullfilename)
+        if os.path.exists(FULLFILENAME):
+            os.remove(FULLFILENAME)
 
-    def _check_file_is_written(self, signal, filename):
-        # check that the data have change
-        s = load(filename)
-        return np.array_equal(s.data, signal.data)
+    def _check_file_is_written(self, filename):
+        # Check that we have a different hash, in case the file have different 
+        # content from the original, the hash will be different.
+        return not self.s_file_hashed == self._hash_file(filename)
 
     def test_io_overwriting_True(self):
         # Overwrite is True, when file exists we overwrite
-        self.new_s.save(fullfilename, overwrite=True)
-        assert self._check_file_is_written(self.new_s, fullfilename)
+        self.new_s.save(FULLFILENAME, overwrite=True)
+        assert self._check_file_is_written(FULLFILENAME)
 
     def test_io_overwriting_False(self):
         # Overwrite if False, file exists we don't overwrite
-        self.new_s.save(fullfilename, overwrite=False)
-        assert not self._check_file_is_written(self.new_s, fullfilename)
+        self.new_s.save(FULLFILENAME, overwrite=False)
+        assert not self._check_file_is_written(FULLFILENAME)
 
     @pytest.mark.parametrize("overwrite", [None, True, False])
     def test_io_overwriting_no_existing_file(self, overwrite):
         self._clean_file()  # remove the file
-        self.new_s.save(fullfilename, overwrite=overwrite)
-        assert self._check_file_is_written(self.new_s, fullfilename)
+        self.new_s.save(FULLFILENAME, overwrite=overwrite)
+        assert self._check_file_is_written(FULLFILENAME)
 
     def test_io_overwriting_None_existing_file_y(self):
         # Overwrite is None, when file exists we ask, mock `y` here
         with patch('builtins.input', return_value='y'):
-            self.new_s.save(fullfilename)
-            assert self._check_file_is_written(self.new_s, fullfilename)
+            self.new_s.save(FULLFILENAME)
+            assert self._check_file_is_written(FULLFILENAME)
 
-    def test_io_overwriting_None_existing_file_n(self):        
+    def test_io_overwriting_None_existing_file_n(self):
         # Overwrite is None, when file exists we ask, mock `n` here
         with patch('builtins.input', return_value='n'):
-            self.new_s.save(fullfilename)
-            assert not self._check_file_is_written(self.new_s, fullfilename)
+            self.new_s.save(FULLFILENAME)
+            assert not self._check_file_is_written(FULLFILENAME)
 
     def teardown_method(self, method):
         self._clean_file()
