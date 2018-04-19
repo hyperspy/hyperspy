018f7b87f2d050a14f932bb958905f25b643186e
Francisco de la Peña <frandelapena@gmail.com>
2018-04-19 16:01:12 +0200

    Update version

--- hyperspy/Release.py
+++ hyperspy/Release.py
@@ -25,7 +25,7 @@ name = 'hyperspy'
 # When running setup.py the ".dev" string will be replaced (if possible)
 # by the output of "git describe" if git is available or the git
 # hash if .git is present.
-version = "1.3.1.dev"
+version = "1.3.1"
 description = "Multidimensional data analysis toolbox"
 license = 'GPL v3'
 
