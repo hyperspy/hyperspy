1a8dfcb2cf0bb8038bf5bc14c1d2c4212bdd2b3f
Vadim Migunov <vadim.migunov@gmail.com>
2018-04-27 23:38:14 +0200

    Added first tests for apodization

---
+++ hyperspy/tests/signal/test_apodiztion.py
@@ -0,0 +1,51 @@
+# Copyright 2007-2016 The Hyperspy developers
+#
+# This file is part of  Hyperspy.
+#
+#  Hyperspy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+#  Hyperspy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with  Hyperspy.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+import numpy as np
+from numpy.testing import assert_allclose
+
+from hyperspy.signals import Signal1D, Signal2D, ComplexSignal1D, ComplexSignal2D, BaseSignal
+
+
+@pytest.mark.parametrize('lazy', [True, False])
+def test_apodization(lazy):
+    SIZE_NAV0 = 2
+    SIZE_NAV1 = 3
+    SIZE_NAV2 = 4
+    SIZE_SIG0 = 50
+
+    # ax_dict0 = {'size': SIZE_NAV0, 'navigate': True}
+    # ax_dict1 = {'size': SIZE_NAV1, 'navigate': True}
+    # ax_dict2 = {'size': SIZE_SIG0, 'navigate': False}
+    # ax_dict3 = {'size': SIZE_NAV2, 'navigate': True}
+    # data = np.random.rand(SIZE_NAV0 * SIZE_NAV1 * SIZE_SIG0 * SIZE_NAV2).reshape(
+    #     (SIZE_NAV0, SIZE_NAV1, SIZE_SIG0, SIZE_NAV2))
+    # signal1d = Signal1D(data,
+    #                     axes=[ax_dict0, ax_dict1, ax_dict2, ax_dict3])
+    data = np.random.rand(SIZE_NAV0 * SIZE_NAV1 * SIZE_SIG0 * SIZE_NAV2).reshape(
+        (SIZE_NAV0, SIZE_NAV1, SIZE_NAV2, SIZE_SIG0))
+    signal1d = Signal1D(data)
+    signal1d_a = signal1d.apply_apodization()
+    window = np.hanning(SIZE_SIG0)
+    # data_a = data * window[np.newaxis, np.newaxis, :, np.newaxis]
+    data_a = data * window[np.newaxis, np.newaxis, np.newaxis, :]
+
+    assert np.alltrue(signal1d_a.data == data_a)
+
+
+
