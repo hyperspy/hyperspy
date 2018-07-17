ddc8933eed526e380374505b67363deb1d192098
Joshua Taillon <jat255@gmail.com>
2018-07-17 09:25:28 -0400

    change logger error to actual error

--- hyperspy/learn/mva.py
+++ hyperspy/learn/mva.py
@@ -162,14 +162,13 @@ class MVA():
         to_return = None
         # Check if it is the wrong data type
         if self.data.dtype.char not in ['e', 'f', 'd']:  # If not float
-            _logger.error(
+            raise TypeError(
                 'To perform a decomposition the data must be of the float '
-                'type, but the current type is {}. '
+                'type, but the current type is \'{}\'. '
                 'To fix this issue, you can change the type using the '
                 'change_dtype method (e.g. s.change_dtype(\'float64\')) '
                 'and then repeat the decomposition.\n'
-                'No decomposition was performed and the data was not '
-                'changed.'.format(self.data.dtype))
+                'No decomposition was performed.'.format(self.data.dtype))
             return
 
         if self.axes_manager.navigation_size < 2:
