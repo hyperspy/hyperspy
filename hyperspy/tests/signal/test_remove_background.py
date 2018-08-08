83f7c898eaedbcfe04b0c8e20e597f048832e37b
AEljarrat <alberto.eljarrat@gmail.com>
2018-08-08 12:01:36 +0200

    NEW test for the remainder line

--- hyperspy/tests/signal/test_remove_background.py
+++ hyperspy/tests/signal/test_remove_background.py
@@ -3,7 +3,9 @@ import numpy as np
 from hyperspy import signals
 from hyperspy import components1d
 from hyperspy.decorators import lazifyTestClass
+from hyperspy.misc.test_utils import update_close_figure
 
+from hyperspy._signals.signal1d import BackgroundRemoval
 
 @lazifyTestClass
 class TestRemoveBackground1DGaussian:
@@ -59,3 +61,54 @@ class TestRemoveBackground1DPowerLaw:
             background_type='PowerLaw',
             show_progressbar=None)
         assert np.allclose(s1.data, np.zeros(len(s1.data)), atol=60)
+
+#@update_close_figure
+def test_BackgroundRemoval_gui():
+    # create a signal
+    axd = {'name':'some',
+           'units':'axis',
+           'scale':0.1,
+           'offset':-10.,
+           'size':512}
+    s = signals.Signal1D(np.random.rand(axd['size']), axes=[axd,])
+    s = signals.BaseSignal(np.ones((2,2))).T * s
+
+    m = s.create_model()
+    vpm = components1d.VolumePlasmonDrude()
+    zlp = components1d.Gaussian(1000.)
+    m.append(vpm)
+    m.append(zlp)
+    s = m.as_signal(show_progressbar=False)
+
+    # Remove Background using CLI
+    import warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        bck_cli = s.remove_background(signal_range=(1., 2.))
+
+    # Remove Background GUI
+    br = BackgroundRemoval(s,
+                           background_type='Power Law',
+                           polynomial_order=2,
+                           fast=True,
+                           plot_remainder=True,
+                           show_progressbar=None)
+
+    # Interact with GUI...
+    Ei = 1.
+    Ef = 2.
+    br.ss_left_value  = Ei
+    br.ss_right_value = Ef
+    br.span_selector_changed()
+
+    # ... extracting remainder line
+    bck_gui = bck_cli.deepcopy()
+    for si in s:
+        idx = s.axes_manager.indices
+        bck_gui.data[idx] = np.nan_to_num(
+                                s._plot.signal_plot.ax_lines[2].data_function())
+    bck_cli.crop_signal1D(Ei)
+    bck_gui.crop_signal1D(Ei)
+
+    # compare
+    assert np.allclose(bck_cli.data, bck_gui.data)
