fc934bc66b8ece5df564ac0996db5e28b70b00f2
Eric Prestat <eric.prestat@gmail.com>
2018-11-03 15:54:07 +0000

    Use imageio instead of deprecated scipy functions. Add tests for reading and writing images (png, jpeg, gif, bmp).

--- hyperspy/io_plugins/image.py
+++ hyperspy/io_plugins/image.py
@@ -18,9 +18,10 @@
 
 import os
 
-from scipy.misc import imread, imsave
+from imageio import imread, imwrite
 
-from hyperspy.misc.rgb_tools import regular_array2rgbx
+
+from hyperspy.misc import rgb_tools
 
 # Plugin characteristics
 # ----------------------
@@ -47,7 +48,10 @@ def file_writer(filename, signal, file_format='png', **kwds):
             The fileformat defined by its extension that is any one supported by
             PIL.
     """
-    imsave(filename, signal.data)
+    data = signal.data
+    if rgb_tools.is_rgbx(data):
+        data = rgb_tools.rgbx2regular_array(data)
+    imwrite(filename, data)
 
 
 def file_reader(filename, **kwds):
@@ -86,5 +90,5 @@ def _read_data(filename):
                 (dc[:, :, 1] == dc[:, :, 2]).all():
             dc = dc[:, :, 0]
         else:
-            dc = regular_array2rgbx(dc)
+            dc = rgb_tools.regular_array2rgbx(dc)
     return dc
