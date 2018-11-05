c1d0cb0e8c78c9e93de11d15de30be1fe2104687
ZanettaPM <pierre-marie.zanetta@hotmail.fr>
2018-11-05 16:25:49 +0100

    Update hyperspy/models/edsmodel.py

--- hyperspy/models/edsmodel.py
+++ hyperspy/models/edsmodel.py
@@ -290,7 +290,6 @@ class EDSModel(Model1D):
         self.append(background)
         self.background_components.append(background)
 
-<<<<<<< HEAD
     def add_physical_background(self, E0='from_metadata', detector='Polymer_C', quantification=None,absorption_model='quadrilateral', coating_thickness=0, TOA='from_metadata', Phase_map=None):
         """
         Add a background based on physical property of the interraction e-/mater (see Zanetta et al. 2018)
@@ -356,8 +355,6 @@ class EDSModel(Model1D):
         self.append(background)
         self.background_components.append(background)
 
-=======
->>>>>>> RELEASE_next_minor
     def free_background(self):
         """
         Free the yscale of the background components.
