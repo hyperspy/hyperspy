235d958ac1ac9937132ca6b6b93519294bbd95aa
Eric Prestat <eric.prestat@gmail.com>
2019-03-25 13:14:32 +0000

    Make `function_nd` of the `Offset` component behave consistently with other components.

--- hyperspy/_signals/signal1d.py
+++ hyperspy/_signals/signal1d.py
@@ -1036,10 +1036,7 @@ _spikes_diagnosis,
         if fast and not self._lazy:
             try:
                 axis = self.axes_manager.signal_axes[0].axis
-                if self.axes_manager.navigation_dimension == 0:
-                    result = self - background_estimator.function(axis)
-                else:
-                    result = self - background_estimator.function_nd(axis)
+                result = self - background_estimator.function_nd(axis)
             except MemoryError:
                 result = self - model.as_signal(
                     show_progressbar=show_progressbar)
