0cc26f21a29766fd365bc33447667d2d4f2faa9f
jlaehne <jonas@pdi-berlin.de>
2019-03-27 18:02:41 +0100

    Consistency parameter mapping table.

--- hyperspy/_components/rc.py
+++ hyperspy/_components/rc.py
@@ -28,7 +28,7 @@ class RC(Expression):
         f(x) = V_\\mathrm{0} + V_\\mathrm{max} \\left[1 - \\mathrm{exp}\\left(- \\frac{x}{\\tau}\\right)\\right]
     
     ========================= ===========
-     Parameter                 Attribute 
+     Variable                  Parameter 
     ========================= ===========
      :math:`V_\\mathrm{0}`     V0     
      :math:`V_\\mathrm{max}`   Vmax    
