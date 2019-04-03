113858a484fbeee4b832ec5a3f976d646dad5624
jlaehne <jonas@pdi-berlin.de>
2019-04-03 16:25:13 +0200

    Added test function.

---
+++ hyperspy/tests/component/test_logistic.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2007-2016 The HyperSpy developers
+#
+# This file is part of  HyperSpy.
+#
+#  HyperSpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+#  HyperSpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import numpy as np
+from numpy.testing import assert_allclose
+
+from hyperspy.components1d import Logistic
+from hyperspy.signals import Signal1D
+from hyperspy.utils import stack
+
+
+def test_function():
+    g = Logistic()
+    g.a.value = 1
+    g.b.value = 2
+    g.c.value = 3
+    g.origin.value = 4
+    assert_allclose(g.function(2),1.5)
+    assert_allclose(g.function(4),0.3)
+
