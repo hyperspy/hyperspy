598d014daf62b0b6fb3dced8c1068fc07e5cfc13
jlaehne <jonas@pdi-berlin.de>
2019-04-16 23:55:25 +0200

    Rewritten function as Expression.

--- hyperspy/_components/error_function.py
+++ hyperspy/_components/error_function.py
@@ -16,72 +16,45 @@
 # You should have received a copy of the GNU General Public License
 # along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
 
-import math
+#import math
 
 import numpy as np
-from scipy.special import erf
+#from scipy.special import erf
 
-from hyperspy.component import Component
+from hyperspy._components.expression import Expression
 
 sqrt2pi = np.sqrt(2 * np.pi)
 
 
-class Erf(Component):
+class Erf(Expression):
 
-    """Error function component
+    r"""Error function component.
 
-    Attributes
-    ----------
-    A : float
-    sigma : float
-    origin : float
+    Parameters
+    -----------
+        A : float
+        
+        sigma : float
+        
+        origin : float
+        
     """
 
-    def __init__(self):
-        Component.__init__(self, ['A', 'sigma', 'origin'])
+    def __init__(self, A=1., sigma=1., origin=0., module="scipy",
+                 **kwargs):
+        super(Erf, self).__init__(
+            expression="A * erf((x - origin) / sqrt(2) / sigma) / 2",
+            name="Erf",
+            A=A,
+            sigma=sigma,
+            origin=origin,
+            module=module,
+            autodoc=False,
+            **kwargs,
+        )
 
         # Boundaries
         self.A.bmin = 0.
-        self.A.bmax = None
-
-        self.sigma.bmin = None
-        self.sigma.bmax = None
 
         self.isbackground = False
         self.convolved = True
-
-        # Gradients
-        self.A.grad = self.grad_A
-        self.sigma.grad = self.grad_sigma
-        self.origin.grad = self.grad_origin
-        self._position = self.origin
-
-    def function(self, x):
-        A = self.A.value
-        sigma = self.sigma.value
-        origin = self.origin.value
-        return A * erf((x - origin) / math.sqrt(2) / sigma) / 2
-
-    def grad_A(self, x):
-        sigma = self.sigma.value
-        origin = self.origin.value
-        return erf((x - origin) / math.sqrt(2) / sigma) / 2
-
-    def grad_sigma(self, x):
-        A = self.A.value
-        sigma = self.sigma.value
-        origin = self.origin.value
-        s2 = math.sqrt(2)
-        return (
-            (origin / (s2 * sigma ** 2) - x / (s2 * sigma ** 2)) *
-            np.exp(-(x / (s2 * sigma) - origin / (s2 * sigma)) ** 2) *
-            A) / math.sqrt(math.pi)
-
-    def grad_origin(self, x):
-        A = self.A.value
-        sigma = self.sigma.value
-        origin = self.origin.value
-        s2 = math.sqrt(2)
-        return -(
-            np.exp(-(x / (s2 * sigma) - origin / (s2 * sigma)) ** 2) *
-            A) / (sqrt2pi * sigma)
