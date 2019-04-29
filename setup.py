b43931d37cb78d37e5ff4fa70546e4ff1be1d38e
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-04-29 18:26:18 -0400

    Added pyUSID as an optional requirement

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ extras_require = {
     "gui-traitsui": ["hyperspy_gui_traitsui>=1.1.0"],
     "mrcz": ["blosc>=1.5", 'mrcz>=0.3.6'],
     "speed": ["numba"],
+    "usid": ["pyUSID"],
     # bug in pip: matplotib is ignored here because it is already present in
     # install_requires.
     "tests": ["pytest>=3.6", "pytest-mpl", "matplotlib>=3.0.0"], # for testing
