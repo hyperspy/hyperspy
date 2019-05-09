0692456ce6e5b7336683ed2157eef9f241db41a7
ssomnath <14300780+ssomnath@users.noreply.github.com>
2019-05-09 16:05:59 -0400

    Correcting minimum version of pyUSID required

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ extras_require = {
     "gui-traitsui": ["hyperspy_gui_traitsui>=1.1.0"],
     "mrcz": ["blosc>=1.5", 'mrcz>=0.3.6'],
     "speed": ["numba"],
-    "usid": ["pyUSID"],
+    "usid": ["pyUSID>=0.0.6.1"],
     # bug in pip: matplotib is ignored here because it is already present in
     # install_requires.
     "tests": ["pytest>=3.6", "pytest-mpl", "matplotlib>=3.0.0"], # for testing
