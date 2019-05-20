9556e3617785353efb75c9876033ef921e1b623f
autopep8 <autopep8@email.com>
2019-05-20 17:24:50 +0200

    Automatic style corrections courtesy of autopep8

--- hyperspy/extensions.py
+++ hyperspy/extensions.py
@@ -17,11 +17,16 @@ with open(_ext_f, 'r') as stream:
 # import unless needed
 ALL_EXTENSIONS = copy.deepcopy(EXTENSIONS)
 
-_external_exts_f = os.path.join(hyperspy.misc.config_dir.config_path, "hspy_extensions.yaml")
+_external_exts_f = os.path.join(
+    hyperspy.misc.config_dir.config_path,
+    "hspy_extensions.yaml")
 with open(_external_exts_f, 'r') as stream:
     external_exts = yaml.safe_load(stream)
     for ext_ext_mod in external_exts:
-        path = os.path.join(os.path.dirname(importlib.import_module(ext_ext_mod).__file__), "hyperspy_extension.yaml")
+        path = os.path.join(
+            os.path.dirname(
+                importlib.import_module(ext_ext_mod).__file__),
+            "hyperspy_extension.yaml")
         with open(path, 'r') as stream:
             ext_ext = yaml.safe_load(stream)
             if "signals" in ext_ext:
