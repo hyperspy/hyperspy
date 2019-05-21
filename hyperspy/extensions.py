4975ffd6c059f5dd83232bb9a621395cbcc9ff63
Francisco de la Peña <francisco.de-la-pena-manchon@univ-lille.fr>
2019-05-21 11:13:38 +0200

    Don't raise error if hspy_extensions.yaml doesn't exist

--- hyperspy/extensions.py
+++ hyperspy/extensions.py
@@ -21,19 +21,21 @@ ALL_EXTENSIONS = copy.deepcopy(EXTENSIONS)
 _external_exts_f = os.path.join(
     hyperspy.misc.config_dir.config_path,
     "hspy_extensions.yaml")
-with open(_external_exts_f, 'r') as stream:
-    external_exts = yaml.safe_load(stream)
-    for ext_ext_mod in external_exts:
-        _logger.info("Loading extension %s" % ext_ext_mod)
-        path = os.path.join(
-            os.path.dirname(
-                importlib.import_module(ext_ext_mod).__file__),
-            "hyperspy_extension.yaml")
-        with open(path, 'r') as stream:
-            ext_ext = yaml.safe_load(stream)
-            if "signals" in ext_ext:
-                ALL_EXTENSIONS["signals"].update(ext_ext["signals"])
-            if "components1D" in ext_ext:
-                ALL_EXTENSIONS["components1D"].update(ext_ext["components1D"])
-            if "components2D" in ext_ext:
-                ALL_EXTENSIONS["components2D"].update(ext_ext["components2D"])
+
+if os.path.isfile(_external_exts_f):
+    with open(_external_exts_f, 'r') as stream:
+        external_exts = yaml.safe_load(stream)
+        for ext_ext_mod in external_exts:
+            _logger.info("Loading extension %s" % ext_ext_mod)
+            path = os.path.join(
+                os.path.dirname(
+                    importlib.import_module(ext_ext_mod).__file__),
+                "hyperspy_extension.yaml")
+            with open(path, 'r') as stream:
+                ext_ext = yaml.safe_load(stream)
+                if "signals" in ext_ext:
+                    ALL_EXTENSIONS["signals"].update(ext_ext["signals"])
+                if "components1D" in ext_ext:
+                    ALL_EXTENSIONS["components1D"].update(ext_ext["components1D"])
+                if "components2D" in ext_ext:
+                    ALL_EXTENSIONS["components2D"].update(ext_ext["components2D"])
