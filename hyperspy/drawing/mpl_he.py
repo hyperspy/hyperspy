81e3ca52f8753fb72d5a5c823cfff3fdb810933e
Eric Prestat <eric.prestat@gmail.com>
2019-09-10 21:11:55 +0100

    Add color map to preferences.

--- hyperspy/drawing/mpl_he.py
+++ hyperspy/drawing/mpl_he.py
@@ -21,6 +21,7 @@ import logging
 from traits.api import Undefined
 
 from hyperspy.drawing import widgets, signal1d, image
+from hyperspy.defaults_parser import preferences
 
 
 _logger = logging.getLogger(__name__)
@@ -139,6 +140,8 @@ class MPL_HyperExplorer(object):
                                     imf.update), [])
 
             imf.title = title
+            if "cmap" not in kwds.keys() or kwds['cmap'] is None:
+                kwds["cmap"] = preferences.Plot.cmap_navigator
             imf.plot(**kwds)
             self.pointer.set_mpl_ax(imf.ax)
             self.navigator_plot = imf
