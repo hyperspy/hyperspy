2554c0b5a106ee8ccbaf13d7763e6856c1750b74
Eric Prestat <eric.prestat@gmail.com>
2020-01-16 21:40:33 +0000

    Add numpy as module in lambdify since scipy.exp is deprecated and Fix doc build.

--- hyperspy/tests/signal/test_remove_background.py
+++ hyperspy/tests/signal/test_remove_background.py
@@ -146,13 +146,13 @@ class TestRemoveBackground1DLorentzian:
 class TestRemoveBackground1DSkewNormal:
 
     def setup_method(self, method):
-        lorentzian = components1d.SkewNormal()
-        lorentzian.A.value = 3
-        lorentzian.x0.value = 1
-        lorentzian.scale.value = 2
-        lorentzian.shape.value = 10
+        skewnormal = components1d.SkewNormal()
+        skewnormal.A.value = 3
+        skewnormal.x0.value = 1
+        skewnormal.scale.value = 2
+        skewnormal.shape.value = 10
         self.signal = signals.Signal1D(
-            lorentzian.function(np.arange(0, 10, 0.01)))
+            skewnormal.function(np.arange(0, 10, 0.01)))
         self.signal.axes_manager[0].scale = 0.01
         self.signal.metadata.Signal.binned = False
 
@@ -164,7 +164,7 @@ class TestRemoveBackground1DSkewNormal:
             show_progressbar=None)
         assert np.allclose(np.zeros(len(s1.data)), s1.data, atol=0.2)
 
-    def test_background_remove_lorentzian_full_fit(self):
+    def test_background_remove_skewnormal_full_fit(self):
         s1 = self.signal.remove_background(
             signal_range=(None, None),
             background_type='SkewNormal',
@@ -184,34 +184,34 @@ def compare_axes_manager_metadata(s0, s1):
     assert s0.metadata.General.title == s1.metadata.General.title
 
 
-# @pytest.mark.parametrize('nav_dim', [0, 1])
-# @pytest.mark.parametrize('fast', [True, False])
-# @pytest.mark.parametrize('zero_fill', [True, False])
-# @pytest.mark.parametrize('show_progressbar', [True, False])
-# @pytest.mark.parametrize('plot_remainder', [True, False])
-# @pytest.mark.parametrize('background_type',
-#                          ['Power Law', 'Polynomial', 'Offset',
-#                          'Gaussian', 'Lorentzian', 'SkewNormal'])
-# def test_remove_background_metadata_axes_manager_copy(nav_dim,
-#                                                       fast,
-#                                                       zero_fill,
-#                                                       show_progressbar,
-#                                                       plot_remainder,
-#                                                       background_type):
-#     if nav_dim == 0:
-#         s = signals.Signal1D(np.arange(10, 100)[::-1])
-#     else:
-#         s = signals.Signal1D(np.arange(10, 210)[::-1].reshape(2, 100))
-#     s.axes_manager[0].name = 'axis0'
-#     s.axes_manager[0].units = 'units0'
-#     s.axes_manager[0].scale = 0.9
-#     s.axes_manager[0].offset = 1.
-#     s.metadata.General.title = "atitle"
+@pytest.mark.parametrize('nav_dim', [0, 1])
+@pytest.mark.parametrize('fast', [True, False])
+@pytest.mark.parametrize('zero_fill', [True, False])
+@pytest.mark.parametrize('show_progressbar', [True, False])
+@pytest.mark.parametrize('plot_remainder', [True, False])
+@pytest.mark.parametrize('background_type',
+                          ['Power Law', 'Polynomial', 'Offset',
+                          'Gaussian', 'Lorentzian', 'SkewNormal'])
+def test_remove_background_metadata_axes_manager_copy(nav_dim,
+                                                      fast,
+                                                      zero_fill,
+                                                      show_progressbar,
+                                                      plot_remainder,
+                                                      background_type):
+    if nav_dim == 0:
+        s = signals.Signal1D(np.arange(10, 100)[::-1])
+    else:
+        s = signals.Signal1D(np.arange(10, 210)[::-1].reshape(2, 100))
+    s.axes_manager[0].name = 'axis0'
+    s.axes_manager[0].units = 'units0'
+    s.axes_manager[0].scale = 0.9
+    s.axes_manager[0].offset = 1.
+    s.metadata.General.title = "atitle"
 
-#     s_r = s.remove_background(signal_range=(2, 50),
-#                               fast=fast,
-#                               zero_fill=zero_fill,
-#                               show_progressbar=show_progressbar,
-#                               plot_remainder=plot_remainder,
-#                               background_type=background_type)
-#     compare_axes_manager_metadata(s, s_r)
+    s_r = s.remove_background(signal_range=(2, 50),
+                              fast=fast,
+                              zero_fill=zero_fill,
+                              show_progressbar=show_progressbar,
+                              plot_remainder=plot_remainder,
+                              background_type=background_type)
+    compare_axes_manager_metadata(s, s_r)
