3237a8d489645ff33b3c55933d3db4597474789c
Eric Prestat <eric.prestat@gmail.com>
2020-04-08 12:58:02 +0100

    Fix error message.

--- hyperspy/ui_registry.py
+++ hyperspy/ui_registry.py
@@ -51,9 +51,7 @@ def get_gui(self, toolkey, display=True, toolkit=None, **kwargs):
     if not TOOLKIT_REGISTRY:
         raise ImportError(
             "No toolkit registered. Install hyperspy_gui_ipywidgets or "
-            "hyperspy_gui_traitsui GUI elements. If hyperspy_gui_traitsui"
-            "is installed, initialize a toolkit supported by traitsui "
-            "before importing HyperSpy."
+            "hyperspy_gui_traitsui GUI elements."
         )
     from hyperspy.defaults_parser import preferences
     if isinstance(toolkit, str):
