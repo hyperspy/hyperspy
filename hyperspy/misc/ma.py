126250f43929372dca37436186873bc6bf6a29e1
Carter Francis <csfrancis@wisc.edu>
2020-04-21 10:47:37 -0500

    changed default copying behavior

--- hyperspy/misc/ma.py
+++ hyperspy/misc/ma.py
@@ -11,77 +11,78 @@ def asarray(signal, dtype=None):
     else:
         signal.data = np.ma.asanyarray(signal.data, dtype=dtype)
 
-def masked_equal(signal, value, copy=True):
+def masked_equal(signal, value, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_equal(signal.data, value)
     else:
         signal.data = np.ma.masked_equal(signal.data, value=value, copy=copy)
 
 
-def masked_greater(signal, value, copy=True):
+def masked_greater(signal, value, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_greater(signal.data, value, copy=copy)
     else:
         signal.data = np.ma.masked_greater(signal.data, value=value, copy=copy)
 
 
-def masked_greater_equal(signal, value, copy=True):
+def masked_greater_equal(signal, value, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_greater_equal(signal.data, value)
     else:
         signal.data = np.ma.masked_greater_equal(signal.data, value=value, copy=copy)
 
-def masked_inside(signal, v1, v2, copy=True):
+def masked_inside(signal, v1, v2, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_inside(signal.data, v1, v2)
     else:
         signal.data = np.ma.masked_inside(signal.data, v1, v2, copy=copy)
 
 
-def masked_invalid(signal, copy=True):
+def masked_invalid(signal, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_invalid(signal.data)
     else:
         signal.data = np.ma.masked_invalid(signal.data,copy=copy)
 
 
-def masked_less(signal, value, copy=True):
+def masked_less(signal, value, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_less(signal.data, value=value)
     else:
         signal.data = np.ma.masked_less(signal.data, value=value, copy=copy)
 
 
-def masked_less_equal(signal, value, copy=True):
+def masked_less_equal(signal, value, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_less_equal(signal.data, value=value)
     else:
         signal.data = np.ma.masked_less_equal(signal.data, value=value, copy=copy)
 
 
-def masked_not_equal(signal, value, copy=True):
+def masked_not_equal(signal, value, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_not_equal(signal.data, value=value)
     else:
         signal.data = np.ma.masked_not_equal(signal.data, value=value, copy=copy)
 
 
-def masked_outside(signal, v1, v2, copy=True):
+def masked_outside(signal, v1, v2, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_outside(signal.data, v1, v2)
     else:
         signal.data = np.ma.masked_outside(signal.data, v1, v2, copy=copy)
 
 
-def masked_values(signal, value, copy=True):
+def masked_values(signal, value, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_values(signal.data, value)
     else:
         signal.data = np.ma.masked_values(signal.data, value, copy=copy)
 
 
-def masked_where(condition, signal, copy=True):
+def masked_where(condition, signal, copy=False):
     if signal._lazy:
         signal.data = da.ma.masked_where(condition, signal.data)
     else:
         signal.data = np.ma.masked_where(signal.data, condition, copy=copy)
+
