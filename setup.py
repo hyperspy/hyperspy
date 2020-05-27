7aa4d353574f5e43996d3534e3118cf021c1f769
Quinn <paul.quinn@diamond.ac.uk>
2020-05-27 17:17:25 +0100

    fix plot_metric

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ install_req = ['scipy>=0.15',
                ]
 
 extras_require = {
-    "learning": ['scikit-learn>=0.10'],
+    "learning": ['scikit-learn'],
     "gui-jupyter": ["hyperspy_gui_ipywidgets>=1.1.0"],
     "gui-traitsui": ["hyperspy_gui_traitsui>=1.1.0"],
     "mrcz": ["blosc>=1.5", 'mrcz>=0.3.6'],
