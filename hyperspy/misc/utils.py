dd9d5b526cc3ce8d71954cce7354c77f289a06d5
Tom Furnival <tjof2@cam.ac.uk>
2020-08-26 14:31:16 +0100

    Add back deprecation warning

--- hyperspy/misc/utils.py
+++ hyperspy/misc/utils.py
@@ -907,6 +907,15 @@ def stack(signal_list, axis=None, new_axis_name="stack_element", lazy=None, **kw
     import dask.array as da
     from numbers import Number
 
+    deprecated = ['mmap', 'mmap_dir']
+    for k in [k for k in ["mmap", "mmap_dir"] if k in kwargs]:
+        lazy = True
+        warnings.warn(
+            f"'{k}' argument is deprecated and will be removed in "
+            "HyperSpy v2.0. Please use 'lazy=True' instead.",
+            VisibleDeprecationWarning,
+        )
+
     axis_input = copy.deepcopy(axis)
     signal_list = list(signal_list)
 
