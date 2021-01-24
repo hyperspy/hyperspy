efa8344e03008ee5e3cf6a58e1dfd4ced5c02b5a
J. Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2021-01-24 22:47:41 +0000

    Fixes in tests

--- hyperspy/tests/signal/test_spike_removal_tool.py
+++ hyperspy/tests/signal/test_spike_removal_tool.py
@@ -69,6 +69,8 @@ def test_spikes_removal_tool_non_interactive():
     s.data[0, 2, 29] += 1
     s.data[1, 2, 14] += 1
 
-    sr = s.spikes_removal_tool(threshold=0.5, interactive=False)
+    sr = s.spikes_removal_tool(threshold=0.5, interactive=False, add_noise=False)
     np.testing.assert_almost_equal(s.data[1, 0, 1], 1, decimal=5)
+    np.testing.assert_almost_equal(s.data[0, 2, 29], 1, decimal=5)
+    np.testing.assert_almost_equal(s.data[1, 2, 14], 1, decimal=5)
     assert isinstance(sr, SpikesRemoval)
