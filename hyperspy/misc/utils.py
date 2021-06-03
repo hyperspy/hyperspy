ca1fe5fc0df2fa5d88858b55ab22fd9b4590a859
Eric Prestat <eric.prestat@gmail.com>
2021-06-03 13:25:00 +0100

    Fix formatting docsting

--- hyperspy/misc/utils.py
+++ hyperspy/misc/utils.py
@@ -1185,30 +1185,29 @@ def process_function_blockwise(data,
                                arg_keys=None,
                                **kwargs):
     """
-    Function for processing the function blockwise...
-    This gets passed to map_blocks so that the function
-    only gets applied to the signal axes.
+    Convenience function for processing a function blockwise. By design, its
+    output is used as an argument of the dask ``map_blocks`` so that the
+    function only gets applied to the signal axes.
 
-    Parameters:
-    ------------
-    data: np.array
+    Parameters
+    ----------
+    data : np.ndarray
         The data for one chunk
-    *args: tuple
-        Any signal the is iterated alongside the data in. In the
-        form ((key1, value1),(key2,value2)...)
-    function: function
+    *args : tuple
+        Any signal the is iterated alongside the data in. In the form
+        ((key1, value1), (key2, value2))
+    function : function
         The function to applied to the signal axis
-    nav_indexes: tuple
+    nav_indexes : tuple
         The indexes of the navigation axes for the dataset.
     output_signal_shape: tuple
-        The shape of the output signal.  For a ragged signal
-        this is equal to 1.
-    block_info: dict
-        The block info as described by the `dask.array.map_blocks` function
-    arg_keys: tuple
+        The shape of the output signal. For a ragged signal, this is equal to 1
+    block_info : dict
+        The block info as described by the ``dask.array.map_blocks`` function
+    arg_keys : tuple
         The list of keys for the passed arguments (args).  Together this makes
         a set of key:value pairs to be passed to the function.
-    **kwargs: dict
+    **kwargs : dict
         Any additional key value pairs to be used by the function
         (Note that these are the constants that are applied.)
 
@@ -1251,11 +1250,12 @@ def guess_output_signal_size(test_signal,
     the resulting signal shape and datatype.
 
     Parameters
+    ----------
     test_signal: BaseSignal
         A test signal for the function to be applied to. A signal
         with 0 navigation dimensions
     function: function
-        The function to be applied to the dataset
+        The function to be applied to the data
     ragged: bool
         If the data is ragged then the output signal size is () and the
         data type is 'object'
