8ef140a3c2134eb4e674fe8f645a56a7ad6503f2
jlaehne <jonas@pdi-berlin.de>
2022-01-17 21:43:36 +0100

    Remove extra spaces in header

--- hyperspy/io_plugins/digital_micrograph.py
+++ hyperspy/io_plugins/digital_micrograph.py
@@ -2,7 +2,7 @@
 # Copyright 2010 Stefano Mazzucco
 # Copyright 2011-2020 The HyperSpy developers
 #
-# This file is part of  HyperSpy. It is a fork of the original PIL dm3 plugin
+# This file is part of HyperSpy. It is a fork of the original PIL dm3 plugin
 # written by Stefano Mazzucco.
 #
 #  HyperSpy is free software: you can redistribute it and/or modify
@@ -12,11 +12,11 @@
 #
 #  HyperSpy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
+# along with HyperSpy. If not, see <http://www.gnu.org/licenses/>.
 
 # Plugin to read the Gatan Digital Micrograph(TM) file format
 
--- hyperspy/misc/holography/reconstruct.py
+++ hyperspy/misc/holography/reconstruct.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2007-2022 The HyperSpy developers
 #
-# This file is part of  HyperSpy.
+# This file is part of HyperSpy.
 #
 #  HyperSpy is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -10,11 +10,11 @@
 #
 #  HyperSpy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
+# along with HyperSpy. If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 from scipy.fftpack import fft2, ifft2, fftshift
