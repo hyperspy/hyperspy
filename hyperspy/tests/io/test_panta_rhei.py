03ab6eb8a7bcda0e4cee17fd66ead063f7739142
Giulio Guzzinati <guzzinati@ceos-gmbh.eu>
2022-05-30 18:05:27 +0200

    Add test for PRZ file reader
    Minor: enable compression in PRZ file writer

---
+++ hyperspy/tests/io/test_panta_rhei.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from numpy.ma.testutils import assert_almost_equal
+
+from hyperspy.io import load
+from hyperspy.misc.test_utils import assert_deep_almost_equal
+
+my_path = Path(__file__).parent
+
+
+class TestLoadingPrzFiles:
+    def test_metadata_prz_v5(self):
+        md = {'_HyperSpy': {'Folding': {'unfolded': False,
+                                        'signal_unfolded': False,
+                                        'original_shape': None,
+                                        'original_axes_manager': None}},
+              'General': {'title': 'AD', 'original_filename': 'panta_rhei_sample_v5.prz'},
+              'Signal': {'signal_type': ''},
+              'Acquisition_instrument': {'TEM': {'beam_energy': 200.0,
+                                                 'acquisition_mode': 'STEM',
+                                                 'magnification': 10000000,
+                                                 'camera_length': 0.02,
+                                                 'Detector': {'EELS': {'spectrometer': 'CEOS CEFID'}}}}}
+        am = {'axis-0': {'_type': 'UniformDataAxis',
+                         'name': 'Y',
+                         'units': 'm',
+                         'navigate': False,
+                         'is_binned': False,
+                         'size': 16,
+                         'scale': 7.795828292907633e-09,
+                         'offset': 0.0},
+              'axis-1': {'_type': 'UniformDataAxis',
+                         'name': 'X',
+                         'units': 'm',
+                         'navigate': False,
+                         'is_binned': False,
+                         'size': 16,
+                         'scale': 7.795828292907633e-09,
+                         'offset': 0.0}}
+        
+        s = load(my_path / 'panta_rhei_files' / 'panta_rhei_sample_v5.prz')
+        
+        assert_deep_almost_equal(s.metadata.as_dictionary(), md)
+        assert_deep_almost_equal(s.axes_manager.as_dictionary(), am)
+        assert (s.data.shape == (16, 16))
+        assert (s.data.max() == 40571)
+        assert (s.data.min() == 36193)
+        assert_almost_equal(s.data.std(), 1025.115644550)
