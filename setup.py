50e57f074c9c32693eb5d1a4d7ee92706541b8e9
Francisco de la Peña <francisco.de-la-pena-manchon@univ-lille.fr>
2022-06-30 15:58:25 +0200

    Remove IO from setup.py

--- setup.py
+++ setup.py
@@ -19,15 +19,11 @@
 from __future__ import print_function
 
 import hyperspy.Release as Release
-from distutils.errors import CompileError, DistutilsPlatformError
-import distutils.ccompiler
-import distutils.sysconfig
 import itertools
 import subprocess
 import os
 import warnings
-from tempfile import TemporaryDirectory
-from setuptools import setup, Extension, Command
+from setuptools import setup
 import sys
 
 v = sys.version_info
@@ -91,7 +87,6 @@ extras_require = {
                  'scikit-learn;sys_platform!="darwin"'],
     "gui-jupyter": ["hyperspy_gui_ipywidgets>=1.1.0"],
     "gui-traitsui": ["hyperspy_gui_traitsui>=1.1.0"],
-    "mrcz": ["blosc>=1.5", 'mrcz>=0.3.6'],
     "speed": ["cython", "imagecodecs>=2020.1.31"],
     "usid": ["pyUSID>=0.0.7", "sidpy"],
     "scalebar": ["matplotlib-scalebar"],
@@ -124,115 +119,6 @@ def update_version(version):
     with open(release_path, "w") as f:
         f.writelines(lines)
 
-
-# Extensions. Add your extension here:
-raw_extensions = [Extension("hyperspy.io_plugins.unbcf_fast",
-                            [os.path.join('hyperspy', 'io_plugins', 'unbcf_fast.pyx')]),
-                  ]
-
-cleanup_list = []
-for leftover in raw_extensions:
-    path, ext = os.path.splitext(leftover.sources[0])
-    if ext in ('.pyx', '.py'):
-        cleanup_list.append(''.join([os.path.join(setup_path, path), '.c*']))
-        if os.name == 'nt':
-            bin_ext = '.cpython-*.pyd'
-        else:
-            bin_ext = '.cpython-*.so'
-        cleanup_list.append(''.join([os.path.join(setup_path, path), bin_ext]))
-
-
-def count_c_extensions(extensions):
-    c_num = 0
-    for extension in extensions:
-        # if first source file with extension *.c or *.cpp exists
-        # it is cythonised or pure c/c++ extension:
-        sfile = extension.sources[0]
-        path, ext = os.path.splitext(sfile)
-        if os.path.exists(path + '.c') or os.path.exists(path + '.cpp'):
-            c_num += 1
-    return c_num
-
-
-def cythonize_extensions(extensions):
-    try:
-        from Cython.Build import cythonize
-        return cythonize(extensions, compiler_directives={'language_level' : "3"})
-    except ImportError:
-        warnings.warn("""WARNING: cython required to generate fast c code is not found on this system.
-Only slow pure python alternative functions will be available.
-To use fast implementation of some functions writen in cython either:
-a) install cython and re-run the installation,
-b) try alternative source distribution containing cythonized C versions of fast code,
-c) use binary distribution (i.e. wheels, egg).""")
-        return []
-
-
-def no_cythonize(extensions):
-    for extension in extensions:
-        sources = []
-        for sfile in extension.sources:
-            path, ext = os.path.splitext(sfile)
-            if ext in ('.pyx', '.py'):
-                if extension.language == 'c++':
-                    ext = '.cpp'
-                else:
-                    ext = '.c'
-                sfile = path + ext
-            sources.append(sfile)
-        extension.sources[:] = sources
-    return extensions
-
-
-# to cythonize, or not to cythonize... :
-if len(raw_extensions) > count_c_extensions(raw_extensions):
-    extensions = cythonize_extensions(raw_extensions)
-else:
-    extensions = no_cythonize(raw_extensions)
-
-
-# to compile or not to compile... depends if compiler is present:
-compiler = distutils.ccompiler.new_compiler()
-assert isinstance(compiler, distutils.ccompiler.CCompiler)
-distutils.sysconfig.customize_compiler(compiler)
-try:
-    with TemporaryDirectory() as tmpdir:
-        compiler.compile([os.path.join(setup_path, 'hyperspy', 'misc', 'etc',
-                                   'test_compilers.c')], output_dir=tmpdir)
-except (CompileError, DistutilsPlatformError):
-    warnings.warn("""WARNING: C compiler can't be found.
-Only slow pure python alternative functions will be available.
-To use fast implementation of some functions writen in cython/c either:
-a) check that you have compiler (EXACTLY SAME as your python
-distribution was compiled with) installed,
-b) use binary distribution of hyperspy (i.e. wheels, egg, (only osx and win)).
-Installation will continue in 5 sec...""")
-    extensions = []
-    from time import sleep
-    sleep(5)  # wait 5 secs for user to notice the message
-
-
-class Recythonize(Command):
-
-    """cythonize all extensions"""
-    description = "(re-)cythonize all changed cython extensions"
-
-    user_options = []
-
-    def initialize_options(self):
-        """init options"""
-        pass
-
-    def finalize_options(self):
-        """finalize options"""
-        pass
-
-    def run(self):
-        # if there is no cython it is supposed to fail:
-        from Cython.Build import cythonize
-        global raw_extensions
-        global extensions
-        cythonize(extensions)
 
 
 class update_version_when_dev:
@@ -274,12 +160,10 @@ with update_version_when_dev() as version:
         name="hyperspy",
         package_dir={'hyperspy': 'hyperspy'},
         version=version,
-        ext_modules=extensions,
         packages=['hyperspy',
                   'hyperspy.datasets',
                   'hyperspy._components',
                   'hyperspy.datasets',
-                  'hyperspy.io_plugins',
                   'hyperspy.docstrings',
                   'hyperspy.drawing',
                   'hyperspy.drawing._markers',
@@ -292,7 +176,6 @@ with update_version_when_dev() as version:
                   'hyperspy.tests.component',
                   'hyperspy.tests.datasets',
                   'hyperspy.tests.drawing',
-                  'hyperspy.tests.io',
                   'hyperspy.tests.learn',
                   'hyperspy.tests.model',
                   'hyperspy.tests.samfire',
@@ -398,9 +281,6 @@ with update_version_when_dev() as version:
         url=Release.url,
         project_urls=Release.PROJECT_URLS,
         keywords=Release.keywords,
-        cmdclass={
-            'recythonize': Recythonize,
-        },
         classifiers=[
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.7",
