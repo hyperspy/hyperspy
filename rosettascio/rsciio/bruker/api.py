50a0b1b44315aafa2ace931a92a219d8b8874529
Francisco de la Peña <francisco.de-la-pena-manchon@univ-lille.fr>
2022-07-06 09:40:12 +0200

    Fix bruker unbcf_fast import

--- rosettascio/rsciio/bruker/api.py
+++ rosettascio/rsciio/bruker/api.py
@@ -48,7 +48,7 @@ _logger = logging.getLogger(__name__)
 warn_once = True
 
 try:
-    from hyperspy.io_plugins import unbcf_fast
+    from rsciio.bruker import unbcf_fast
     fast_unbcf = True
     _logger.info("The fast cython based bcf unpacking library were found")
 except ImportError:  # pragma: no cover
