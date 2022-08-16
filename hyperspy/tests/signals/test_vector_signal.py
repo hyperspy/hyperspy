84e4bff3c87e53d11ac52905f17079579617303a
cssfrancis <csfrancis@wisc.edu>
2022-08-16 14:04:12 -0500

    Refactor: Cleaned up some code

--- hyperspy/tests/signals/test_vector_signal.py
+++ hyperspy/tests/signals/test_vector_signal.py
@@ -29,10 +29,10 @@ class TestVectorSignal:
     def two_d_vector(self):
         x = np.empty(shape=(4, 3), dtype=object)
         for i in np.ndindex(x.shape):
-            x[i] = np.random.random((4, 2))*10
+            x[i] = np.random.random((4, 2)) * 10
         s = BaseVectorSignal(x)
         s.vector = True
-        for ax, name, scale in zip(s.axes_manager._axes, "abcd",[0.1,0.1,0.1,0.1]):
+        for ax, name, scale in zip(s.axes_manager._axes, "abcd", [0.1, 0.1, 0.1, 0.1]):
             ax.name = name
             ax.scale = scale
         return s
@@ -55,10 +55,10 @@ class TestVectorSignal:
         new = two_d_vector.nav_to_vector(inplace=inplace, axis=axis)
         if inplace:
             new = two_d_vector
-        if axis is None or axis== (0, 1):
+        if axis is None or axis == (0, 1):
             assert len(new.axes_manager.signal_axes) == 4
             assert new.data.shape == (1,)
-        elif axis ==0:
+        elif axis == 0:
             assert len(new.axes_manager.signal_axes) == 3
             assert new.data.shape == (3,)
 
@@ -66,17 +66,17 @@ class TestVectorSignal:
     @pytest.mark.parametrize("nav_axis", [(), (0,), (0, 1), "all"])
     @pytest.mark.parametrize("sig_axis", ["all", (0,), (0, 1)])
     @pytest.mark.parametrize("real_units", [True, False])
-    def test_get_real_vectors(self, two_d_vector, nav_axis, sig_axis, real_units, flatten):
-        new = two_d_vector.get_real_vectors(sig_axis=sig_axis,
-                                            real_units=real_units,
-                                            nav_axis=nav_axis,
-                                            flatten=flatten
-                                            )
+    def test_get_real_vectors(
+        self, two_d_vector, nav_axis, sig_axis, real_units, flatten
+    ):
+        new = two_d_vector.get_real_vectors(
+            sig_axis=sig_axis, real_units=real_units, nav_axis=nav_axis, flatten=flatten
+        )
         if sig_axis == "all":
             sig_axis = (2, 3)
         if nav_axis == "all":
             nav_axis = (0, 1)
-        output_len = len(sig_axis)+len(nav_axis)
+        output_len = len(sig_axis) + len(nav_axis)
         if flatten:
             assert new.shape[1] == output_len
             if real_units:
@@ -87,10 +87,12 @@ class TestVectorSignal:
             assert new[0, 0].shape[1] == output_len
             assert new.shape == two_d_vector.data.shape
 
-    @pytest.mark.parametrize("top", [0.5, 5])
+    @pytest.mark.parametrize("top", [0.5, 5,])
     def test_slicing_vector(self, two_d_vector, top):
         sliced = two_d_vector.isig[0:top, :]
-        is_between = np.all([np.all(5 > sliced.data[i][:, 1]) for i in np.ndindex((4, 3))])
+        is_between = np.all(
+            [np.all(5 > sliced.data[i][:, 1]) for i in np.ndindex((4, 3))]
+        )
 
         assert is_between
         assert isinstance(sliced, BaseVectorSignal)
@@ -103,9 +105,10 @@ class TestVectorSignal:
         assert sliced.axes_manager.navigation_shape == (2, 4)
 
     def test_deepcopy(self, two_d_vector):
-        s =two_d_vector.deepcopy()
-        assert isinstance(s,BaseVectorSignal)
-    @pytest.mark.parametrize('axis', [0, 1, None])
+        s = two_d_vector.deepcopy()
+        assert isinstance(s, BaseVectorSignal)
+
+    @pytest.mark.parametrize("axis", [0, 1, None])
     def test_all_to_vector_axis(self, two_d_vector, axis):
         new = two_d_vector.nav_to_vector(axis=axis, inplace=False)
         if axis is None:
@@ -118,4 +121,3 @@ class TestVectorSignal:
     def test_to_markers(self, two_d_vector):
         markers = two_d_vector.to_markers()
         print(markers)
-
