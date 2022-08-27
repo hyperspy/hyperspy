91e28740ced40901ef25cea366a005e419437d65
Eric Prestat <eric.prestat@gmail.com>
2022-08-27 10:21:35 +0100

    Remove deprecation changes for `decomposition` method

--- hyperspy/tests/learn/test_decomposition.py
+++ hyperspy/tests/learn/test_decomposition.py
@@ -364,7 +364,7 @@ class TestDecompositionAlgorithm:
     def setup_method(self, method):
         self.s = signals.Signal1D(generate_low_rank_matrix())
 
-    @pytest.mark.parametrize("algorithm", ["SVD", "MLPCA"])
+    @pytest.mark.parametrize("algorithm", ["SVD", "MLPCA", "RPCA"])
     def test_decomposition(self, algorithm):
         self.s.decomposition(algorithm=algorithm, output_dimension=2)
 
@@ -376,24 +376,6 @@ class TestDecompositionAlgorithm:
     def test_decomposition_output_dimension_not_given(self, algorithm):
         with pytest.raises(ValueError, match="`output_dimension` must be specified"):
             self.s.decomposition(algorithm=algorithm, return_info=False)
-
-    @skip_sklearn
-    @pytest.mark.parametrize("algorithm", ["fast_svd", "fast_mlpca"])
-    def test_fast_deprecation_warning(self, algorithm):
-        with pytest.warns(
-            VisibleDeprecationWarning,
-            match="argument `svd_solver='randomized'` instead.",
-        ):
-            self.s.decomposition(algorithm=algorithm, output_dimension=2)
-
-    @skip_sklearn
-    @pytest.mark.parametrize("algorithm", ["RPCA_GoDec", "svd", "mlpca", "nmf",])
-    def test_name_deprecation_warning(self, algorithm):
-        with pytest.warns(
-            VisibleDeprecationWarning,
-            match="has been deprecated and will be removed in HyperSpy 2.0.",
-        ):
-            self.s.decomposition(algorithm=algorithm, output_dimension=2)
 
     def test_algorithm_error(self):
         with pytest.raises(ValueError, match="not recognised. Expected"):
@@ -591,11 +573,6 @@ def test_decomposition_mlpca_var_func():
 
 def test_decomposition_mlpca_warnings_errors():
     s = signals.Signal1D(generate_low_rank_matrix())
-
-    with pytest.warns(
-        VisibleDeprecationWarning, match="`polyfit` argument has been deprecated"
-    ):
-        s.decomposition(output_dimension=2, algorithm="MLPCA", polyfit=[1, 2, 3])
 
     with pytest.raises(
         ValueError, match="`var_func` and `var_array` cannot both be defined"
