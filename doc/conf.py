44db803f6b2eea630b4f6b2473a66879354f111a
Zanetta Pierre-marie <pierre-marie.zanetta@hotmail.fr>
2022-10-08 12:02:38 -0700

    Update conf

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
     'sphinx.ext.imgmath',
-    'sphinx.ext.mathjax',
     'sphinx.ext.graphviz',
     'sphinx.ext.autosummary',
     'sphinx_toggleprompt',
