fc4a2dffbe69967b6161c88b5bfc0e081b7edaa5
Eric Prestat <eric.prestat@gmail.com>
2023-04-23 10:11:44 +0100

    Add explicit support for python 3.11 - needs numba 0.57

--- setup.py
+++ setup.py
@@ -421,6 +421,7 @@ with update_version_when_dev() as version:
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
             "Development Status :: 4 - Beta",
             "Environment :: Console",
             "Intended Audience :: Science/Research",
