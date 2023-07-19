0d7f39e66357e1aaa9cf304aeb482abaa4965194
Carter Francis <csfrancis@wisc.edu>
2023-07-19 17:18:40 -0500

    Documentation: Added example for rectangular marker

--- examples/Markers/rectangle.py
+++ examples/Markers/rectangle.py
@@ -11,14 +11,12 @@ import numpy as np
 rng = np.random.default_rng()
 s = hs.signals.Signal2D(rng.random((25, 25, 100, 100)))
 
-# Define the position of the lines
-segments = rng.random((10, 2, 2)) * 100
+# Define the position of the boxes
+offsets = rng.random((10, 2)) * 100
 
-m = hs.plot.markers.MarkerCollection(
-    collection_class=mpl.collections.,
-    segments=segments,
-    colors='r',
-    )
-
+m = hs.plot.markers.MarkerCollection(collection_class=mpl.collections.PolyCollection,
+                                     offsets=offsets,
+                                     verts=np.array([[[-5, -5], [-5, 5], [5, 5], [5, -5]]]),
+                                     color="red")
 s.plot()
 s.add_marker(m)
