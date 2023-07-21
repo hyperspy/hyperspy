caaafcec967c573288fae1b394569ae677b57ae9
Carter Francis <csfrancis@wisc.edu>
2023-07-21 10:12:27 -0500

    Moved: Moved EELS tools to spectrospy

--- spectrospy/spectrospy/misc/eels/hartree_slater_gos.py
+++ spectrospy/spectrospy/misc/eels/hartree_slater_gos.py
@@ -23,7 +23,7 @@ import numpy as np
 from scipy import constants
 
 from hyperspy.defaults_parser import preferences
-from hyperspy.misc.eels.base_gos import TabulatedGOS
+from spectrospy.spectrospy.misc.eels.base_gos import TabulatedGOS
 
 
 _logger = logging.getLogger(__name__)
