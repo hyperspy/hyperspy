56c609149b54f48083a7bd90b2251a646c979ecb
Carter Francis <csfrancis@wisc.edu>
2023-07-23 14:19:05 -0500

    Documentation: Fixed Failing Example for Markers

--- examples/Markers/rectangle.py
+++ examples/Markers/rectangle.py
@@ -11,15 +11,14 @@ import numpy as np
 rng = np.random.default_rng()
 s = hs.signals.Signal2D(rng.random((25, 25, 100, 100)))
 
-# Define the corners of the rectangles
-rectangles = rng.random((10, 4) * 100)
+# Define the position of the ellipses
+offsets = rng.random((10, 2)) * 100
 
-m = hs.plot.markers.Rectangles(rectangles=rectangles,
-                               color="red"
-                               )
-
-s.plot()
-s.add_marker(m)
+m = hs.plot.markers.Rectangles(
+    widths=(4,),
+    heights=(2,),
+    offsets=offsets,
+    )
 
 """
 Dynamic Rectangle Markers
@@ -27,11 +26,13 @@ Dynamic Rectangle Markers
 """
 
 # Create a ragged array of offsets
-rectangles = np.empty(s.axes_manager.navigation_shape, dtype=object)
-for ind in np.ndindex(rectangles.shape):
-    rectangles[ind] = rng.random((10, 2)) * 100
+offsets = np.empty(s.axes_manager.navigation_shape, dtype=object)
+for ind in np.ndindex(offsets.shape):
+    offsets[ind] = rng.random((10, 2)) * 100
 
-m = hs.plot.markers.Rectangles(rectangles=rectangles, color="red")
+m = hs.plot.markers.Rectangles(offsets=offsets, widths=(15,), heights=(15,),
+                               color="blue", facecolor="none", linewidth=3)
+
 
 s.plot()
 s.add_marker(m)
