f1cf976a4f64a8741a5b440f3b74cba706286279
Carter Francis <csfrancis@wisc.edu>
2023-07-24 07:18:33 -0500

    Copyright :Added spectrospy copyright

--- spectrospy/spectrospy/misc/eels/hartree_slater_gos.py
+++ spectrospy/spectrospy/misc/eels/hartree_slater_gos.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
-# Copyright 2007-2023 The HyperSpy developers
+# Copyright 2007-2023 The SpectroSpy developers
 #
-# This file is part of HyperSpy.
+# This file is part of SpectroSpy.
 #
 # HyperSpy is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# HyperSpy is distributed in the hope that it will be useful,
+# SpectroSpy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with HyperSpy. If not, see <https://www.gnu.org/licenses/#GPL>.
+# along with SpectroSpy. If not, see <https://www.gnu.org/licenses/#GPL>.
 
 import logging
 from pathlib import Path
