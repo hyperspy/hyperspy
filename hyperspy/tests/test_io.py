985b0e6fcfed203d09c0568b885bc889296c9480
Francisco de la Peña <frandelapena@gmail.com>
2023-10-20 19:23:03 +0200

    Move IO test back to where it belongs, thanks @ericpre

--- hyperspy/tests/test_io.py
+++ hyperspy/tests/test_io.py
@@ -294,3 +294,46 @@ def test_marker_save_load(tmp_path):
     assert s2.metadata.Markers["Points"]._signal is s2
 
     s2.plot()
+
+def test_load_save_filereader_metadata(tmp_path):
+    # tests that original FileReader metadata is correctly persisted and
+    # appended through a save and load cycle
+
+    fname = PATH.parent / "drawing" / "data" / "Cr_L_cl.hspy"
+    s = hs.load(fname)
+    assert s.metadata.General.FileIO.Number_0.io_plugin == \
+           'rsciio.hspy'
+    assert s.metadata.General.FileIO.Number_0.operation == 'load'
+    assert s.metadata.General.FileIO.Number_0.hyperspy_version == hs_version
+
+    f = tmp_path / "temp"
+    s.save(f)
+    expected = {
+        '0': {
+            'io_plugin': 'rsciio.hspy',
+            'operation': 'load',
+            'hyperspy_version': hs_version
+        },
+        '1': {
+            'io_plugin': 'rsciio.hspy',
+            'operation': 'save',
+            'hyperspy_version': hs_version
+        },
+        '2': {
+            'io_plugin': 'rsciio.hspy',
+            'operation': 'load',
+            'hyperspy_version': hs_version
+        },
+    }
+    del s.metadata.General.FileIO.Number_0.timestamp  # runtime dependent
+    del s.metadata.General.FileIO.Number_1.timestamp  # runtime dependent
+    assert \
+        s.metadata.General.FileIO.Number_0.as_dictionary() == expected['0']
+    assert \
+        s.metadata.General.FileIO.Number_1.as_dictionary() == expected['1']
+
+    t = hs.load(tmp_path / "temp.hspy")
+    del t.metadata.General.FileIO.Number_0.timestamp  # runtime dependent
+    del t.metadata.General.FileIO.Number_1.timestamp  # runtime dependent
+    del t.metadata.General.FileIO.Number_2.timestamp  # runtime dependent
+    assert t.metadata.General.FileIO.as_dictionary() == expected
