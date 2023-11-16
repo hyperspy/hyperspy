ceb9351d624614b8e8bbb8ec738439c6accb959a
Eric Prestat <eric.prestat@gmail.com>
2023-11-16 20:33:37 +0000

    Use `setuptools_scm` to define version at build time

--- hyperspy/__init__.py
+++ hyperspy/__init__.py
@@ -18,9 +18,22 @@
 
 import importlib
 import logging
+from pathlib import Path
 from hyperspy import docstrings
 
 _logger = logging.getLogger(__name__)
+
+
+if Path(__file__).parent.parent.name == "site-packages":  # pragma: no cover
+    # Tested in the "build" workflow on GitHub CI
+    from importlib.metadata import version
+
+    __version__ = version("hyperspy")
+else:
+    # Editable install
+    from setuptools_scm import get_version
+
+    __version__ = get_version(Path(__file__).parent.parent)
 
 
 __doc__ = """
@@ -51,19 +64,10 @@ def __dir__():
     return sorted(__all__)
 
 
-_import_mapping = {
-    '__version__':'.Release',
-    }
-
-
 def __getattr__(name):
-    if name in __all__:
-        if name in _import_mapping.keys():
-            import_path = 'hyperspy' + _import_mapping.get(name)
-            return getattr(importlib.import_module(import_path), name)
-        else:  # pragma: no cover
-            # We can't get this block covered in the test suite because it is
-            # already imported, when running the test suite.
-            # If this is broken, a lot of things will be broken!
-            return importlib.import_module("." + name, 'hyperspy')
+    if name in __all__: # pragma: no cover
+        # We can't get this block covered in the test suite because it is
+        # already imported, when running the test suite.
+        # If this is broken, a lot of things will be broken!
+        return importlib.import_module("." + name, 'hyperspy')
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
