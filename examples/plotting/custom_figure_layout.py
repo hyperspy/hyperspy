d7e9d57a13d03ccdc447ae0117ce07b1daf95889
Carter Francis <csfrancis@wisc.edu>
2024-03-27 08:14:43 -0500

    Example: Add Example for ROI insets

--- examples/plotting/custom_figure_layout.py
+++ examples/plotting/custom_figure_layout.py
@@ -15,17 +15,17 @@ import hyperspy.api as hs
 import numpy as np
 
 rng = np.random.default_rng()
-s = hs.signals.Signal2D(rng.random((10,10,10,10)))
+s = hs.signals.Signal2D(rng.random((10, 10, 10, 10)))
 fig = plt.figure(figsize=(10, 5))
 subfigs = fig.subfigures(1, 2, wspace=0.07)
 s.plot(navigator_kwds=dict(fig=subfigs[0]), fig=subfigs[1])
 
-#%%
+# %%
 
 # Sharing a navigator between two hyperspy signals
 
-s = hs.signals.Signal2D(rng.random((10,10,10,10)))
-s2 = hs.signals.Signal2D(rng.random((10,10,50,50)))
+s = hs.signals.Signal2D(rng.random((10, 10, 10, 10)))
+s2 = hs.signals.Signal2D(rng.random((10, 10, 50, 50)))
 
 fig = plt.figure(figsize=(8, 7))
 head_figures = fig.subfigures(1, 2, wspace=0.07)
@@ -33,5 +33,5 @@ signal_figures = head_figures[1].subfigures(2, 1, hspace=0.07)
 s.plot(navigator_kwds=dict(fig=head_figures[0], colorbar=None), fig=signal_figures[0])
 s2.plot(navigator=None, fig=signal_figures[1], axes_manager=s.axes_manager)
 
-#%%
+# %%
 # sphinx_gallery_thumbnail_number = 2
