c4f54c74944b01c25682889a25f677083d2651be
Eric Prestat <eric.prestat@gmail.com>
2024-05-04 08:49:30 +0100

    Only import gui widget when the toolkit is enabled

--- hyperspy/ui_registry.py
+++ hyperspy/ui_registry.py
@@ -127,8 +127,8 @@ def get_gui(self, toolkey, display=True, toolkit=None, **kwargs):
     available_toolkits = set()
     used_toolkits = set()
     for toolkit, specs in UI_REGISTRY[toolkey].items():
-        f = getattr(importlib.import_module(specs["module"]), specs["function"])
         if toolkit in toolkits:
+            f = getattr(importlib.import_module(specs["module"]), specs["function"])
             used_toolkits.add(toolkit)
             try:
                 thisw = f(obj=self, display=display, **kwargs)
