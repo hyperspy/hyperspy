e4595c110c491f912d23c0ed2020cf1d9b864083
Francisco de la Peña <francisco.de-la-pena-manchon@univ-lille.fr>
2024-06-01 15:35:12 +0200

    Update hyperspy/tests/signals/test_2D.py
    Co-authored-by: Eric Prestat <eric.prestat@gmail.com>

--- hyperspy/tests/signals/test_2D.py
+++ hyperspy/tests/signals/test_2D.py
@@ -447,8 +447,7 @@ class Test2D:
 
         if s._lazy:
             s.compute()
-        else:
-            assert s.data is original_data
+        assert s.data is original_data
 
         np.testing.assert_array_almost_equal(s.data, rng2.poisson(lam=data, **kwargs))
         s.change_dtype("float64")
