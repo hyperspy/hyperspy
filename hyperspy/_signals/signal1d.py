22c40f5ee1e1f6fa69487820da325f8a80b7a6fd
Eric Prestat <eric.prestat@gmail.com>
2024-06-13 17:27:30 +0100

    Add `BaselineRemoval` class

--- hyperspy/_signals/signal1d.py
+++ hyperspy/_signals/signal1d.py
@@ -1303,27 +1303,37 @@ class Signal1D(BaseSignal, CommonSignal1D):
 
     remove_background.__doc__ %= (SHOW_PROGRESSBAR_ARG, DISPLAY_DT, TOOLKIT_DT)
 
-    def remove_baseline(self, interactive=False, algorithm="iasls", **kwargs):
+    def remove_baseline(
+        self,
+        algorithm=None,
+        inplace=True,
+        display=True,
+        toolkit=None,
+        **kwargs,
+    ):
         """
         Remove baselines using algorithm implemented in pybaselines.
         """
-        from pybaselines import Baseline
+        if algorithm is None:
+            from hyperspy.utils.baseline_removal_tool import BaselineRemoval
 
-        if interactive:
-            pass
+            br = BaselineRemoval(self, algorithm=algorithm, **kwargs)
+            return br.gui(display=display, toolkit=toolkit)
         else:
+            from pybaselines import Baseline
+
             baseline_fitter = getattr(
                 Baseline(
-                    self.axes_manager.signal_axes[0].axis,
+                    self.axes_manager[-1].axis,
                     check_finite=False,
                 ),
                 algorithm,
             )
 
-            def baseline_fitting(data):
+            def baseline_fitting(data, **kwargs):
                 return data - baseline_fitter(data, **kwargs)[0]
 
-            self.map(baseline_fitting)
+            return self.map(baseline_fitting, inplace=inplace, **kwargs)
 
     @interactive_range_selector
     def crop_signal(
