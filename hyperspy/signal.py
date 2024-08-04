4060859a40b2723dc10ca1408d311a76e558b15a
Eric Prestat <eric.prestat@gmail.com>
2024-08-04 13:52:53 +0100

    Add option to silence warning when `BaseSignal.map` operates on signals with non-uniform axes or differens scales, units for the signals axis

--- hyperspy/signal.py
+++ hyperspy/signal.py
@@ -5177,6 +5177,7 @@ class BaseSignal(
         output_signal_size=None,
         output_dtype=None,
         lazy_output=None,
+        silence_warnings=False,
         **kwargs,
     ):
         """Apply a function to the signal data at all the navigation
@@ -5233,6 +5234,14 @@ class BaseSignal(
             See docstring for output_signal_size for more information.
             Default None.
         %s
+        silence_warnings : bool or list of str
+            If ``True``, don't warn when one of the signal axes are non-uniform,
+            or the scales of the signal axes differ. If ``"non-uniform"``,
+            ``"scales"`` or ``"units"`` are in the list the warning will be
+            silenced when using non-uniform axis, different scales or different
+            units of the signal axes, respectively.
+            If ``False``, all warnings will be added to the logger.
+            Default is ``False``.
         **kwargs : dict
             All extra keyword arguments are passed to the provided function
 
@@ -5328,12 +5337,12 @@ class BaseSignal(
                     f"with the size of the mapped signal "
                     f"<{self_nav_shape}>"
                 )
-        # TODO: Consider support for non-uniform signal axis
         if any([not ax.is_uniform for ax in self.axes_manager.signal_axes]):
-            _logger.warning(
-                "At least one axis of the signal is non-uniform. Can your "
-                "`function` operate on non-uniform axes?"
-            )
+            if not (silence_warnings or silence_warnings == "non-uniform"):
+                _logger.warning(
+                    "At least one axis of the signal is non-uniform. Can your "
+                    "`function` operate on non-uniform axes?",
+                )
         else:
             # Check if the signal axes have inhomogeneous scales and/or units and
             # display in warning if yes.
@@ -5342,10 +5351,19 @@ class BaseSignal(
             for i in range(len(self.axes_manager.signal_axes)):
                 scale.add(self.axes_manager.signal_axes[i].scale)
                 units.add(self.axes_manager.signal_axes[i].units)
-            if len(units) != 1 or len(scale) != 1:
+            if len(scale) != 1 and not (
+                silence_warnings or silence_warnings == "scales"
+            ):
                 _logger.warning(
                     "The function you applied does not take into account "
-                    "the difference of units and of scales in-between axes."
+                    "the difference of scales in-between axes."
+                )
+            if len(units) != 1 and not (
+                silence_warnings or silence_warnings == "units"
+            ):
+                _logger.warning(
+                    "The function you applied does not take into account "
+                    "the difference of units in-between axes."
                 )
         # If the function has an axis argument and the signal dimension is 1,
         # we suppose that it can operate on the full array and we don't
--- hyperspy/tests/signals/test_map_method.py
+++ hyperspy/tests/signals/test_map_method.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with HyperSpy. If not, see <https://www.gnu.org/licenses/#GPL>.
 
+import logging
 from unittest import mock
 
 import dask
@@ -1293,3 +1294,73 @@ def test_map_processes_scheduler():
 
     with dask.config.set(scheduler="processes"):
         s.map(rotate, angle=angles.T, reshape=False)
+
+
+def test_silence_warning_non_uniform(caplog):
+    s = hs.signals.Signal1D(np.arange(5 * 10).reshape(5, 10))
+    s.axes_manager[-1].convert_to_non_uniform_axis()
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=True, inplace=False)
+    assert caplog.text == ""
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=["non-uniform"], inplace=False)
+    assert caplog.text == ""
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, inplace=False)
+    assert "At least one axis of the signal is non-uniform." in caplog.text
+
+
+def test_silence_warning_scales(caplog):
+    s = hs.signals.Signal2D(np.arange(5 * 10 * 15).reshape(5, 10, 15))
+    s.axes_manager.signal_axes[0].scale = 0.5
+    s.axes_manager.signal_axes[1].scale = 1.0
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=True, inplace=False)
+    assert caplog.text == ""
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=["scale"], inplace=False)
+    assert caplog.text == ""
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=False, inplace=False)
+    assert "The function you applied does not take into account" in caplog.text
+    assert "scales" in caplog.text
+
+
+def test_silence_warning_units(caplog):
+    s = hs.signals.Signal2D(np.arange(5 * 10 * 15).reshape(5, 10, 15))
+    s.axes_manager.signal_axes[0].units = "m"
+    s.axes_manager.signal_axes[1].units = "nm"
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=True, inplace=False)
+    assert caplog.text == ""
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=["units"], inplace=False)
+    assert caplog.text == ""
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=False, inplace=False)
+    assert "The function you applied does not take into account" in caplog.text
+    assert "units" in caplog.text
+
+
+def test_silence_warning_scales_units(caplog):
+    s = hs.signals.Signal2D(np.arange(5 * 10 * 15).reshape(5, 10, 15))
+    s.axes_manager.signal_axes[0].units = "m"
+    s.axes_manager.signal_axes[0].scale = 10
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=["scales", "units"], inplace=False)
+    assert caplog.text == ""
+
+    with caplog.at_level(logging.WARNING):
+        s.map(np.sum, silence_warnings=False, inplace=False)
+    assert "scales" in caplog.text
+    assert "units" in caplog.text
