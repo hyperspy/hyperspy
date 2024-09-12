cd3fc13301faadd3c4d68848fcbc442a52dcef9b
Eric Prestat <eric.prestat@gmail.com>
2024-09-12 19:12:59 +0100

    Add style parameter to `print_known_signal_types`

--- hyperspy/tests/utils/test_print_known_signal_types.py
+++ hyperspy/tests/utils/test_print_known_signal_types.py
@@ -1,3 +1,5 @@
+from prettytable import MARKDOWN
+
 from hyperspy.utils import print_known_signal_types
 
 
@@ -7,3 +9,14 @@ def test_text_output(capsys):
     assert "signal_type" in captured.out
     # the output will be str, not html
     assert "<p>" not in captured.out
+
+
+def test_style(capsys):
+    print_known_signal_types(style=MARKDOWN)
+    captured = capsys.readouterr()
+
+    assert "signal_type" in captured.out
+    # the output will be markdown, not ascii
+    assert ":--" in captured.out  # markdown
+    assert "<p>" not in captured.out  # not html
+    assert "+--" not in captured.out  # not ascii
