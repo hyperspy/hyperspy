0e2765140735645edef774e7ae44f1fb8b4a0a48
Eric Prestat <eric.prestat@gmail.com>
2024-09-22 10:33:45 +0100

    Fix enable/disable parameters in `remove_baseline` GUI and add tests

--- hyperspy/utils/baseline_removal_tool.py
+++ hyperspy/utils/baseline_removal_tool.py
@@ -78,13 +78,6 @@ algorithms_parameters = {
     ),
     "irsqr": ("lam", "quantile", "num_knots", "spline_degree", "diff_order"),
 }
-# # Penalized splines version of whittaker
-# algorithms_parameters_splines = {
-#     f"pspline_{algorithm}": algorithms_parameters[algorithm]
-#     + ("num_knots", "spline_degree")
-#     for algorithm in algorithms_mapping_whittaker.values()
-# }
-# algorithms_parameters.update(algorithms_parameters_splines)
 
 algorithms_mapping_inverse = {v: k for k, v in algorithms_mapping.items()}
 # Get the mapping of parameters to algorithms
@@ -106,7 +99,7 @@ def _baseline_fitting(data, baseline_fitter, **kwargs):
 class BaselineRemoval(t.HasTraits):
     algorithm = t.Enum(
         *algorithms_mapping.keys(),
-        default="Improved Asymmetric Least Squares",
+        default="Adaptive Smoothness Penalized Least Squares",
     )
     # Whittaker parameters
     lam = t.Range(1.0, 1e15, value=1e6)
@@ -115,23 +108,28 @@ class BaselineRemoval(t.HasTraits):
     eta = t.Range(0.0, 1.0, value=0.5)
     diff_order = t.Range(1, 3, value=2)
     penalized_spline = t.Bool()
+    # Polynomial parameters
+    poly_order = t.Range(1, 10, value=2)
     # Spline parameters
     num_knots = t.Range(10, 10000, value=100)
     spline_degree = t.Range(1, 5, value=3)
     symmetric = t.Bool()
-    # Polynomial parameters
-    poly_order = t.Range(1, 10)
     quantile = t.Range(0.001, 0.5, value=0.05)
 
-    # use to know if parameters needs to be enable or not
-    # workaround for traitsui which doesn't support evaluation of more than
-    # 2 conditions...
+    # these are used to know if parameters needs to be enable or not
     # Whittaker parameters
     _enable_p = t.Bool()
+    _enable_lam = t.Bool()
     _enable_lam_1 = t.Bool()
     _enable_eta = t.Bool()
+    _enable_diff_order = t.Bool()
+    _enable_penalized_spline = t.Bool()
+    # Polynomial parameters
+    _enable_poly_order = t.Bool()
+    # Splines parameters
+    _enable_spline_parameters = t.Bool()
 
-    def __init__(self, signal, algorithm="Asymmetric Least Squares"):
+    def __init__(self, signal):
         super().__init__()
         self.signal = signal
         # Plot the signal if not already plotted
@@ -140,37 +138,49 @@ class BaselineRemoval(t.HasTraits):
         self.bl_line = None  # The baseline line
         self.estimator = None
         self.estimator_line = None
+        # Use as default, good speed and baseline estimation
+        self.algorithm = "Adaptive Smoothness Penalized Least Squares"
         self.set_estimator()
 
     def set_estimator(self):
         from pybaselines import Baseline
 
-        algorithm = self._get_algorithm_name()
+        algorithm = self._get_algorithm_abbreviation()
         self.estimator = getattr(
             Baseline(
                 self.signal.axes_manager[-1].axis,
                 check_finite=False,
             ),
-            self._get_algorithm_name(),
+            self._get_algorithm_abbreviation(),
         )
 
-        algorithm = self._get_algorithm_name(False)
+        # get algorithm full name
+        algorithm = algorithms_mapping_inverse[self._get_algorithm_abbreviation(False)]
 
         self._enable_p = algorithm in parameters_algorithms["p"]
+        self._enable_lam = algorithm in parameters_algorithms["lam"]
         self._enable_lam_1 = algorithm in parameters_algorithms["lam_1"]
         self._enable_eta = algorithm in parameters_algorithms["eta"]
-        self._enable_num_knots = algorithm in parameters_algorithms["num_knots"]
+        self._enable_diff_order = algorithm in parameters_algorithms["diff_order"]
+        self._enable_poly_order = algorithm in algorithms_mapping_polynomial.keys()
+        self._enable_penalized_spline = algorithm in algorithms_mapping_whittaker.keys()
+        self._enable_spline_parameters = (
+            algorithm in parameters_algorithms["spline_degree"] or self.penalized_spline
+        )
         self._update_lines()
 
-    def _penalized_spline_changed(self, old, new):
+    @t.observe(
+        [
+            "penalized_spline",
+            "algorithm",
+        ],
+        post_init=True,
+    )
+    def _update_estimator(self, event):
         self.set_estimator()
         self._update_lines()
 
-    def _algorithm_changed(self, old, new):
-        self.set_estimator()
-        self._update_lines()
-
-    def _get_algorithm_name(self, with_prefix=True):
+    def _get_algorithm_abbreviation(self, with_prefix=True):
         algorithm = algorithms_mapping[self.algorithm]
         if (
             self.penalized_spline
@@ -182,7 +192,7 @@ class BaselineRemoval(t.HasTraits):
         return algorithm
 
     def _get_kwargs(self):
-        args_name = algorithms_parameters[self._get_algorithm_name(False)]
+        args_name = algorithms_parameters[self._get_algorithm_abbreviation(False)]
         if self.penalized_spline:
             args_name += ("num_knots", "spline_degree")
         kwargs = {key: getattr(self, key) for key in args_name}
@@ -210,13 +220,11 @@ class BaselineRemoval(t.HasTraits):
             "spline_degree",
             "symmetric",
             "poly_order",
+            "quantile",
         ],
         post_init=True,
     )
-    def lam_change(self, event):
-        self._update_lines()
-
-    def _update_lines(self):
+    def _update_lines(self, event=None):
         if self.estimator_line is None:
             self._create_lines()
         try:
@@ -233,4 +241,9 @@ class BaselineRemoval(t.HasTraits):
             inplace=True,
             **self._get_kwargs(),
         )
-        self.estimator_line.close()
+        self.close()
+
+    def close(self):
+        if self.signal._plot.is_active and self.estimator_line is not None:
+            self.estimator_line.close()
+            self.estimator_line = None
