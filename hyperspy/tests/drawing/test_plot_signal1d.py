3a27fee93f60df095d91bd5fe040acadb3e6142d
Aidan Campbell <campbell@pdi-berlin.de>
2025-11-20 19:09:04 +0100

    Add tests for extending spectra normalisation functionality

--- hyperspy/tests/drawing/test_plot_signal1d.py
+++ hyperspy/tests/drawing/test_plot_signal1d.py
@@ -469,15 +469,24 @@ def test_plot_spectra_linestyle_error():
         hs.plot.plot_spectra(s, linestyle="invalid")
 
 
+
+def callable_normalisation_function(signal):
+    data = signal.data
+    normalise_range = signal.isig[40:50].data
+    scale_factor = 1 / normalise_range.mean()
+    return data * scale_factor
+
+@pytest.mark.parametrize("normalise", (True, False, callable_normalisation_function))
+@pytest.mark.parametrize("style", ("overlap", "cascade", "mosaic", "heatmap"))
 @pytest.mark.mpl_image_compare(
     baseline_dir=baseline_dir, tolerance=default_tol, style=style_pytest_mpl
 )
-@pytest.mark.parametrize("style", ("overlap", "cascade", "mosaic", "heatmap"))
-def test_plot_spectra_normalise(style):
+    
+def test_plot_spectra_normalise(style, normalise):
     s = hs.signals.Signal1D(np.arange(100)) + 100
     s2 = s * 1000
-
-    ax = hs.plot.plot_spectra([s, s2], style=style, normalise=True)
+    
+    ax = hs.plot.plot_spectra([s, s2], style=style, normalise=normalise)
     if style == "mosaic":
         ax = ax[0]
 
