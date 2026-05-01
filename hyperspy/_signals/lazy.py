Fix coeff.map_blocks result discarded in Poissonian noise normalisation
- coeff.map_blocks(np.nan_to_num) returns a new dask array but the
  result was not assigned back, leaving NaNs in the coefficient array
  and producing incorrect scaled data

hyperspy/_signals/lazy.py
@@ -1057,7 +1057,7 @@ class LazySignal(signals.BaseSignal):
                 coeff = (
                     raG[(...,) + (None,) * rbH.ndim] * rbH[(None,) * raG.ndim + (...,)]
                 )
-                coeff.map_blocks(np.nan_to_num)
+                coeff = coeff.map_blocks(np.nan_to_num)
                 coeff = da.where(coeff == 0, 1, coeff)
                 data = data / coeff
                 self.data = data