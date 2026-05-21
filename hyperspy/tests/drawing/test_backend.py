e18dee5883d1606dba5f46128e10a12fcb9451df
Carter Francis <cartsfrancis@gmail.com>
2026-05-20 21:50:38 -0500

    drawing: fix test_default_backend_satisfies_protocol to trigger backend registration on import

--- hyperspy/tests/drawing/test_backend.py
+++ hyperspy/tests/drawing/test_backend.py
@@ -3,5 +3,7 @@ from hyperspy.drawing.backends._protocol import PlottingBackend
 
 
 def test_default_backend_satisfies_protocol():
+    import hyperspy.drawing  # noqa: F401 triggers default backend registration
+
     backend = get_backend()
     assert isinstance(backend, PlottingBackend)
